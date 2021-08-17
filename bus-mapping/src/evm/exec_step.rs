38e77bf37f1bca412dabfe0802c6319009b0f0ca
CPerezz <c.perezbaro@gmail.com>
2021-08-17 21:20:50 +0200

    Add needed methods to `ExecutionStep`s API

--- bus-mapping/src/evm/exec_step.rs
+++ bus-mapping/src/evm/exec_step.rs
@@ -1,7 +1,14 @@
 //! Doc this
 
-use super::{EvmWord, GlobalCounter, Instruction, MemoryAddress, ProgramCounter};
-use crate::{error::Error, operation::Operation};
+use super::{
+    EvmWord, GlobalCounter, Instruction, MemoryAddress, ProgramCounter, MEM_ADDR_ZERO,
+    STACK_ADDR_ZERO,
+};
+use crate::{
+    error::Error,
+    operation::{bus_mapping::BusMappingInstance, Operation},
+    StackAddress,
+};
 use core::str::FromStr;
 use serde::{Deserialize, Serialize};
 use std::{
@@ -11,43 +18,78 @@ use std::{
 
 /// Doc
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct ExecutionStep<'a, T: Operation> {
+pub struct ExecutionStep<'a> {
     memory: BTreeMap<MemoryAddress, EvmWord>,
     stack: Vec<EvmWord>,
-    opcode: Instruction,
+    instruction: Instruction,
     pc: ProgramCounter,
     gc: GlobalCounter,
-    bus_mapping_instances: Vec<&'a T>, // Holds refs to the container with the related mem ops.
+    // Holds refs to the container with the related mem ops.
+    bus_mapping_instances: BusMappingInstance<'a>,
 }
 
-impl<'a, T: Operation> ExecutionStep<'a, T> {
+impl<'a> ExecutionStep<'a> {
     /// Doc
     pub fn new(
         memory: BTreeMap<MemoryAddress, EvmWord>,
         stack: Vec<EvmWord>,
-        opcode: Instruction,
+        instruction: Instruction,
         pc: ProgramCounter,
         gc: GlobalCounter,
-        instances: Vec<&'a T>,
+        instances: BusMappingInstance<'a>,
     ) -> Self {
         ExecutionStep {
             memory,
             stack,
-            opcode,
+            instruction,
             pc,
             gc,
             bus_mapping_instances: instances,
         }
     }
+
+    pub const fn memory(&self) -> &BTreeMap<MemoryAddress, EvmWord> {
+        &self.memory
+    }
+
+    pub const fn stack(&self) -> &Vec<EvmWord> {
+        &self.stack
+    }
+
+    pub fn stack_addr(&self) -> StackAddress {
+        StackAddress::from(1024 - self.stack_addr().0)
+    }
+
+    pub fn memory_addr(&self) -> &MemoryAddress {
+        self.memory
+            .iter()
+            .last()
+            .map(|items| items.0)
+            .unwrap_or_else(|| &MEM_ADDR_ZERO)
+    }
+
+    pub const fn instruction(&self) -> &Instruction {
+        &self.instruction
+    }
+
+    pub const fn pc(&self) -> ProgramCounter {
+        self.pc
+    }
+
+    pub const fn gc(&self) -> GlobalCounter {
+        self.gc
+    }
+
+    pub const fn bus_mapping_instances(&self) -> &BusMappingInstance {
+        &self.bus_mapping_instances
+    }
 }
 
-impl<'a, T: Operation> TryFrom<(&ParsedExecutionStep<'a>, GlobalCounter, Vec<&'a T>)>
-    for ExecutionStep<'a, T>
-{
+impl<'a> TryFrom<(&ParsedExecutionStep<'a>, GlobalCounter)> for ExecutionStep<'a> {
     type Error = Error;
 
     fn try_from(
-        parse_info: (&ParsedExecutionStep<'a>, GlobalCounter, Vec<&'a T>),
+        parse_info: (&ParsedExecutionStep<'a>, GlobalCounter),
     ) -> Result<Self, Self::Error> {
         // Memory part
         let mut mem_map = BTreeMap::new();
@@ -79,7 +121,7 @@ impl<'a, T: Operation> TryFrom<(&ParsedExecutionStep<'a>, GlobalCounter, Vec<&'a
             Instruction::from_str(parse_info.0.opcode)?,
             parse_info.0.pc,
             parse_info.1,
-            parse_info.2,
+            BusMappingInstance::new(),
         ))
     }
 }
@@ -96,7 +138,11 @@ struct ParsedExecutionStep<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::evm::Opcode;
+    use crate::{
+        evm::{opcodes::OpcodeId, Opcode},
+        operation::{container::OperationContainer, MemoryOp, StackOp, RW},
+        BlockConstants, ExecutionTrace,
+    };
     use num::BigUint;
 
     #[test]
@@ -114,30 +160,115 @@ mod tests {
         }
         "#;
 
-        let trace_loaded = ExecutionStep::try_from((
+        let step_loaded: ExecutionStep<'_> = ExecutionStep::try_from((
             &serde_json::from_str::<ParsedExecutionStep>(step_json).expect("Error on parsing"),
             GlobalCounter(0usize),
         ))
         .expect("Error on conversion");
 
-        let expected_trace = {
+        let expected_step = {
             let mut mem_map = BTreeMap::new();
-            mem_map.insert(MemoryAddress(0x00), EvmWord(BigUint::from(0u8)));
-            mem_map.insert(MemoryAddress(0x20), EvmWord(BigUint::from(0u8)));
-            mem_map.insert(MemoryAddress(0x40), EvmWord(BigUint::from(0x80u8)));
+            mem_map.insert(
+                MemoryAddress(BigUint::from(0x00u8)),
+                EvmWord(BigUint::from(0u8)),
+            );
+            mem_map.insert(
+                MemoryAddress(BigUint::from(0x20u8)),
+                EvmWord(BigUint::from(0u8)),
+            );
+            mem_map.insert(
+                MemoryAddress(BigUint::from(0x40u8)),
+                EvmWord(BigUint::from(0x80u8)),
+            );
 
             ExecutionStep::new(
                 mem_map,
                 vec![],
-                Instruction::new(Opcode::JUMPDEST, None),
+                Instruction::new(OpcodeId::JUMPDEST, None),
                 ProgramCounter(53),
                 GlobalCounter(0),
+                BusMappingInstance::new(),
             )
         };
 
-        assert_eq!(trace_loaded, expected_trace)
+        assert_eq!(step_loaded, expected_step)
     }
 
+    #[test]
+    fn parse_execution_trace() {
+        let input_trace = r#"
+        [
+            {
+                "memory": {
+                    "0": "0000000000000000000000000000000000000000000000000000000000000000",
+                    "20": "0000000000000000000000000000000000000000000000000000000000000000",
+                    "40": "0000000000000000000000000000000000000000000000000000000000000080"
+                },
+                "stack": [
+                    "40"
+                ],
+                "opcode": "PUSH1 40",
+                "pc": 0
+            },
+            {
+                "memory": {
+                    "00": "0000000000000000000000000000000000000000000000000000000000000000",
+                    "20": "0000000000000000000000000000000000000000000000000000000000000000",
+                    "40": "0000000000000000000000000000000000000000000000000000000000000080"
+                },
+                "stack": [
+                    "40",
+                    "40"
+                ],
+                "opcode": "PUSH1 40",
+                "pc": 1
+            }
+        ]
+        "#;
+
+        let trace_loaded: Vec<ExecutionStep> =
+            serde_json::from_str::<Vec<ParsedExecutionStep>>(input_trace)
+                .expect("Error on parsing")
+                .iter()
+                .enumerate()
+                .map(|(idx, step)| ExecutionStep::try_from((step, GlobalCounter(idx))))
+                .collect::<Result<Vec<ExecutionStep>, Error>>()
+                .expect("Error on conversion");
+
+        let block_ctants = BlockConstants {
+            hash: EvmWord(BigUint::from(0u8)),
+            coinbase: pasta_curves::Fp::zero(),
+            timestamp: pasta_curves::Fp::zero(),
+            number: pasta_curves::Fp::zero(),
+            difficulty: pasta_curves::Fp::zero(),
+            gaslimit: pasta_curves::Fp::zero(),
+            chain_id: pasta_curves::Fp::zero(),
+        };
+
+        let mut container = OperationContainer::new();
+        let mut bus_mapping_inst = BusMappingInstance::new();
+
+        let mem = StackOp::new(
+            RW::WRITE,
+            GlobalCounter(1usize),
+            StackAddress::from(1024),
+            EvmWord(BigUint::from(0x40u8)),
+        );
+        let mem = StackOp::new(
+            RW::WRITE,
+            GlobalCounter(3usize),
+            StackAddress::from(1023),
+            EvmWord(BigUint::from(0x40u8)),
+        );
+        bus_mapping_inst.insert(container.insert(mem.into()));
+
+        let trace_global_obj = ExecutionTrace {
+            entries: trace_loaded,
+            block_ctants,
+            container,
+        };
+    }
+    /*
     #[test]
     fn parse_execution_trace() {
         let input_trace = r#"
@@ -162,11 +293,7 @@ mod tests {
                     "40"
                 ],
                 "opcode": "PUSH1 40",
-                "pc": 54,
-                "gc": 54,
-                "MemOps: [
-                    StackOp{gc, key, value, ....}
-                ]
+                "pc": 54
             },
             {
                 "memory": {
@@ -187,7 +314,7 @@ mod tests {
                     "40": "0000000000000000000000000000000000000000000000000000000000000080"
                 },
                 "stack": [
-                    "deadbeef",  
+                    "deadbeef",
                     "80"
                 ],
                 "opcode": "PUSH4 deadbeaf",
@@ -200,9 +327,9 @@ mod tests {
                     "40": "0000000000000000000000000000000000000000000000000000000000000080"
                 },
                 "stack": [
-                    "80",   
-                    "deadbeef",  
-                    "80"  
+                    "80",
+                    "deadbeef",
+                    "80"
                 ],
                 "opcode": "DUP2",
                 "pc": 62
@@ -257,8 +384,8 @@ mod tests {
                     "80": "00000000000000000000000000000000000000000000000000000000deadbeef"
                 },
                 "stack": [
-                    "deadbeef",   
-                    "faceb00c",  
+                    "deadbeef",
+                    "faceb00c",
                     "80"
                 ],
                 "opcode": "MLOAD",
@@ -423,4 +550,5 @@ mod tests {
 
         assert_eq!(*trace_loaded.last().unwrap(), expected_trace)
     }
+    */
 }
