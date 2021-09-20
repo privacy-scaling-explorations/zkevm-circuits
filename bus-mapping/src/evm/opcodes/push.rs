f1abf8e2f66078c36c9a9b78cba44a913ddbed02
CPerezz <c.perezbaro@gmail.com>
2021-09-20 11:24:49 +0200

    Add `Opcode` impl for `Push1`

--- bus-mapping/src/evm/opcodes/push.rs
+++ bus-mapping/src/evm/opcodes/push.rs
@@ -1,9 +1,11 @@
+use core::ops::Deref;
 // Port this to a macro if possible to avoid defining all the PushN
 use super::Opcode;
 use crate::{
     evm::GlobalCounter,
     exec_trace::ExecutionStep,
     operation::{container::OperationContainer, StackOp, RW},
+    Error,
 };
 
 /// Number of ops that PUSH1 adds to the container & busmapping
@@ -17,23 +19,29 @@ pub(crate) struct Push1;
 impl Opcode for Push1 {
     fn gen_associated_ops(
         &self,
+        // Contains the PUSH1 instr
         exec_step: &mut ExecutionStep,
         container: &mut OperationContainer,
-    ) -> usize {
+        // Contains the next step where we can find the value that was pushed.
+        next_steps: &[ExecutionStep],
+    ) -> Result<usize, Error> {
         let op = StackOp::new(
             RW::WRITE,
             GlobalCounter::from(exec_step.gc().0 + 1),
-            exec_step.stack_addr(),
-            // TODO: This could be more robust.
-            // We should have a way to collect this addr (the parsed one which
-            // is not tied to 1024)
-            exec_step.stack()[exec_step.stack().len() - 1].clone(),
+            // Get the value and addr from the next step. Being the last position filled with an element in the stack
+            next_steps[0].stack().last_filled(),
+            next_steps[0]
+                .stack()
+                .deref()
+                .last()
+                .cloned()
+                .ok_or_else(|| Error::InvalidStackPointer)?,
         );
 
         exec_step
             .bus_mapping_instance_mut()
             .push(container.insert(op));
 
-        PUSH1_OP_NUM
+        Ok(PUSH1_OP_NUM)
     }
 }
