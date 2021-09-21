f3ad99da74a2c0675aec42712525c6cf69c82394
ChihChengLiang <chihchengliang@gmail.com>
2021-09-21 22:32:21 +0800

    make rust happy with the bigInt

--- keccak256/src/keccak_arith.rs
+++ keccak256/src/keccak_arith.rs
@@ -1,5 +1,6 @@
 use num_bigint::BigUint;
 use num_traits::{One, Zero};
+use std::ops::{Index, IndexMut};
 
 const B13: u64 = 13;
 const B9: u64 = 9;
@@ -7,9 +8,44 @@ const B9: u64 = 9;
 type Lane13 = BigUint;
 type Lane9 = BigUint;
 
-type StateB2 = [[u64; 5]; 5];
-type StateB13 = [[Lane13; 5]; 5];
-type StateB9 = [[Lane9; 5]; 5];
+struct State {
+    xy: Vec<BigUint>,
+}
+impl Default for State {
+    fn default() -> Self {
+        let mut xy: Vec<BigUint> = Vec::with_capacity(25);
+        for _ in 0..25 {
+            xy.push(Zero::zero());
+        }
+        Self { xy }
+    }
+}
+
+impl Index<(usize, usize)> for State {
+    type Output = BigUint;
+    fn index(&self, xy: (usize, usize)) -> &Self::Output {
+        assert!(xy.0 < 5);
+        assert!(xy.1 < 5);
+
+        &self.xy[xy.0 * 5 + xy.1]
+    }
+}
+
+impl IndexMut<(usize, usize)> for State {
+    fn index_mut(&mut self, xy: (usize, usize)) -> &mut Self::Output {
+        assert!(xy.0 < 5);
+        assert!(xy.1 < 5);
+
+        &mut self.xy[xy.0 * 5 + xy.1]
+    }
+}
+
+impl Clone for State {
+    fn clone(&self) -> State {
+        let xy = self.xy.clone();
+        State { xy }
+    }
+}
 
 const PERMUTATION: usize = 24;
 
@@ -83,7 +119,7 @@ fn convert_b13_lane_to_b9(x: Lane13, rot: u32) -> Lane9 {
     let mut acc: Lane9 = Zero::zero();
 
     for i in 0..65 {
-        let remainder: u64 = (raw % B13).to_u64_digits()[0];
+        let remainder: u64 = (raw.clone() % B13).to_u64_digits()[0];
         if i == 0 || i == 64 {
             special_chunk += remainder;
         } else {
@@ -105,9 +141,9 @@ fn convert_b9_lane_to_b13(x: Lane9) -> Lane13 {
     let mut raw = x;
     let mut acc: Lane13 = Zero::zero();
 
-    for i in 0..64 {
-        let remainder: u64 = (raw % B9).to_u64_digits()[0];
-        acc += convert_b9_coef(remainder) * base;
+    for _ in 0..64 {
+        let remainder: u64 = (raw.clone() % B9).to_u64_digits()[0];
+        acc += convert_b9_coef(remainder) * base.clone();
         raw /= B9;
         base *= B13;
     }
@@ -121,88 +157,94 @@ impl KeccakFArith {
         KeccakFArith {}
     }
 
-    pub fn permutations(&self, a: &StateB13) {
-        let mut in_b13: StateB13 = [[Zero::zero(); 5]; 5];
+    pub fn permutations(&self, a: &State) {
+        let mut in_b13 = State::default();
         for x in 0..5 {
             for y in 0..5 {
-                in_b13[x][y] = a[x][y];
+                in_b13[(x, y)] = a[(x, y)].clone();
             }
         }
 
         for i in 0..PERMUTATION {
-            let out_b9 = KeccakFArith::round_b(in_b13, ROUND_CONSTANTS[i]);
+            let out_b9 =
+                KeccakFArith::round_b(in_b13.clone(), ROUND_CONSTANTS[i]);
             for x in 0..5 {
                 for y in 0..5 {
-                    in_b13[x][y] = convert_b9_lane_to_b13(out_b9[x][y]);
+                    in_b13[(x, y)] =
+                        convert_b9_lane_to_b13(out_b9[(x, y)].clone());
                 }
             }
         }
     }
 
-    fn round_b(a: StateB13, rc: u64) -> StateB9 {
-        let s1 = KeccakFArith::theta(a);
-        let s2 = KeccakFArith::rho(s1);
+    fn round_b(a: State, rc: u64) -> State {
+        let s1 = KeccakFArith::theta(&a);
+        let s2 = KeccakFArith::rho(&s1);
 
-        let s3 = KeccakFArith::pi(s2);
-        let s4 = KeccakFArith::xi(s3);
-        let s5 = KeccakFArith::iota(s4, rc);
+        let s3 = KeccakFArith::pi(&s2);
+        let s4 = KeccakFArith::xi(&s3);
+        let s5 = KeccakFArith::iota(&s4, rc);
         s5
     }
 
-    fn theta(a: StateB13) -> StateB13 {
-        let mut c: [Lane13; 5] = [Zero::zero(); 5];
-        let mut out: StateB13 = [[Zero::zero(); 5]; 5];
+    fn theta(a: &State) -> State {
+        let mut c: Vec<Lane13> = Vec::with_capacity(5);
+        let mut out = State::default();
 
         for x in 0..5 {
-            c[x] = a[x][0] + a[x][1] + a[x][2] + a[x][3] + a[x][4];
+            c[x] =
+                &a[(x, 0)] + &a[(x, 1)] + &a[(x, 2)] + &a[(x, 3)] + &a[(x, 4)];
         }
 
         for x in 0..5 {
             for y in 0..5 {
-                out[x][y] = a[x][y] + c[(x + 4) % 5] + c[(x + 1) % 5] * B13;
+                out[(x, y)] =
+                    &a[(x, y)] + &c[(x + 4) % 5] + &c[(x + 1) % 5] * B13;
             }
         }
         out
     }
 
-    fn rho(a: StateB13) -> StateB9 {
-        let mut out: StateB9 = [[Zero::zero(); 5]; 5];
+    fn rho(a: &State) -> State {
+        let mut out = State::default();
         for x in 0..5 {
             for y in 0..5 {
-                out[x][y] =
-                    convert_b13_lane_to_b9(a[x][y], ROTATION_CONSTANTS[x][y]);
+                out[(x, y)] = convert_b13_lane_to_b9(
+                    a[(x, y)].clone(),
+                    ROTATION_CONSTANTS[x][y],
+                );
             }
         }
         out
     }
 
-    fn pi(a: StateB9) -> StateB9 {
-        let mut out: StateB9 = [[Zero::zero(); 5]; 5];
+    fn pi(a: &State) -> State {
+        let mut out = State::default();
         for x in 0..5 {
             for y in 0..5 {
-                out[y][(2 * x + 3 * y) % 5] = a[x][y];
+                out[(y, (2 * x + 3 * y) % 5)] = a[(x, y)].clone();
             }
         }
         out
     }
 
-    fn xi(a: StateB9) -> StateB9 {
-        let mut out: StateB9 = [[Zero::zero(); 5]; 5];
-        let TWO = BigUint::from(2 as u8);
-        let THREE = BigUint::from(3 as u8);
+    fn xi(a: &State) -> State {
+        let mut out = State::default();
+        let two = BigUint::from(2 as u8);
+        let three = BigUint::from(3 as u8);
         for x in 0..5 {
             for y in 0..5 {
-                out[x][y] = TWO * a[x][y]
-                    + a[(x + 1) % 5][y]
-                    + THREE * a[(x + 2) % 5][y];
+                out[(x, y)] = two.clone() * a[(x, y)].clone()
+                    + a[((x + 1) % 5, y)].clone()
+                    + three.clone() * a[((x + 2) % 5, y)].clone();
             }
         }
         out
     }
 
-    fn iota(a: StateB9, rc: u64) -> StateB9 {
-        let mut out = a;
-        out[0][0] += BigUint::from(2 as u8) * convert_b2_to_b9(rc);
+    fn iota(a: &State, rc: u64) -> State {
+        let mut out = a.clone();
+        out[(0, 0)] += BigUint::from(2 as u8) * convert_b2_to_b9(rc);
         out
     }
 }
