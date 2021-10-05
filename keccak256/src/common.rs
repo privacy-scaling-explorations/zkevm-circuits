85bc27c59201e79114225c4cec2f0adce91e431e
ChihChengLiang <chihchengliang@gmail.com>
2021-10-05 21:05:51 +0800

    add doc round

--- keccak256/src/common.rs
+++ keccak256/src/common.rs
@@ -1,7 +1,12 @@
+//! Types and constants of Keccak hash function. The constants can be found in the appendices of <https://keccak.team/keccak_specs_summary.html> or [pycryptodome](https://github.com/Legrandin/pycryptodome).
+
+/// The State is a 5x5 matrix of 64 bit lanes.
 pub type State = [[u64; 5]; 5];
 
+/// The number of rounds for the 1600 bits permutation used in Keccak-256. See [here](https://github.com/Legrandin/pycryptodome/blob/016252bde04456614b68d4e4e8798bc124d91e7a/src/keccak.c#L230)
 pub const PERMUTATION: usize = 24;
 
+/// The Keccak [round constants](https://github.com/Legrandin/pycryptodome/blob/016252bde04456614b68d4e4e8798bc124d91e7a/src/keccak.c#L257-L282)
 pub static ROUND_CONSTANTS: [u64; PERMUTATION] = [
     0x0000000000000001,
     0x0000000000008082,
@@ -29,6 +34,7 @@ pub static ROUND_CONSTANTS: [u64; PERMUTATION] = [
     0x8000000080008008,
 ];
 
+/// The Keccak [rotation offsets](https://github.com/Legrandin/pycryptodome/blob/016252bde04456614b68d4e4e8798bc124d91e7a/src/keccak.c#L232-L255)
 pub static ROTATION_CONSTANTS: [[u32; 5]; 5] = [
     [0, 36, 3, 41, 18],
     [1, 44, 10, 45, 2],
