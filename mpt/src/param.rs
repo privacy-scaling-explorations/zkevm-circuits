cd4277e2c3b41f943bf0390d7163d60af78de30f
Miha Stopar <stopar.miha@gmail.com>
2021-10-13 16:15:46 +0200

    branch json added; assignment of s_advices and c_advices

--- mpt/src/param.rs
+++ mpt/src/param.rs
@@ -1,3 +1,5 @@
 // Currently using 32 - each hash byte goes into its own cell, this might be compressed
 // for optimization purposes in the future.
 pub const HASH_WIDTH: usize = 32;
+pub const WITNESS_ROW_WIDTH: usize = 68;
+pub const LAYOUT_OFFSET: usize = 2;
