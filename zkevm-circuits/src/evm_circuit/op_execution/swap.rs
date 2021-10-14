1062bf1133004ba8142f7cc3c462cec2b1964dbe
Dream Wu <wwuwwei@126.com>
2021-10-14 11:31:44 +0800

    updated with main

--- zkevm-circuits/src/evm_circuit/op_execution/swap.rs
+++ zkevm-circuits/src/evm_circuit/op_execution/swap.rs
@@ -16,212 +16,35 @@ struct SwapSuccessAllocation<F> {
 
 #[derive(Clone, Debug)]
 pub struct SwapGadget<F> {
-	success: SwapSuccessAllocation<F>,
-	stack_underflow: Cell<F>,
-	// case selector
-	out_of_gas: (
-		Cell<F>,    // case selector
-		Cell<F>,    // gas available
-	),
+    success: SwapSuccessAllocation<F>,
+    stack_underflow: Cell<F>,
+    // case selector
+    out_of_gas: (
+        Cell<F>, // case selector
+        Cell<F>, // gas available
+    ),
 }
 
 impl<F: FieldExt> OpGadget<F> for SwapGadget<F> {
-	const RESPONSIBLE_OPCODES: &'static [OpcodeId] = &[
-		OpcodeId::SWAP1,
-		OpcodeId::SWAP2,
-		 OpcodeId::SWAP3,
-		 OpcodeId::SWAP4,
-		OpcodeId::SWAP5,
-		OpcodeId::SWAP6,
-		 OpcodeId::SWAP7,
-		OpcodeId::SWAP8,
-		OpcodeId::SWAP9,
-		 OpcodeId::SWAP10,
-		 OpcodeId::SWAP11,
-		  OpcodeId::SWAP12,
-		  OpcodeId::SWAP13,
-		  OpcodeId::SWAP14,
-		 OpcodeId::SWAP15,
-		 OpcodeId::SWAP16,
-	];
-	
-	const CASE_CONFIGS: &'static [CaseConfig] = &[
-		CaseConfig {
-			case: Case::Success,
-			num_word: 4,
-			num_cell: 0,
-			will_halt: false,
-		},
-		CaseConfig {
-			case: Case::StackUnderflow,
-			num_word: 0,
-			num_cell: 0,
-			will_halt: true,
-		},
-		CaseConfig {
-			case: Case::OutOfGas,
-			num_word: 0,
-			num_cell: 0,
-			will_halt: true,
-		},
-	];
-	
-	fn construct(case_allocations: Vec<CaseAllocation<F>>) -> Self {
-		let [success, stack_underflow, out_of_gas]: [CaseAllocation<F>; 3] =
-			case_allocations.try_into().unwrap();
-		Self {
-			success: SwapSuccessAllocation {
-				case_selector: success.selector.clone(),
-				words: success.words.try_into().unwrap(),
-			},
-			stack_underflow: stack_underflow.selector,
-			out_of_gas: (
-				out_of_gas.selector,
-				out_of_gas.resumption.unwrap().gas_available,
-			),
-		}
-	}
-	
-	fn constraints(
-		&self,
-		state_curr: &OpExecutionState<F>,
-		state_next: &OpExecutionState<F>,
-	) -> Vec<Constraint<F>> {
-		let OpExecutionState { opcode, .. } = &state_curr;
-		
-		let common_lookups = vec![
-			Lookup::FixedLookup(
-				FixedLookup::Range16,
-				[opcode.expr() - OpcodeId::SWAP1.expr(), 0.expr(), 0.expr()],
-			),
-			Lookup::FixedLookup(
-				FixedLookup::Range16,
-				[POSITION.expr() - OpcodeId::SWAP1.expr(), 0.expr(), 0.expr()],
-			),
-		];
-		
-		let success = {
-			let SwapSuccessAllocation {
-				case_selector,
-				words,
-			} = &self.success;
-			
-			let state_transition_constraints = vec![
-				state_next.global_counter.expr()
-					- (state_curr.global_counter.expr() + 1.expr()),
-				state_next.program_counter.expr()
-					- (state_curr.program_counter.expr() + 1.expr()),
-				state_next.gas_counter.expr()
-					- (state_curr.gas_counter.expr() + GasCost::FASTEST.expr()),
-			];
-			
-			// a == c, b == d, (a + c) == (b + d)
-			let swap_constraints = vec![
-				words[0].expr() - words[3].expr(),
-				words[1].expr() - words[2].expr(),
-				(words[0].expr() + words[1].expr()) - (words[2].expr() + words[3].expr()),
-			];
-			
-			/*
-				bus_mapping_lookup(gc, Stack, key+X, $val1, Read)
-				bus_mapping_lookup(gc+1, Stack, key, $val2, Read)
-				bus_mapping_lookup(gc+2, Stack, key+X, $val2, Write)
-				bus_mapping_lookup(gc+3, Stack, key, $val1, Write)
-			*/
-			let num_swaped = (POSITION - OpcodeId::SWAP1.as_u8() + 1) as i32;
-			let bus_mapping_lookups = vec![
-				Lookup::BusMappingLookup(BusMappingLookup::Stack {
-					index_offset: num_swaped,
-					value: words[0].expr(),
-					is_write: false,
-				}),
-				Lookup::BusMappingLookup(BusMappingLookup::Stack {
-					index_offset: 0,
-					value: words[1].expr(),
-					is_write: false,
-				}),
-				Lookup::BusMappingLookup(BusMappingLookup::Stack {
-					index_offset: num_swaped,
-					value: words[2].expr(),
-					is_write: true,
-				}),
-				Lookup::BusMappingLookup(BusMappingLookup::Stack {
-					index_offset: 0,
-					value: words[3].expr(),
-					is_write: true,
-				}),
-			];
-			
-			Constraint {
-				name: "PushGadget success",
-				selector: case_selector.expr(),
-				polys: [state_transition_constraints, swap_constraints]
-					.concat(),
-				lookups: bus_mapping_lookups,
-			}
-		};
-		
-		let stack_overflow = {
-			let stack_pointer = state_curr.stack_pointer.expr();
-			Constraint {
-				name: "PushGadget stack overflow",
-				selector: self.stack_underflow.expr(),
-				polys: vec![stack_pointer],
-				lookups: vec![],
-			}
-		};
-		
-		let out_of_gas = {
-			let (case_selector, gas_available) = &self.out_of_gas;
-			let gas_overdemand = state_curr.gas_counter.expr()
-				+ GasCost::FASTEST.expr()
-				- gas_available.expr();
-			Constraint {
-				name: "PushGadget out of gas",
-				selector: case_selector.expr(),
-				polys: vec![
-					(gas_overdemand.clone() - 1.expr())
-						* (gas_overdemand.clone() - 2.expr())
-						* (gas_overdemand - 3.expr()),
-				],
-				lookups: vec![],
-			}
-		};
-		
-		array::IntoIter::new([success, stack_overflow, out_of_gas])
-			.map(move |mut constraint| {
-				constraint.lookups =
-					[common_lookups.clone(), constraint.lookups].concat();
-				constraint
-			})
-			.collect()
-	}
-	
-	fn assign(
-		&self,
-		region: &mut Region<'_, F>,
-		offset: usize,
-		core_state: &mut CoreStateInstance,
-		execution_step: &ExecutionStep,
-	) -> Result<(), Error> {
-		match execution_step.case {
-			Case::Success => {
-				self.assign_success(region, offset, core_state, execution_step)
-			}
-			Case::StackUnderflow => {
-				unimplemented!()
-			}
-			Case::OutOfGas => {
-				unimplemented!()
-			}
-			_ => unreachable!()
-		}
-	}
-}
+    const RESPONSIBLE_OPCODES: &'static [OpcodeId] = &[
+        OpcodeId::SWAP1,
+        OpcodeId::SWAP2,
+        OpcodeId::SWAP3,
+        OpcodeId::SWAP4,
+        OpcodeId::SWAP5,
+        OpcodeId::SWAP6,
+        OpcodeId::SWAP7,
+        OpcodeId::SWAP8,
+        OpcodeId::SWAP9,
+        OpcodeId::SWAP10,
+        OpcodeId::SWAP11,
+        OpcodeId::SWAP12,
+        OpcodeId::SWAP13,
+        OpcodeId::SWAP14,
+        OpcodeId::SWAP15,
+        OpcodeId::SWAP16,
+    ];
 
-impl<F: FieldExt, const POSITION: u8> OpGadget<F> for SwapGadget<F, POSITION> {
-    const RESPONSIBLE_OPCODES: &'static [OpcodeId] = &[from_u8(POSITION)];
-    
     const CASE_CONFIGS: &'static [CaseConfig] = &[
         CaseConfig {
             case: Case::Success,
@@ -242,7 +65,7 @@ impl<F: FieldExt, const POSITION: u8> OpGadget<F> for SwapGadget<F, POSITION> {
             will_halt: true,
         },
     ];
-    
+
     fn construct(case_allocations: Vec<CaseAllocation<F>>) -> Self {
         let [success, stack_underflow, out_of_gas]: [CaseAllocation<F>; 3] =
             case_allocations.try_into().unwrap();
@@ -258,70 +81,68 @@ impl<F: FieldExt, const POSITION: u8> OpGadget<F> for SwapGadget<F, POSITION> {
             ),
         }
     }
-    
+
     fn constraints(
         &self,
         state_curr: &OpExecutionState<F>,
         state_next: &OpExecutionState<F>,
     ) -> Vec<Constraint<F>> {
         let OpExecutionState { opcode, .. } = &state_curr;
-        
-        let common_lookups = vec![
-            Lookup::FixedLookup(
-                FixedLookup::Range16,
-                [opcode.expr() - OpcodeId::SWAP1.expr(), 0.expr(), 0.expr()],
-            ),
-            Lookup::FixedLookup(
-                FixedLookup::Range16,
-                [POSITION.expr() - OpcodeId::SWAP1.expr(), 0.expr(), 0.expr()],
-            ),
-        ];
-        
+
+        let common_lookups = vec![Lookup::FixedLookup(
+            FixedLookup::Range16,
+            [opcode.expr() - OpcodeId::SWAP1.expr(), 0.expr(), 0.expr()],
+        )];
+
+        let num_swaped = opcode.expr() - OpcodeId::SWAP1.expr() + 1.expr();
         let success = {
             let SwapSuccessAllocation {
                 case_selector,
                 words,
             } = &self.success;
-            
+
             let state_transition_constraints = vec![
                 state_next.global_counter.expr()
-                    - (state_curr.global_counter.expr() + 1.expr()),
+                    - (state_curr.global_counter.expr() + 4.expr()),
                 state_next.program_counter.expr()
                     - (state_curr.program_counter.expr() + 1.expr()),
                 state_next.gas_counter.expr()
                     - (state_curr.gas_counter.expr() + GasCost::FASTEST.expr()),
             ];
-            
+
             /*
                 bus_mapping_lookup(gc, Stack, key+X, $val1, Read)
                 bus_mapping_lookup(gc+1, Stack, key, $val2, Read)
                 bus_mapping_lookup(gc+2, Stack, key+X, $val2, Write)
                 bus_mapping_lookup(gc+3, Stack, key, $val1, Write)
             */
-            let num_swaped = (POSITION - OpcodeId::SWAP1.as_u8() + 1) as i32;
+
             let bus_mapping_lookups = vec![
+                // constrain (num_swaped)th value of stack
                 Lookup::BusMappingLookup(BusMappingLookup::Stack {
-                    index_offset: num_swaped,
+                    index_offset: num_swaped.clone(),
                     value: words[0].expr(),
                     is_write: false,
                 }),
+                // constrain top value of stack
                 Lookup::BusMappingLookup(BusMappingLookup::Stack {
-                    index_offset: 0,
+                    index_offset: 0.expr(),
                     value: words[1].expr(),
                     is_write: false,
                 }),
+                // constrains when swap done
                 Lookup::BusMappingLookup(BusMappingLookup::Stack {
-                    index_offset: num_swaped,
+                    index_offset: num_swaped.clone(),
                     value: words[1].expr(),
                     is_write: true,
                 }),
                 Lookup::BusMappingLookup(BusMappingLookup::Stack {
-                    index_offset: 0,
+                    index_offset: 0.expr(),
                     value: words[0].expr(),
                     is_write: true,
                 }),
             ];
-            
+
             Constraint {
                 name: "SwapGadget success",
                 selector: case_selector.expr(),
@@ -329,17 +150,22 @@ impl<F: FieldExt, const POSITION: u8> OpGadget<F> for SwapGadget<F, POSITION> {
                 lookups: bus_mapping_lookups,
             }
         };
-        
-        let stack_overflow = {
-            let stack_pointer = state_curr.stack_pointer.expr();
+
+        // stack_underflow condition: 0 <= num_swaped +  stack_pointer - 1025 < 16
+        let stack_pointer = state_curr.stack_pointer.expr();
+        let diff = num_swaped + stack_pointer - 1025.expr();
+        let stack_underflow = {
             Constraint {
-                name: "SwapGadget stack overflow",
+                name: "SwapGadget stack underflow",
                 selector: self.stack_underflow.expr(),
-                polys: vec![stack_pointer],
-                lookups: vec![],
+                polys: vec![],
+                lookups: vec![Lookup::FixedLookup(
+                    FixedLookup::Range16,
+                    [diff, 0.expr(), 0.expr()],
+                )],
             }
         };
-        
+
         let out_of_gas = {
             let (case_selector, gas_available) = &self.out_of_gas;
             let gas_overdemand = state_curr.gas_counter.expr()
@@ -356,8 +182,8 @@ impl<F: FieldExt, const POSITION: u8> OpGadget<F> for SwapGadget<F, POSITION> {
                 lookups: vec![],
             }
         };
-        
-        array::IntoIter::new([success, stack_overflow, out_of_gas])
+
+        array::IntoIter::new([success, stack_underflow, out_of_gas])
             .map(move |mut constraint| {
                 constraint.lookups =
                     [common_lookups.clone(), constraint.lookups].concat();
@@ -365,7 +191,7 @@ impl<F: FieldExt, const POSITION: u8> OpGadget<F> for SwapGadget<F, POSITION> {
             })
             .collect()
     }
-    
+
     fn assign(
         &self,
         region: &mut Region<'_, F>,
@@ -388,7 +214,7 @@ impl<F: FieldExt, const POSITION: u8> OpGadget<F> for SwapGadget<F, POSITION> {
     }
 }
 
-impl<F: FieldExt, const POSITION: u8> SwapGadget<F, POSITION> {
+impl<F: FieldExt> SwapGadget<F> {
     fn assign_success(
         &self,
         region: &mut Region<'_, F>,
@@ -396,10 +222,10 @@ impl<F: FieldExt, const POSITION: u8> SwapGadget<F, POSITION> {
         core_state: &mut CoreStateInstance,
         execution_step: &ExecutionStep,
     ) -> Result<(), Error> {
-        core_state.global_counter += 1;
+        core_state.global_counter += 4;
         core_state.program_counter += 1;
         core_state.gas_counter += 3;
-        
+
         for idx in 0..2 {
             self.success.words[idx].assign(
                 region,
@@ -420,7 +246,7 @@ mod test {
     use halo2::{arithmetic::FieldExt, dev::MockProver};
     use num::BigUint;
     use pasta_curves::pallas::Base;
-    
+
     macro_rules! try_test_circuit {
         ($execution_steps:expr, $operations:expr, $result:expr) => {{
             let circuit =
@@ -429,11 +255,10 @@ mod test {
             assert_eq!(prover.verify(), $result);
         }};
     }
-    
+
     // TODO: add failure cases
     #[test]
     fn swap2_gadget() {
-        // SWAP2
         try_test_circuit!(
             vec![
                 ExecutionStep {
@@ -471,7 +296,6 @@ mod test {
             ],
             vec![
                 Operation {
-                    // push3
                     gc: 1,
                     target: Target::Stack,
                     is_write: true,
@@ -483,7 +307,6 @@ mod test {
                     ],
                 },
                 Operation {
-                    // push2
                     gc: 2,
                     target: Target::Stack,
                     is_write: true,
@@ -495,7 +318,6 @@ mod test {
                     ]
                 },
                 Operation {
-                    // push1
                     gc: 3,
                     target: Target::Stack,
                     is_write: true,
@@ -506,7 +328,7 @@ mod test {
                         Base::zero(),
                     ]
                 },
-                // swap1 1021 <=> 1022
+                // swap1 1021 <=> 1023
                 Operation {
                     gc: 4,
                     target: Target::Stack,
@@ -555,7 +377,7 @@ mod test {
             Ok(())
         );
     }
-    
+
     #[test]
     fn swap1_gadget() {
         // SWAP1
@@ -588,7 +410,6 @@ mod test {
             ],
             vec![
                 Operation {
-                    // push3
                     gc: 1,
                     target: Target::Stack,
                     is_write: true,
@@ -600,7 +421,6 @@ mod test {
                     ],
                 },
                 Operation {
-                    // push2
                     gc: 2,
                     target: Target::Stack,
                     is_write: true,
