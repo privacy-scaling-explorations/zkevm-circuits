1117a31d80697d9ea61e237defa59370d510e960
CPerezz <c.perezbaro@gmail.com>
2021-10-20 13:56:06 +0200

    Refactor `Absorb` circuit `next_imp`len to 17
    The new len is now updated in the specs and forces to just multiply the
    first 17 values of the state by the `next_inputs` parameter of the
    second `Advice` `Column`.

--- keccak256/src/gates/absorb.rs
+++ keccak256/src/gates/absorb.rs
@@ -3,15 +3,17 @@ use halo2::{
     plonk::{Advice, Column, ConstraintSystem, Error, Expression, Selector},
     poly::Rotation,
 };
-use itertools::Itertools;
 use pasta_curves::arithmetic::FieldExt;
 use std::marker::PhantomData;
+
+/// The number of next_inputs that are used inside the `absorb` circuit.
+pub(crate) const ABSORB_NEXT_INPUTS: usize = 17;
 
 #[derive(Clone, Debug)]
 pub struct AbsorbConfig<F> {
     q_enable: Selector,
     state: [Column<Advice>; 25],
-    next_input: [Column<Advice>; 25],
+    next_input: [Column<Advice>; ABSORB_NEXT_INPUTS],
     _marker: PhantomData<F>,
 }
 
@@ -21,7 +23,7 @@ impl<F: FieldExt> AbsorbConfig<F> {
         q_enable: Selector,
         meta: &mut ConstraintSystem<F>,
         state: [Column<Advice>; 25],
-        next_input: [Column<Advice>; 25],
+        next_input: [Column<Advice>; ABSORB_NEXT_INPUTS],
     ) -> AbsorbConfig<F> {
         // def absorb(state: List[List[int], next_input: List[List[int]):
         //     for x in range(5):
@@ -31,19 +33,17 @@ impl<F: FieldExt> AbsorbConfig<F> {
         //             state[x][y] += 2 * next_input[x][y]
         //     return state
         meta.create_gate("absorb", |meta| {
-            (0..5)
-                .cartesian_product(0..5usize)
-                .map(|(x, y)| {
-                    let val = meta
-                        .query_advice(state[5 * x + y], Rotation::cur())
+            (0..ABSORB_NEXT_INPUTS)
+                .map(|idx| {
+                    let val = meta.query_advice(state[idx], Rotation::cur())
                         + (Expression::Constant(F::from(2))
                             * meta.query_advice(
-                                next_input[5 * x + y],
+                                next_input[idx],
                                 Rotation::cur(),
                             ));
 
                     let next_lane =
-                        meta.query_advice(state[5 * x + y], Rotation::next());
+                        meta.query_advice(state[idx], Rotation::next());
 
                     meta.query_selector(q_enable) * (val - next_lane)
                 })
@@ -79,8 +79,8 @@ impl<F: FieldExt> AbsorbConfig<F> {
         &self,
         region: &mut Region<'_, F>,
         offset: usize,
-        next_input: [F; 25],
-    ) -> Result<[F; 25], Error> {
+        next_input: [F; ABSORB_NEXT_INPUTS],
+    ) -> Result<[F; ABSORB_NEXT_INPUTS], Error> {
         for (idx, lane) in next_input.iter().enumerate() {
             region.assign_advice(
                 || format!("assign next_input {}", idx),
@@ -114,7 +114,7 @@ mod tests {
         #[derive(Default)]
         struct MyCircuit<F> {
             in_state: [F; 25],
-            next_input: [F; 25],
+            next_input: [F; ABSORB_NEXT_INPUTS],
             out_state: [F; 25],
             _marker: PhantomData<F>,
         }
@@ -135,7 +135,8 @@ mod tests {
                     .try_into()
                     .unwrap();
 
-                let next_input: [Column<Advice>; 25] = (0..25)
+                let next_input: [Column<Advice>; ABSORB_NEXT_INPUTS] = (0
+                    ..ABSORB_NEXT_INPUTS)
                     .map(|_| meta.advice_column())
                     .collect::<Vec<_>>()
                     .try_into()
@@ -206,15 +207,20 @@ mod tests {
         let mut next_biguint = StateBigInt::default();
 
         let mut in_state: [pallas::Base; 25] = [pallas::Base::zero(); 25];
-        let mut in_next_input: [pallas::Base; 25] = [pallas::Base::zero(); 25];
+        let mut in_next_input_25: [pallas::Base; 25] =
+            [pallas::Base::zero(); 25];
 
         for (x, y) in (0..5).cartesian_product(0..5) {
             in_biguint[(x, y)] = convert_b2_to_b9(input1[x][y]);
             next_biguint[(x, y)] = convert_b2_to_b9(next_input[x][y]);
             in_state[5 * x + y] = big_uint_to_pallas(&in_biguint[(x, y)]);
-            in_next_input[5 * x + y] =
+            in_next_input_25[5 * x + y] =
                 big_uint_to_pallas(&next_biguint[(x, y)]);
         }
+
+        let mut in_next_input_17 = [pallas::Base::zero(); ABSORB_NEXT_INPUTS];
+        in_next_input_17
+            .copy_from_slice(&in_next_input_25[0..ABSORB_NEXT_INPUTS]);
         let s1_arith = KeccakFArith::absorb(&in_biguint, &next_input);
         let mut out_state: [pallas::Base; 25] = [pallas::Base::zero(); 25];
         for (x, y) in (0..5).cartesian_product(0..5) {
@@ -222,7 +228,7 @@ mod tests {
         }
         let circuit = MyCircuit::<pallas::Base> {
             in_state,
-            next_input: in_next_input,
+            next_input: in_next_input_17,
             out_state,
             _marker: PhantomData,
         };
