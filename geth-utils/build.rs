d6cefb4e9a417c81ff0c38e5f2a8626a25c2ed00
Brecht Devos <Brechtp.Devos@gmail.com>
2021-10-22 13:48:58 +0200

    Show better error message when Go isn't installed (#130)

--- geth-utils/build.rs
+++ geth-utils/build.rs
@@ -1,11 +1,27 @@
 use std::env;
+use std::io::{self, Write};
 
 fn main() {
     let lib_name = "go-geth-utils";
     let out_dir = env::var("OUT_DIR").unwrap();
 
     // Build
-    gobuild::Build::new().file("./lib/lib.go").compile(lib_name);
+    if let Err(e) = gobuild::Build::new()
+        .file("./lib/lib.go")
+        .try_compile(lib_name)
+    {
+        // The error type is private so have to check the error string
+        if format!("{}", e).starts_with("Failed to find tool.") {
+            fail(
+                " Failed to find Go. Please install Go 1.16 or later \
+                following the instructions at https://golang.org/doc/install.
+                On linux it is also likely available as a package."
+                    .to_string(),
+            );
+        } else {
+            fail(format!("{}", e));
+        }
+    }
 
     // Files the lib depends on that should recompile the lib
     let dep_files = vec!["./gethutil/trace.go"];
@@ -16,4 +32,13 @@ fn main() {
     // Link
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static={}", lib_name);
+}
+
+fn fail(message: String) {
+    let _ = writeln!(
+        io::stderr(),
+        "\n\nError while building geth-utils: {}\n\n",
+        message
+    );
+    std::process::exit(1);
 }
