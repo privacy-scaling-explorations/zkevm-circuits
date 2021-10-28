eace7ae541ce0ab3d51f75bec9f81495e5bffe27
ChihChengLiang <chihchengliang@gmail.com>
2021-10-28 22:52:44 +0800

    fix

--- keccak256/src/gates/gate_helpers.rs
+++ keccak256/src/gates/gate_helpers.rs
@@ -1,7 +1,7 @@
 use halo2::circuit::Cell;
 use num_bigint::BigUint;
 use pasta_curves::arithmetic::FieldExt;
-use std::convert::TryInto;
+use std::cmp::min;
 
 #[derive(Debug, Clone)]
 pub struct Lane<F> {
@@ -18,7 +18,12 @@ pub struct BlockCount<F> {
 pub type BlockCount2<F> = (BlockCount<F>, BlockCount<F>);
 
 pub fn biguint_to_f<F: FieldExt>(x: BigUint) -> Option<F> {
-    Option::from(F::from_bytes(x.to_bytes_le()[..=32].try_into().unwrap()))
+    let mut foo = [0; 32];
+    let x_bytes = x.to_bytes_le();
+    for idx in 0..min(x_bytes.len(), 32) {
+        foo[idx] = x_bytes[idx];
+    }
+    Option::from(F::from_bytes(&foo))
 }
 
 pub fn f_to_biguint<F: FieldExt>(x: F) -> Option<BigUint> {
