126b87e0c1a306e6796edba6603e5c4b1904b90c
CPerezz <c.perezbaro@gmail.com>
2021-11-16 12:49:53 +0100

    Add `eth_getBlockByNumber` support

--- bus-mapping/src/rpc.rs
+++ bus-mapping/src/rpc.rs
@@ -1,6 +1,6 @@
 //! Module which contains all the RPC calls that are needed at any point to query a Geth node in order to get a Block, Tx or Trace info.
 
-use crate::eth_types::{Block, Hash, Transaction};
+use crate::eth_types::{Block, Hash, Transaction, U64};
 use crate::Error;
 use ethers_providers::JsonRpcClient;
 
@@ -13,6 +13,37 @@ pub fn serialize<T: serde::Serialize>(t: &T) -> serde_json::Value {
     serde_json::to_value(t).expect("Types never fail to serialize.")
 }
 
+/// Struct used to define the input that you want to provide to the `eth_getBlockByNumber` call as it mixes numbers with string literals.
+#[derive(Debug)]
+pub enum BlockNumber {
+    /// Specific block number
+    Num(U64),
+    /// Earliest block
+    Earliest,
+    /// Latest block
+    Latest,
+    /// Pending block
+    Pending,
+}
+
+impl From<u64> for BlockNumber {
+    fn from(num: u64) -> Self {
+        BlockNumber::Num(U64::from(num))
+    }
+}
+
+impl BlockNumber {
+    /// Serializes a BlockNumber as a [`Value`](serde_json::Value) to be able to throw it into a JSON-RPC request.
+    pub fn serialize(self) -> serde_json::Value {
+        match self {
+            BlockNumber::Num(num) => serialize(&num),
+            BlockNumber::Earliest => serialize(&"earliest"),
+            BlockNumber::Latest => serialize(&"latest"),
+            BlockNumber::Pending => serialize(&"pending"),
+        }
+    }
+}
+
 /// Placeholder structure designed to contain the methods that the BusMapping needs in order to enable Geth queries.
 pub struct GethClient<P: JsonRpcClient>(P);
 
@@ -22,7 +53,7 @@ impl<P: JsonRpcClient> GethClient<P> {
         Self(provider)
     }
 
-    /// Calls `eth_getBlockByHash` via JSON-RPC returning a [`Block`] returning only the list of tx_hashes involved on it.
+    /// Calls `eth_getBlockByHash` via JSON-RPC returning a [`Block`] returning all the block information including it's transaction's details.
     pub async fn get_block_by_hash(
         &self,
         hash: Hash,
@@ -31,6 +62,19 @@ impl<P: JsonRpcClient> GethClient<P> {
         let flag = serialize(&true);
         self.0
             .request("eth_getBlockByHash", [hash, flag])
+            .await
+            .map_err(|e| Error::JSONRpcError(e.into()))
+    }
+
+    /// Calls `eth_getBlockByNumber` via JSON-RPC returning a [`Block`] returning all the block information including it's transaction's details.
+    pub async fn get_block_by_number(
+        &self,
+        block_num: BlockNumber,
+    ) -> Result<Block<Transaction>, Error> {
+        let num = block_num.serialize();
+        let flag = serialize(&true);
+        self.0
+            .request("eth_getBlockByNumber", [num, flag])
             .await
             .map_err(|e| Error::JSONRpcError(e.into()))
     }
@@ -45,14 +89,33 @@ mod rpc_tests {
 
     // The test is ignored as the values used depend on the Geth instance used each time you run the tests.
     // And we can't assume that everyone will have a Geth client synced with mainnet to have unified "test-vectors".
-    #[ignore]
+    //#[ignore]
     #[tokio::test]
     async fn test_get_block_by_hash() {
         let transport = Http::new(Url::parse("http://localhost:8545").unwrap());
 
-        let hash = Hash::from_str("0x60d58d0e417b2b7fab8c74a5efc8292cb5651cd056caad126fb308523b40abc2").unwrap();
+        let hash = Hash::from_str("0x0492a44be5a74a6724bd666d308498c7a830a498f2c574768cbb25d09b4ec948").unwrap();
         let prov = GethClient::new(transport);
-        let block = prov.get_block_by_hash(hash).await.unwrap();
-        assert!(hash == block.transactions[0].hash)
+        let block_by_hash = prov.get_block_by_hash(hash).await.unwrap();
+        assert!(hash == block_by_hash.hash.unwrap());
+    }
+
+    // The test is ignored as the values used depend on the Geth instance used each time you run the tests.
+    // And we can't assume that everyone will have a Geth client synced with mainnet to have unified "test-vectors".
+    //#[ignore]
+    #[tokio::test]
+    async fn test_get_block_by_number() {
+        let transport = Http::new(Url::parse("http://localhost:8545").unwrap());
+
+        let hash = Hash::from_str("0x0492a44be5a74a6724bd666d308498c7a830a498f2c574768cbb25d09b4ec948").unwrap();
+        let prov = GethClient::new(transport);
+        let block_by_num_latest =
+            prov.get_block_by_number(BlockNumber::Latest).await.unwrap();
+        assert!(hash == block_by_num_latest.hash.unwrap());
+        let block_by_num = prov.get_block_by_number(1u64.into()).await.unwrap();
+        assert!(
+            block_by_num.transactions[0].hash
+                == block_by_num_latest.transactions[0].hash
+        );
     }
 }
