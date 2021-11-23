ec6dc0947a6a96a10c6d2c195e34bec78bed306c
therealyingtong <yingtong@z.cash>
2021-11-23 10:42:14 -0500

    iota_b9: Introduce q_last, q_not_last selectors.
    These are used to differentiate between gates for the steady state,
    and gates for the final round (where an is_mixing flag is witnessed
    by the prover).
    
    In the final round, q_last * flag is used as a composite selector.

--- keccak256/src/gates/iota_b9.rs
+++ keccak256/src/gates/iota_b9.rs
@@ -6,7 +6,8 @@ use halo2::plonk::Instance;
 use halo2::{
     circuit::Region,
     plonk::{
-        Advice, Column, ConstraintSystem, Error, Expression, VirtualCells,
+        Advice, Column, ConstraintSystem, Error, Expression, Selector,
+        VirtualCells,
     },
     poly::Rotation,
 };
@@ -15,7 +16,8 @@ use std::marker::PhantomData;
 
 #[derive(Clone, Debug)]
 pub struct IotaB9Config<F> {
-    q_enable: Expression<F>,
+    q_not_last: Selector,
+    q_last: Selector,
     state: [Column<Advice>; 25],
     pub(crate) round_ctant_b9: Column<Advice>,
     pub(crate) round_constants: Column<Instance>,
@@ -25,32 +27,49 @@ pub struct IotaB9Config<F> {
 impl<F: FieldExt> IotaB9Config<F> {
     // We assume state is recieved in base-9.
     pub fn configure(
-        q_enable_fn: impl FnOnce(&mut VirtualCells<'_, F>) -> Expression<F>,
         meta: &mut ConstraintSystem<F>,
         state: [Column<Advice>; 25],
         round_ctant_b9: Column<Advice>,
         round_constants: Column<Instance>,
     ) -> IotaB9Config<F> {
-        let mut q_enable = Expression::Constant(F::zero());
+        let q_not_last = meta.selector();
+        let q_last = meta.selector();
+
         // Enable copy constraints over PI and the Advices.
         meta.enable_equality(round_ctant_b9.into());
         meta.enable_equality(round_constants.into());
-        meta.create_gate("iota_b9", |meta| {
-            // def iota_b9(state: List[List[int], round_constant_base9: int):
-            //     d = round_constant_base9
-            //     # state[0][0] has 2*a + b + 3*c already, now add 2*d to make
-            // it 2*a + b + 3*c + 2*d     # coefficient in 0~8
-            //     state[0][0] += 2*d
-            //     return state
-            q_enable = q_enable_fn(meta);
+
+        // def iota_b9(state: List[List[int], round_constant_base9: int):
+        //     d = round_constant_base9
+        //     # state[0][0] has 2*a + b + 3*c already, now add 2*d to make it
+        // 2*a + b + 3*c + 2*d     # coefficient in 0~8
+        //     state[0][0] += 2*d
+        //     return state
+        meta.create_gate("iota_b9 in steady state", |meta| {
+            let q_enable = meta.query_selector(q_not_last);
             let state_00 = meta.query_advice(state[0], Rotation::cur())
                 + (Expression::Constant(F::from(2))
                     * meta.query_advice(round_ctant_b9, Rotation::cur()));
             let next_lane = meta.query_advice(state[0], Rotation::next());
             vec![q_enable.clone() * (state_00 - next_lane)]
         });
+
+        meta.create_gate("iota_b9 in final round", |meta| {
+            let q_enable = {
+                let flag = meta.query_advice(round_ctant_b9, Rotation::next());
+                meta.query_selector(q_last) * flag
+            };
+
+            let state_00 = meta.query_advice(state[0], Rotation::cur())
+                + (Expression::Constant(F::from(2))
+                    * meta.query_advice(round_ctant_b9, Rotation::cur()));
+            let next_lane = meta.query_advice(state[0], Rotation::next());
+            vec![q_enable.clone() * (state_00 - next_lane)]
+        });
+
         IotaB9Config {
-            q_enable,
+            q_not_last,
+            q_last,
             state,
             round_ctant_b9,
             round_constants,
@@ -59,7 +78,7 @@ impl<F: FieldExt> IotaB9Config<F> {
     }
 
     /// Doc this
-    pub fn assing_states_and_rc(
+    pub fn not_last_round(
         &self,
         region: &mut Region<'_, F>,
         mut offset: usize,
@@ -67,6 +86,9 @@ impl<F: FieldExt> IotaB9Config<F> {
         out_state: [F; 25],
         absolute_row: usize,
     ) -> Result<(), Error> {
+        // Enable `q_not_last`.
+        self.q_not_last.enable(region, offset)?;
+
         // Assign state
         self.assign_state(region, offset, in_state)?;
 
@@ -78,8 +100,10 @@ impl<F: FieldExt> IotaB9Config<F> {
         self.assign_state(region, offset, out_state)
     }
 
-    /// Doc this
-    pub fn copy_state_flag_and_assing_rc(
+    /// Assignment for iota_b9 in the context of the final round, where
+    /// the prover must witness an `is_mixing` flag in the round_ctant_b9
+    /// advice column.
+    pub fn last_round(
         &self,
         region: &mut Region<'_, F>,
         mut offset: usize,
@@ -88,55 +112,54 @@ impl<F: FieldExt> IotaB9Config<F> {
         absolute_row: usize,
         flag: (Cell, F),
     ) -> Result<(), Error> {
+        // Copies the `[(Cell,F);25]` to the `state` Advice column.
+        let copy_state = |region: &mut Region<'_, F>,
+                          offset: usize,
+                          in_state: [(Cell, F); 25]|
+         -> Result<(), Error> {
+            for (idx, (cell, value)) in in_state.iter().enumerate() {
+                let new_cell = region.assign_advice(
+                    || format!("copy in_state {}", idx),
+                    self.state[idx],
+                    offset,
+                    || Ok(*value),
+                )?;
+
+                region.constrain_equal(*cell, new_cell)?;
+            }
+
+            Ok(())
+        };
+
+        // Copies the `is_mixing` flag to the `round_ctant_b9` Advice column.
+        let copy_flag = |region: &mut Region<'_, F>,
+                         offset: usize,
+                         flag: (Cell, F)|
+         -> Result<(), Error> {
+            let obtained_cell = region.assign_advice(
+                || format!("assign is_mixing flag {:?}", flag.1),
+                self.round_ctant_b9,
+                offset,
+                || Ok(flag.1),
+            )?;
+            region.constrain_equal(flag.0, obtained_cell)?;
+
+            Ok(())
+        };
+
+        // Enable `q_last`.
+        self.q_last.enable(region, offset)?;
+
         // Copy state at offset + 0
-        self.copy_state(region, offset, state)?;
+        copy_state(region, offset, state)?;
         // Assign round_ctant at offset + 0.
         self.assign_round_ctant_b9(region, offset, absolute_row)?;
 
         offset += 1;
         // Copy flag at `round_ctant_b9` at offset + 1
-        self.copy_flag(region, offset, flag)?;
+        copy_flag(region, offset, flag)?;
         // Assign out state at offset + 1
         self.assign_state(region, offset, out_state)
-    }
-
-    /// Copies the `[(Cell,F);25]` to the `state` Advice column.
-    fn copy_state(
-        &self,
-        region: &mut Region<'_, F>,
-        offset: usize,
-        in_state: [(Cell, F); 25],
-    ) -> Result<(), Error> {
-        for (idx, (cell, value)) in in_state.iter().enumerate() {
-            let new_cell = region.assign_advice(
-                || format!("copy in_state {}", idx),
-                self.state[idx],
-                offset,
-                || Ok(*value),
-            )?;
-
-            region.constrain_equal(*cell, new_cell)?;
-        }
-
-        Ok(())
-    }
-
-    /// Copies the `is_mixing` flag to the `round_ctant_b9` Advice column.
-    fn copy_flag(
-        &self,
-        region: &mut Region<'_, F>,
-        offset: usize,
-        flag: (Cell, F),
-    ) -> Result<(), Error> {
-        let obtained_cell = region.assign_advice(
-            || format!("assign is_mixing flag {:?}", flag.1),
-            self.round_ctant_b9,
-            offset,
-            || Ok(flag.1),
-        )?;
-        region.constrain_equal(flag.0, obtained_cell)?;
-
-        Ok(())
     }
 
     // Assign `[F;25]` at `state` `Advice` column at the provided offset.
@@ -198,7 +221,7 @@ mod tests {
     use std::marker::PhantomData;
 
     #[test]
-    fn test_iota_b9_gate_with_flag() {
+    fn test_iota_b9_gate_last_round() {
         #[derive(Default)]
         struct MyCircuit<F> {
             in_state: [F; 25],
@@ -239,7 +262,6 @@ mod tests {
                 // the Mixing step, we make q_enable query
                 // the round_ctant_b9 at `Rotation::next`.
                 IotaB9Config::configure(
-                    |meta| meta.query_advice(round_ctant_b9, Rotation::next()),
                     meta,
                     state,
                     round_ctant_b9,
@@ -258,9 +280,9 @@ mod tests {
                 layouter.assign_region(
                     || "Wittnes & assignation",
                     |mut region| {
-                        // Witness `is_missing` flag
+                        // Witness `is_mixing` flag
                         let cell = region.assign_advice(
-                            || "witness is_missing",
+                            || "witness is_mixing",
                             config.round_ctant_b9,
                             offset + 1,
                             || Ok(val),
@@ -284,7 +306,7 @@ mod tests {
                         };
 
                         // Assign `in_state`, `out_state`, round and flag
-                        config.copy_state_flag_and_assing_rc(
+                        config.last_round(
                             &mut region,
                             offset,
                             in_state,
@@ -313,39 +335,56 @@ mod tests {
             in_state[5 * x + y] = big_uint_to_pallas(&in_biguint[(x, y)]);
         }
 
-        // Test for all rounds
-        for round_ctant in 0..25 {
-            // Compute out state
-            let s1_arith = KeccakFArith::iota_b9(
-                &in_biguint,
-                ROUND_CONSTANTS[round_ctant],
-            );
-            let out_state =
-                state_bigint_to_pallas::<pallas::Base, 25>(s1_arith);
+        // Test for final rounds
+        // Compute out state
 
-            let circuit = MyCircuit::<pallas::Base> {
-                in_state,
-                out_state,
-                round_ctant,
-                flag: true,
-                _marker: PhantomData,
-            };
+        let round_ctant = ROUND_CONSTANTS[PERMUTATION - 1];
+        let s1_arith = KeccakFArith::iota_b9(&in_biguint, round_ctant);
+        let out_state = state_bigint_to_pallas::<pallas::Base, 25>(s1_arith);
 
-            let constants: Vec<pallas::Base> = ROUND_CONSTANTS
-                .iter()
-                .map(|num| big_uint_to_pallas(&convert_b2_to_b9(*num)))
-                .collect();
+        let constants: Vec<pallas::Base> = ROUND_CONSTANTS
+            .iter()
+            .map(|num| big_uint_to_pallas(&convert_b2_to_b9(*num)))
+            .collect();
 
-            let prover =
-                MockProver::<pallas::Base>::run(9, &circuit, vec![constants])
-                    .unwrap();
+        // (flag = 1)
+        let circuit = MyCircuit::<pallas::Base> {
+            in_state,
+            out_state,
+            round_ctant: PERMUTATION - 1,
+            flag: true,
+            _marker: PhantomData,
+        };
 
-            assert_eq!(prover.verify(), Ok(()));
-        }
+        let prover = MockProver::<pallas::Base>::run(
+            9,
+            &circuit,
+            vec![constants.clone()],
+        )
+        .unwrap();
+
+        assert_eq!(prover.verify(), Ok(()));
+
+        // (flag = 0)
+        let circuit = MyCircuit::<pallas::Base> {
+            in_state,
+            // Use a nonsensical out_state to verify that the gate is not
+            // checked.
+            out_state: in_state,
+            round_ctant: PERMUTATION - 1,
+            flag: false,
+            _marker: PhantomData,
+        };
+
+        let prover =
+            MockProver::<pallas::Base>::run(9, &circuit, vec![constants])
+                .unwrap();
+
+        assert_eq!(prover.verify(), Ok(()));
     }
 
     #[test]
-    fn test_iota_b9_gate_with_selector() {
+    fn test_iota_b9_gate_not_last_round() {
         #[derive(Default)]
         struct MyCircuit<F> {
             in_state: [F; 25],
@@ -354,14 +393,8 @@ mod tests {
             _marker: PhantomData<F>,
         }
 
-        #[derive(Clone)]
-        struct MyConfig<F> {
-            q_enable: Selector,
-            iota_b9_config: IotaB9Config<F>,
-        }
-
         impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
-            type Config = MyConfig<F>;
+            type Config = IotaB9Config<F>;
             type FloorPlanner = SimpleFloorPlanner;
 
             fn without_witnesses(&self) -> Self {
@@ -369,7 +402,6 @@ mod tests {
             }
 
             fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
-                let q_enable = meta.selector();
                 let state: [Column<Advice>; 25] = (0..25)
                     .map(|_| meta.advice_column())
                     .collect::<Vec<_>>()
@@ -380,16 +412,12 @@ mod tests {
                 // instance column
                 let round_ctants = meta.instance_column();
 
-                MyConfig {
-                    q_enable,
-                    iota_b9_config: IotaB9Config::configure(
-                        |meta| meta.query_selector(q_enable),
-                        meta,
-                        state,
-                        round_ctant_b9,
-                        round_ctants,
-                    ),
-                }
+                IotaB9Config::configure(
+                    meta,
+                    state,
+                    round_ctant_b9,
+                    round_ctants,
+                )
             }
 
             fn synthesize(
@@ -402,10 +430,8 @@ mod tests {
                 layouter.assign_region(
                     || "assign input state",
                     |mut region| {
-                        // Enable selector at offset = 0
-                        config.q_enable.enable(&mut region, offset)?;
                         // Start IotaB9 config without copy at offset = 0
-                        config.iota_b9_config.assing_states_and_rc(
+                        config.not_last_round(
                             &mut region,
                             offset,
                             self.in_state,
