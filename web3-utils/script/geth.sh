6fa41630be78549c8102220de648caf564ddde0b
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-02 14:46:44 +0900

    integrate script

--- web3-utils/script/geth.sh
+++ web3-utils/script/geth.sh
@@ -1,3 +1,7 @@
+docker-compose build
+cargo test --no-run -- --ignored
+docker-compose up -d deployer geth
+
 GethBlock=`curl localhost:8545 -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'`
 
 while [[ $GethBlock != '{"jsonrpc":"2.0","id":1,"result":"0x2"}' ]]
