074c178e7bb5f89e2a1f5b2da1d78147c39f4a70
ChihChengLiang <chihchengliang@gmail.com>
2021-12-02 19:38:28 +0800

    add from base9 to base2 and base13 lookup

--- keccak256/src/gates/tables.rs
+++ keccak256/src/gates/tables.rs
@@ -1,4 +1,4 @@
-use crate::arith_helpers::{convert_b13_coef, B13, B2, B9};
+use crate::arith_helpers::{convert_b13_coef, convert_b9_coef, B13, B2, B9};
 use crate::common::LANE_SIZE;
 use crate::gates::rho_helpers::{get_block_count, BASE_NUM_OF_CHUNKS};
 use halo2::{
@@ -189,7 +189,7 @@ impl<F: FieldExt> SpecialChunkTableConfig<F> {
     }
 }
 
-const NUM_OF_CHUNKS: usize = 16;
+const NUM_OF_BINARY_CHUNKS: usize = 16;
 
 #[derive(Debug, Clone)]
 pub struct FromBinaryTableConfig<F> {
@@ -208,7 +208,7 @@ impl<F: FieldExt> FromBinaryTableConfig<F> {
             || "2 -> (9 and 13)",
             |mut table| {
                 // Iterate over all possible binary values of size 16
-                for (i, b2_chunks) in (0..NUM_OF_CHUNKS)
+                for (i, b2_chunks) in (0..NUM_OF_BINARY_CHUNKS)
                     .map(|_| 0..B2)
                     .multi_cartesian_product()
                     .enumerate()
@@ -282,6 +282,100 @@ impl<F: FieldExt> FromBinaryTableConfig<F> {
                 (q_enable.clone() * base2_coef, config.base2),
                 (q_enable.clone() * base9_coef, config.base9),
                 (q_enable.clone() * base13_coef, config.base13),
+            ]
+        });
+        config
+    }
+}
+
+const NUM_OF_B9_CHUNKS: usize = 5;
+
+#[derive(Debug, Clone)]
+pub struct FromBase9TableConfig<F> {
+    base9: TableColumn,
+    base13: TableColumn,
+    base2: TableColumn,
+    _marker: PhantomData<F>,
+}
+
+impl<F: FieldExt> FromBase9TableConfig<F> {
+    pub(crate) fn load(
+        &self,
+        layouter: &mut impl Layouter<F>,
+    ) -> Result<(), Error> {
+        layouter.assign_table(
+            || "9 -> (2 and 13)",
+            |mut table| {
+                // Iterate over all possible base 9 values of size 5
+                for (i, b9_chunks) in (0..NUM_OF_B9_CHUNKS)
+                    .map(|_| 0..B9)
+                    .multi_cartesian_product()
+                    .enumerate()
+                {
+                    table.assign_cell(
+                        || "base 9",
+                        self.base9,
+                        i,
+                        || {
+                            Ok(F::from(
+                                b9_chunks
+                                    .iter()
+                                    .fold(0, |acc, x| acc * B9 + *x),
+                            ))
+                        },
+                    )?;
+                    table.assign_cell(
+                        || "base 13",
+                        self.base13,
+                        i,
+                        || {
+                            Ok(F::from(b9_chunks.iter().fold(0, |acc, x| {
+                                acc * B13 + convert_b9_coef(*x)
+                            })))
+                        },
+                    )?;
+                    table.assign_cell(
+                        || "base 2",
+                        self.base2,
+                        i,
+                        || {
+                            Ok(F::from(b9_chunks.iter().fold(0, |acc, x| {
+                                acc * B2 + convert_b9_coef(*x)
+                            })))
+                        },
+                    )?;
+                }
+                Ok(())
+            },
+        )
+    }
+
+    pub(crate) fn configure(
+        meta: &mut ConstraintSystem<F>,
+        q_enable: Selector,
+        base9_coef: Column<Advice>,
+        base13_coef: Column<Advice>,
+        base2_coef: Column<Advice>,
+        fixed: [TableColumn; 3],
+    ) -> Self {
+        let config = Self {
+            base2: fixed[0],
+            base9: fixed[1],
+            base13: fixed[2],
+            _marker: PhantomData,
+        };
+
+        meta.lookup(|meta| {
+            let q_enable = meta.query_selector(q_enable);
+
+            let base9_coef = meta.query_advice(base9_coef, Rotation::cur());
+            let base13_coef = meta.query_advice(base13_coef, Rotation::cur());
+            let base2_coef = meta.query_advice(base2_coef, Rotation::cur());
+
+            vec![
+                (q_enable.clone() * base9_coef, config.base9),
+                (q_enable.clone() * base13_coef, config.base13),
+                (q_enable.clone() * base2_coef, config.base2),
             ]
         });
         config
