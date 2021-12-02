83e82bd1f3b54dc281422b2d4ff065d06545b121
ChihChengLiang <chihchengliang@gmail.com>
2021-12-02 19:16:06 +0800

    add from binary to base9 and base13

--- keccak256/src/gates/tables.rs
+++ keccak256/src/gates/tables.rs
@@ -1,4 +1,4 @@
-use crate::arith_helpers::*;
+use crate::arith_helpers::{convert_b13_coef, B13, B2, B9};
 use crate::common::LANE_SIZE;
 use crate::gates::rho_helpers::{get_block_count, BASE_NUM_OF_CHUNKS};
 use halo2::{
@@ -183,6 +183,105 @@ impl<F: FieldExt> SpecialChunkTableConfig<F> {
             vec![
                 (q_enable.clone() * last_chunk_advice, config.last_chunk),
                 (q_enable * output_coef_advice, config.output_coef),
+            ]
+        });
+        config
+    }
+}
+
+const NUM_OF_CHUNKS: usize = 16;
+
+#[derive(Debug, Clone)]
+pub struct FromBinaryTableConfig<F> {
+    base2: TableColumn,
+    base9: TableColumn,
+    base13: TableColumn,
+    _marker: PhantomData<F>,
+}
+
+impl<F: FieldExt> FromBinaryTableConfig<F> {
+    pub(crate) fn load(
+        &self,
+        layouter: &mut impl Layouter<F>,
+    ) -> Result<(), Error> {
+        layouter.assign_table(
+            || "2 -> (9 and 13)",
+            |mut table| {
+                // Iterate over all possible binary values of size 16
+                for (i, b2_chunks) in (0..NUM_OF_CHUNKS)
+                    .map(|_| 0..B2)
+                    .multi_cartesian_product()
+                    .enumerate()
+                {
+                    table.assign_cell(
+                        || "base 2",
+                        self.base2,
+                        i,
+                        || {
+                            Ok(F::from(
+                                b2_chunks
+                                    .iter()
+                                    .fold(0, |acc, x| acc * B2 + *x),
+                            ))
+                        },
+                    )?;
+
+                    table.assign_cell(
+                        || "base 9",
+                        self.base9,
+                        i,
+                        || {
+                            Ok(F::from(
+                                b2_chunks
+                                    .iter()
+                                    .fold(0, |acc, x| acc * B9 + *x),
+                            ))
+                        },
+                    )?;
+                    table.assign_cell(
+                        || "base 13",
+                        self.base13,
+                        i,
+                        || {
+                            Ok(F::from(
+                                b2_chunks
+                                    .iter()
+                                    .fold(0, |acc, x| acc * B13 + *x),
+                            ))
+                        },
+                    )?;
+                }
+                Ok(())
+            },
+        )
+    }
+
+    pub(crate) fn configure(
+        meta: &mut ConstraintSystem<F>,
+        q_enable: Selector,
+        base2_coef: Column<Advice>,
+        base13_coef: Column<Advice>,
+        base9_coef: Column<Advice>,
+        fixed: [TableColumn; 3],
+    ) -> Self {
+        let config = Self {
+            base2: fixed[0],
+            base9: fixed[1],
+            base13: fixed[2],
+            _marker: PhantomData,
+        };
+
+        meta.lookup(|meta| {
+            let q_enable = meta.query_selector(q_enable);
+
+            let base2_coef = meta.query_advice(base2_coef, Rotation::cur());
+            let base9_coef = meta.query_advice(base9_coef, Rotation::cur());
+            let base13_coef = meta.query_advice(base13_coef, Rotation::cur());
+
+            vec![
+                (q_enable.clone() * base2_coef, config.base2),
+                (q_enable.clone() * base9_coef, config.base9),
+                (q_enable.clone() * base13_coef, config.base13),
             ]
         });
         config
