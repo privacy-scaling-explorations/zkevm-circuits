4b267967736e06472ba88d89258061742b62b84a
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-02 15:17:06 +0900

    update script

--- web3-utils/script/geth.sh
+++ web3-utils/script/geth.sh
@@ -1,15 +1,15 @@
 docker-compose build
 docker pull ethereum/client-go:stable
-cargo test --no-run -- --ignored
 docker-compose up -d deployer geth
+cargo test --no-run -- --ignored
+cd bus-mapping
 
 GethBlock=`curl localhost:8545 -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'`
 
-while [[ $GethBlock != '{"jsonrpc":"2.0","id":1,"result":"0x2"}' ]]
+while [ $GethBlock != '{"jsonrpc":"2.0","id":1,"result":"0x2"}' ]
 do
     sleep 3
     GethBlock=`curl localhost:8545 -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'`
 done
 
-cd bus-mapping
 cargo test -- --ignored
