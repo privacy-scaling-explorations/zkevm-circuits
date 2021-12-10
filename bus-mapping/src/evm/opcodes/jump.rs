5abc76d0d81e7a3f2aceee30d5e7ca71de49b69a
HAOYUatHZ <haoyu@protonmail.com>
2021-12-10 12:25:35 +0800

    add jump_opcode_impl test

--- bus-mapping/src/evm/opcodes/jump.rs
+++ bus-mapping/src/evm/opcodes/jump.rs
@@ -30,3 +30,84 @@ impl Opcode for Jump {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod jump_tests {
+    use super::*;
+    use crate::{
+        bytecode,
+        circuit_input_builder::{
+            CircuitInputBuilder, ExecStep, Transaction, TransactionContext,
+        },
+        eth_types::Word,
+        evm::StackAddress,
+        mock,
+    };
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn jump_opcode_impl() -> Result<(), Error> {
+        let destination = 35;
+
+        let mut code = bytecode! {
+            PUSH32(destination)
+            #[start]
+            JUMP
+        };
+        for _ in 0..(destination - 34) {
+            code.write(0);
+        }
+        code.append(&bytecode! {
+            JUMPDEST
+            STOP
+        });
+
+        // Get the execution steps from the external tracer
+        let block =
+            mock::BlockData::new_single_tx_trace_code_at_start(&code).unwrap();
+
+        let mut builder = CircuitInputBuilder::new(
+            block.eth_block.clone(),
+            block.block_ctants.clone(),
+        );
+        builder.handle_tx(&block.eth_tx, &block.geth_trace).unwrap();
+
+        let mut test_builder = CircuitInputBuilder::new(
+            block.eth_block,
+            block.block_ctants.clone(),
+        );
+        let mut tx = Transaction::new(&block.eth_tx);
+        let mut tx_ctx = TransactionContext::new(&block.eth_tx);
+
+        // Generate step corresponding to JUMP
+        let mut step = ExecStep::new(
+            &block.geth_trace.struct_logs[0],
+            0,
+            test_builder.block_ctx.gc,
+            0,
+        );
+        let mut state_ref =
+            test_builder.state_ref(&mut tx, &mut tx_ctx, &mut step);
+
+        // Add the last Stack read
+        state_ref.push_op(StackOp::new(
+            RW::READ,
+            StackAddress::from(1023),
+            Word::from(destination),
+        ));
+
+        tx.steps_mut().push(step);
+        test_builder.block.txs_mut().push(tx);
+
+        // Compare first step bus mapping instance
+        assert_eq!(
+            builder.block.txs()[0].steps()[0].bus_mapping_instance,
+            test_builder.block.txs()[0].steps()[0].bus_mapping_instance,
+        );
+
+        // Compare containers
+        assert_eq!(builder.block.container, test_builder.block.container);
+
+        Ok(())
+    }
+}
