cf259307d28aeaf8767bee2ea3bbd852dd8fd7fd
ChihChengLiang <chihchengliang@gmail.com>
2021-12-12 21:26:07 +0800

    add from base 9 state config

--- keccak256/src/gates/state_conversion.rs
+++ keccak256/src/gates/state_conversion.rs
@@ -4,25 +4,23 @@ use halo2::{
 };
 
 use crate::gates::base_conversion::BaseConversionConfig;
-use crate::gates::tables::FromBinaryTableConfig;
+use crate::gates::tables::BaseInfo;
 use pairing::arithmetic::FieldExt;
 use std::convert::TryInto;
 
-struct FromBinaryState<F> {
-    table: FromBinaryTableConfig<F>,
+#[derive(Debug, Clone)]
+pub struct StateBaseConversion<F> {
+    bi: BaseInfo<F>,
     bccs: [BaseConversionConfig<F>; 25],
     state: [Column<Advice>; 25],
-    output_b9: bool,
 }
 
-impl<F: FieldExt> FromBinaryState<F> {
-    fn configure(
+impl<F: FieldExt> StateBaseConversion<F> {
+    pub fn configure(
         meta: &mut ConstraintSystem<F>,
         state: [Column<Advice>; 25],
-        table: FromBinaryTableConfig<F>,
-        output_b9: bool,
+        bi: BaseInfo<F>,
     ) -> Self {
-        let bi = table.get_base_info(output_b9);
         let bccs: [BaseConversionConfig<F>; 25] = state
             .iter()
             .map(|&lane| {
@@ -32,12 +30,7 @@ impl<F: FieldExt> FromBinaryState<F> {
             .try_into()
             .unwrap();
 
-        Self {
-            table,
-            bccs,
-            state,
-            output_b9,
-        }
+        Self { bi, bccs, state }
     }
 
     pub fn assign_region(
@@ -57,5 +50,137 @@ impl<F: FieldExt> FromBinaryState<F> {
         let output_state = output_state?;
         let output_state: [F; 25] = output_state.try_into().unwrap();
         Ok(output_state)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::arith_helpers::convert_b2_to_b13;
+    use crate::gates::{
+        gate_helpers::biguint_to_f, tables::FromBinaryTableConfig,
+    };
+    use halo2::{
+        circuit::{Layouter, SimpleFloorPlanner},
+        dev::MockProver,
+        plonk::{Advice, Circuit, Column, ConstraintSystem, Error},
+    };
+    use pairing::arithmetic::FieldExt;
+    use pairing::bn256::Fr as Fp;
+    use pretty_assertions::assert_eq;
+    #[test]
+    fn test_state_base_conversion() {
+        // We have to use a MyConfig because:
+        // We need to load the table
+        #[derive(Debug, Clone)]
+        struct MyConfig<F> {
+            state: [Column<Advice>; 25],
+            table: FromBinaryTableConfig<F>,
+            conversion: StateBaseConversion<F>,
+        }
+        impl<F: FieldExt> MyConfig<F> {
+            pub fn configure(meta: &mut ConstraintSystem<F>) -> Self {
+                let table = FromBinaryTableConfig::configure(meta);
+                let state: [Column<Advice>; 25] = (0..25)
+                    .map(|_| meta.advice_column())
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap();
+                let bi = table.get_base_info(false);
+                let conversion =
+                    StateBaseConversion::configure(meta, state, bi);
+                Self {
+                    state,
+                    table,
+                    conversion,
+                }
+            }
+
+            pub fn load(
+                &self,
+                layouter: &mut impl Layouter<F>,
+            ) -> Result<(), Error> {
+                self.table.load(layouter)
+            }
+
+            pub fn assign_region(
+                &self,
+                layouter: &mut impl Layouter<F>,
+                input_state: [F; 25],
+            ) -> Result<[F; 25], Error> {
+                let output_state =
+                    self.conversion.assign_region(layouter, input_state)?;
+                Ok(output_state)
+            }
+        }
+
+        #[derive(Default)]
+        struct MyCircuit<F> {
+            in_state: [F; 25],
+            out_state: [F; 25],
+        }
+        impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
+            type Config = MyConfig<F>;
+            type FloorPlanner = SimpleFloorPlanner;
+
+            fn without_witnesses(&self) -> Self {
+                Self::default()
+            }
+
+            fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+                Self::Config::configure(meta)
+            }
+
+            fn synthesize(
+                &self,
+                config: Self::Config,
+                mut layouter: impl Layouter<F>,
+            ) -> Result<(), Error> {
+                config.load(&mut layouter)?;
+                let out_state =
+                    config.assign_region(&mut layouter, self.in_state)?;
+                assert_eq!(out_state, self.out_state);
+                Ok(())
+            }
+        }
+        let in_state: [[u64; 5]; 5] = [
+            [4398046511105, 8, 2, 268436480, 2305844108725321728],
+            [
+                17592186044416,
+                52776560230400,
+                544,
+                68719493120,
+                2199023255552,
+            ],
+            [
+                4398046543872,
+                1152921504606846984,
+                262144,
+                1024,
+                1099511627780,
+            ],
+            [0, 52776558133248, 514, 268451840, 2305845208236949504],
+            [17592186077184, 1152921504608944128, 262176, 68719476736, 4],
+        ];
+
+        let in_state_flat = in_state.iter().flatten().collect::<Vec<_>>();
+        let in_state: [Fp; 25] = in_state_flat
+            .iter()
+            .map(|&x| Fp::from(*x))
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+        let out_state: [Fp; 25] = in_state_flat
+            .iter()
+            .map(|&x| biguint_to_f::<Fp>(&convert_b2_to_b13(*x)).unwrap())
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+        let circuit = MyCircuit::<Fp> {
+            in_state,
+            out_state,
+        };
+        let prover = MockProver::<Fp>::run(17, &circuit, vec![]).unwrap();
+        assert_eq!(prover.verify(), Ok(()));
     }
 }
