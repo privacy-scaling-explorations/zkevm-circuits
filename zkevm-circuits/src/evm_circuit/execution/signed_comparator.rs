87f6fbd7b370eef693d3bbd86b73d602e69d9904
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-01-11 10:10:45 +0800

    fix: use LtGadget, ignore equality case for SLT/SGT

--- zkevm-circuits/src/evm_circuit/execution/signed_comparator.rs
+++ zkevm-circuits/src/evm_circuit/execution/signed_comparator.rs
@@ -8,7 +8,7 @@ use crate::{
                 ConstraintBuilder, StepStateTransition, Transition,
             },
             from_bytes,
-            math_gadget::{ComparisonGadget, IsEqualGadget},
+            math_gadget::{IsEqualGadget, LtGadget},
             select, Word,
         },
         witness::{Block, Call, ExecStep, Transaction},
@@ -28,10 +28,10 @@ pub(crate) struct SignedComparatorGadget<F> {
     a: Word<F>,
     b: Word<F>,
 
-    sign_check_a: ComparisonGadget<F, 1>,
-    sign_check_b: ComparisonGadget<F, 1>,
-    comparison_lo: ComparisonGadget<F, 16>,
-    comparison_hi: ComparisonGadget<F, 16>,
+    sign_check_a: LtGadget<F, 1>,
+    sign_check_b: LtGadget<F, 1>,
+    lt_lo: LtGadget<F, 16>,
+    lt_hi: LtGadget<F, 16>,
 
     is_sgt: IsEqualGadget<F>,
 }
@@ -58,50 +58,40 @@ impl<F: FieldExt> ExecutionGadget<F> for SignedComparatorGadget<F> {
         // of the absolute value in the rest of the 255 bits. This means the
         // number is negative if the most significant cell >= 128
         // (0b10000000).
-        let sign_check_a = ComparisonGadget::construct(
-            cb,
-            from_bytes::expr(&a.cells[0..1]),
-            128u8.expr(),
-        );
-        let sign_check_b = ComparisonGadget::construct(
-            cb,
-            from_bytes::expr(&b.cells[0..1]),
-            128u8.expr(),
-        );
+        let sign_check_a =
+            LtGadget::construct(cb, a.cells[0].expr(), 128u8.expr());
+        let sign_check_b =
+            LtGadget::construct(cb, b.cells[0].expr(), 128u8.expr());
 
         // sign_check_a_lt expression implies a is positive since its MSB < 2**7
         // sign_check_b_lt expression implies b is positive since its MSB < 2**7
-        let (sign_check_a_lt, _sign_check_a_eq) = sign_check_a.expr();
-        let (sign_check_b_lt, _sign_check_b_eq) = sign_check_b.expr();
+        let sign_check_a_lt = sign_check_a.expr();
+        let sign_check_b_lt = sign_check_b.expr();
 
         // We require the comparison check only for the cases where:
         // (a < 0 && b < 0) || (a > 0 && b > 0).
         // We ignore the equality expression since we only care about the LT
         // check.
-        let comparison_lo = ComparisonGadget::construct(
+        let lt_lo = LtGadget::construct(
             cb,
             from_bytes::expr(&a.cells[0..16]),
             from_bytes::expr(&b.cells[0..16]),
         );
-        let comparison_hi = ComparisonGadget::construct(
+        let lt_hi = LtGadget::construct(
             cb,
             from_bytes::expr(&a.cells[16..32]),
             from_bytes::expr(&b.cells[16..32]),
         );
-        let (lt_lo, _eq_lo) = comparison_lo.expr();
-        let (lt_hi, eq_hi) = comparison_hi.expr();
+        let a_lt_b_lo = lt_lo.expr();
+        let a_lt_b_hi = lt_hi.expr();
 
         // If a and b both are positive, we do a standard comparison check. If
         // both a and b are negative, we invert those outcomes.
         let a_b_positive = sign_check_a_lt.clone() * sign_check_b_lt.clone();
         let a_lt_b = select::expr(
             a_b_positive,
-            select::expr(
-                lt_hi.clone(),
-                1u8.expr(),
-                eq_hi.clone() * lt_lo.clone(),
-            ),
-            select::expr(lt_hi, 0u8.expr(), 1u8.expr() - (eq_hi * lt_lo)),
+            select::expr(a_lt_b_hi.clone(), 1u8.expr(), a_lt_b_lo.clone()),
+            select::expr(a_lt_b_hi, 0u8.expr(), 1u8.expr() - a_lt_b_lo),
         );
 
         // Add a trivial selector: if only a or only b is negative we have the
@@ -145,8 +135,8 @@ impl<F: FieldExt> ExecutionGadget<F> for SignedComparatorGadget<F> {
             b,
             sign_check_a,
             sign_check_b,
-            comparison_lo,
-            comparison_hi,
+            lt_lo,
+            lt_hi,
             is_sgt,
         }
     }
@@ -192,13 +182,13 @@ impl<F: FieldExt> ExecutionGadget<F> for SignedComparatorGadget<F> {
             F::from(128u64),
         )?;
 
-        self.comparison_lo.assign(
+        self.lt_lo.assign(
             region,
             offset,
             from_bytes::value(&a[0..16]),
             from_bytes::value(&b[0..16]),
         )?;
-        self.comparison_hi.assign(
+        self.lt_hi.assign(
             region,
             offset,
             from_bytes::value(&a[16..32]),
