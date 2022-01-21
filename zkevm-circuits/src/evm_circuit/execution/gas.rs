2fbc879640cb9b4808bdac1441aebf1bb4b549b6
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-01-21 15:42:38 +0800

    fix: update based on review

--- zkevm-circuits/src/evm_circuit/execution/gas.rs
+++ zkevm-circuits/src/evm_circuit/execution/gas.rs
@@ -1,7 +1,7 @@
 use crate::{
     evm_circuit::{
         execution::ExecutionGadget,
-        param::NUM_BYTES_GAS,
+        param::N_BYTES_GAS,
         step::ExecutionState,
         util::{
             common_gadget::SameContextGadget,
@@ -15,14 +15,13 @@ use crate::{
     util::Expr,
 };
 
-use array_init::array_init;
 use bus_mapping::evm::OpcodeId;
 use halo2::{arithmetic::FieldExt, circuit::Region, plonk::Error};
 
 #[derive(Clone, Debug)]
 pub(crate) struct GasGadget<F> {
     same_context: SameContextGadget<F>,
-    value: RandomLinearCombination<F, NUM_BYTES_GAS>,
+    gas_left: RandomLinearCombination<F, N_BYTES_GAS>,
 }
 
 impl<F: FieldExt> ExecutionGadget<F> for GasGadget<F> {
@@ -32,21 +31,19 @@ impl<F: FieldExt> ExecutionGadget<F> for GasGadget<F> {
 
     fn configure(cb: &mut ConstraintBuilder<F>) -> Self {
         // The gas passed to a transaction is a 64-bit number.
-        let gas_left = array_init(|_| cb.query_cell());
+        let gas_left = cb.query_rlc();
 
         // The `gas_left` in the current state has to be deducted by the gas
         // used by the `GAS` opcode itself.
         cb.require_equal(
             "Constraint: gas left equal to stack value",
-            from_bytes::expr(&gas_left),
+            from_bytes::expr(&gas_left.cells),
             cb.curr.state.gas_left.expr()
                 - OpcodeId::GAS.constant_gas_cost().expr(),
         );
 
         // Construct the value and push it to stack.
-        let value =
-            RandomLinearCombination::new(gas_left, cb.power_of_randomness());
-        cb.stack_push(value.expr());
+        cb.stack_push(gas_left.expr());
 
         let step_state_transition = StepStateTransition {
             rw_counter: Delta(1.expr()),
@@ -64,7 +61,7 @@ impl<F: FieldExt> ExecutionGadget<F> for GasGadget<F> {
 
         Self {
             same_context,
-            value,
+            gas_left,
         }
     }
 
@@ -81,7 +78,7 @@ impl<F: FieldExt> ExecutionGadget<F> for GasGadget<F> {
 
         // The GAS opcode takes into account the reduction of gas available due
         // to the instruction itself.
-        self.value.assign(
+        self.gas_left.assign(
             region,
             offset,
             Some(
@@ -97,9 +94,7 @@ impl<F: FieldExt> ExecutionGadget<F> for GasGadget<F> {
 
 #[cfg(test)]
 mod test {
-    use crate::evm_circuit::{
-        test::run_test_circuit_incomplete_fixed_table, witness,
-    };
+    use crate::test_util::run_test_circuits;
     use bus_mapping::bytecode;
 
     fn test_ok() {
@@ -108,8 +103,7 @@ mod test {
             GAS
             STOP
         };
-        let block = witness::build_block_from_trace_code_at_start(&bytecode);
-        assert_eq!(run_test_circuit_incomplete_fixed_table(block), Ok(()));
+        assert_eq!(run_test_circuits(bytecode), Ok(()));
     }
 
     #[test]
