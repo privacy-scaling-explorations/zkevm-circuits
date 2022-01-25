e468040a3c35c2f58295b9f8f59b70f70bd8f9eb
Scroll Dev <dev@scroll.tech>
2022-01-26 00:42:15 +0800

    Split eth-types Error (#304)
    * Move some bus-mapping related errors out of eth-types.
    
    * Add not used errors InvalidOpConversion and StorageKeyNotFound back.
    
    Co-authored-by: Steven Gu <asongala@163.com>

--- bus-mapping/src/lib.rs
+++ bus-mapping/src/lib.rs
@@ -218,6 +218,7 @@
 #![allow(clippy::upper_case_acronyms)] // Too pedantic
 
 extern crate alloc;
+pub mod error;
 pub mod evm;
 pub mod exec_trace;
 pub mod external_tracer;
@@ -229,4 +230,4 @@ pub(crate) mod geth_errors;
 pub mod mock;
 pub mod rpc;
 pub mod state_db;
-pub use eth_types::Error;
+pub use error::Error;
