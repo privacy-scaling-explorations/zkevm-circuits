7518c31bd2164d2aea58ecd5354e87c271930763
Mason Liang <mason@scroll.tech>
2022-01-28 00:06:24 -0500

    Simplify code with query_rlc

--- zkevm-circuits/src/evm_circuit/execution/timestamp.rs
+++ zkevm-circuits/src/evm_circuit/execution/timestamp.rs
@@ -15,14 +15,13 @@ use crate::{
     },
     util::Expr,
 };
-use array_init::array_init;
 use halo2::{arithmetic::FieldExt, circuit::Region, plonk::Error};
 use std::convert::TryFrom;
 
 #[derive(Clone, Debug)]
 pub(crate) struct TimestampGadget<F> {
     same_context: SameContextGadget<F>,
-    value: RandomLinearCombination<F, N_BYTES_U64>,
+    timestamp: RandomLinearCombination<F, N_BYTES_U64>,
 }
 
 impl<F: FieldExt> ExecutionGadget<F> for TimestampGadget<F> {
@@ -31,18 +30,15 @@ impl<F: FieldExt> ExecutionGadget<F> for TimestampGadget<F> {
     const EXECUTION_STATE: ExecutionState = ExecutionState::TIMESTAMP;
 
     fn configure(cb: &mut ConstraintBuilder<F>) -> Self {
-        let timestamp = array_init(|_| cb.query_cell());
+        let timestamp = cb.query_rlc();
+        cb.stack_push(timestamp.expr());
 
         // Lookup block table with timestamp
         cb.block_lookup(
             BlockContextFieldTag::Time.expr(),
             None,
-            from_bytes::expr(&timestamp),
+            from_bytes::expr(&timestamp.cells),
         );
-
-        let value =
-            RandomLinearCombination::new(timestamp, cb.power_of_randomness());
-        cb.stack_push(value.expr());
 
         // State transition
         let opcode = cb.query_cell();
@@ -61,7 +57,7 @@ impl<F: FieldExt> ExecutionGadget<F> for TimestampGadget<F> {
 
         Self {
             same_context,
-            value,
+            timestamp,
         }
     }
 
@@ -78,7 +74,7 @@ impl<F: FieldExt> ExecutionGadget<F> for TimestampGadget<F> {
 
         let timestamp = block.rws[step.rw_indices[0]].stack_value();
 
-        self.value.assign(
+        self.timestamp.assign(
             region,
             offset,
             Some(u64::try_from(timestamp).unwrap().to_le_bytes()),
