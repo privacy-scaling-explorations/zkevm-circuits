a07200fb2675cada12bbcc3abc5fdaf21d101b55
Han <tinghan0110@gmail.com>
2022-02-26 16:34:07 +0800

    feat: initialize last_callee info in BeginTx (#298)

--- zkevm-circuits/src/evm_circuit/table.rs
+++ zkevm-circuits/src/evm_circuit/table.rs
@@ -178,6 +178,10 @@ pub enum CallContextFieldTag {
     IsPersistent,
     IsStatic,
 
+    LastCalleeId,
+    LastCalleeReturnDataOffset,
+    LastCalleeReturnDataLength,
+
     IsRoot,
     IsCreate,
     CodeSource,
