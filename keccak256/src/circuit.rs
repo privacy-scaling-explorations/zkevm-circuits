e604680f1f88c94206117583d7f5e8c33dbc7652
CPerezz <c.perezbaro@gmail.com>
2022-03-07 20:10:27 +0100

    keccak: Add padding related constants

---
+++ keccak256/src/circuit.rs
@@ -0,0 +1,7 @@
+pub mod padding;
+
+pub const MAX_INPUT_BYTES: usize = MAX_INPUT_WORDS * BYTES_PER_WORD;
+pub const MAX_INPUT_WORDS: usize = MAX_PERM_ROUNDS * NEXT_INPUTS_WORDS;
+pub const BYTES_PER_WORD: usize = 8;
+pub const NEXT_INPUTS_WORDS: usize = 17;
+pub const MAX_PERM_ROUNDS: usize = 10;
