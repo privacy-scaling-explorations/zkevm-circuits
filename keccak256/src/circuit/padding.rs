9218557029bb93cbc3b2d06e9df0bb5864cd5140
CPerezz <c.perezbaro@gmail.com>
2022-03-10 10:51:01 +0100

    keccak: Add tests for word generation
    Adds tests for the correct and expected error test-cases.
    
    This concludes the work on #338

--- keccak256/src/circuit/padding.rs
+++ keccak256/src/circuit/padding.rs
@@ -121,3 +121,227 @@ impl<F: Field> WordBuilderConfig<F> {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use halo2_proofs::{
+        circuit::SimpleFloorPlanner,
+        dev::{
+            metadata::{Column as MetaColumn, Region},
+            FailureLocation, MockProver, VerifyFailure,
+        },
+        plonk::{Advice, Any, Circuit},
+    };
+    use pairing::bn256::Fr;
+    //use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_word_construction_gate() {
+        #[derive(Default)]
+        struct MyCircuit<F> {
+            bytes: [F; BYTES_PER_WORD],
+            word: F,
+        }
+
+        #[derive(Clone)]
+        struct MyConfig<F> {
+            word_build_conf: WordBuilderConfig<F>,
+            bytes: [Column<Advice>; BYTES_PER_WORD],
+            word: Column<Advice>,
+        }
+
+        impl<F: Field> Circuit<F> for MyCircuit<F> {
+            type Config = MyConfig<F>;
+            type FloorPlanner = SimpleFloorPlanner;
+
+            fn without_witnesses(&self) -> Self {
+                Self::default()
+            }
+
+            fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+                // Create WordBuilderConfig
+                let table = RangeCheckConfig::<F, 255>::configure(meta);
+                let bytes: [Column<Advice>; BYTES_PER_WORD] = (0..BYTES_PER_WORD)
+                    .map(|_| {
+                        let column = meta.advice_column();
+                        meta.enable_equality(column);
+                        column
+                    })
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap();
+
+                let word = meta.advice_column();
+                meta.enable_equality(word);
+                let word_build_conf = WordBuilderConfig::configure(meta, bytes, word, table);
+
+                // Create a MyConfig instance with all the extra columns needed
+                // for testing purposes
+                let bytes: [Column<Advice>; BYTES_PER_WORD] = (0..BYTES_PER_WORD)
+                    .map(|_| {
+                        let column = meta.advice_column();
+                        meta.enable_equality(column);
+                        column
+                    })
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap();
+                let word = meta.advice_column();
+                meta.enable_equality(word);
+
+                MyConfig {
+                    word_build_conf,
+                    bytes,
+                    word,
+                }
+            }
+
+            fn synthesize(
+                &self,
+                config: Self::Config,
+                mut layouter: impl Layouter<F>,
+            ) -> Result<(), Error> {
+                config.word_build_conf.byte_table.load(&mut layouter)?;
+                let bytes = layouter.assign_region(
+                    || "Wittnes of word's bytes",
+                    |mut region| {
+                        let bytes: [AssignedCell<F, F>; BYTES_PER_WORD] = {
+                            let mut state: Vec<AssignedCell<F, F>> =
+                                Vec::with_capacity(BYTES_PER_WORD);
+                            for (idx, val) in self.bytes.iter().enumerate() {
+                                let cell = region.assign_advice(
+                                    || "witness input state",
+                                    config.bytes[idx],
+                                    0,
+                                    || Ok(*val),
+                                )?;
+                                state.push(cell)
+                            }
+                            state.try_into().unwrap()
+                        };
+                        Ok(bytes)
+                    },
+                )?;
+
+                let circuit_word = config.word_build_conf.assign_bytes(&mut layouter, bytes)?;
+
+                // Add equality constrain between the obtained word from the circuit and the
+                // witnessed one.
+                layouter.assign_region(
+                    || "Constraint word to be equal to the test announced one",
+                    |mut region| {
+                        let announced_word = region.assign_advice(
+                            || "Add test word",
+                            config.word,
+                            0,
+                            || Ok(self.word),
+                        )?;
+                        region.constrain_equal(circuit_word.cell(), announced_word.cell())
+                    },
+                )?;
+                Ok(())
+            }
+        }
+
+        // Test that the circuit works when we provide correct witnesses.
+        {
+            let bytes = [
+                Fr::from(0xafu64),
+                Fr::from(0xafu64),
+                Fr::from(0xafu64),
+                Fr::from(0xafu64),
+                Fr::zero(),
+                Fr::zero(),
+                Fr::zero(),
+                Fr::zero(),
+            ];
+            let word = Fr::from(2947526575u64);
+
+            let circuit = MyCircuit::<Fr> { bytes, word };
+
+            // Test without public inputs
+            let prover = MockProver::<Fr>::run(9, &circuit, vec![]).unwrap();
+
+            assert_eq!(prover.verify(), Ok(()));
+        }
+
+        // Test that if we pass a byte greater than 255 the lookup will cause the
+        // circuit to fail.
+        {
+            let bytes = [
+                Fr::from(0x100u64),
+                Fr::from(0xafu64),
+                Fr::from(0xafu64),
+                Fr::from(0xafu64),
+                Fr::zero(),
+                Fr::zero(),
+                Fr::zero(),
+                Fr::zero(),
+            ];
+            let word = Fr::from(68730990511u64);
+
+            let circuit = MyCircuit::<Fr> { bytes, word };
+
+            // Test without public inputs
+            let prover = MockProver::<Fr>::run(9, &circuit, vec![]).unwrap();
+
+            assert_eq!(
+                prover.verify(),
+                Err(vec![
+                    VerifyFailure::Lookup {
+                        name: "Range check for word byte",
+                        lookup_index: 0,
+                        location: FailureLocation::InRegion {
+                            region: Region::from((2, "Word construction")),
+                            offset: 0
+                        }
+                    },
+                    VerifyFailure::Permutation {
+                        column: MetaColumn::from((Any::Advice, 8)),
+                        row: 0
+                    },
+                    VerifyFailure::Permutation {
+                        column: MetaColumn::from((Any::Advice, 17)),
+                        row: 0
+                    }
+                ])
+            )
+        }
+
+        // Test that if the bytes are not equivalent to the announced word, the circuit
+        // fails.
+        {
+            let bytes = [
+                Fr::from(0xafu64),
+                Fr::from(0xafu64),
+                Fr::from(0xafu64),
+                Fr::from(0xafu64),
+                Fr::zero(),
+                Fr::zero(),
+                Fr::zero(),
+                Fr::zero(),
+            ];
+            let word = Fr::from(2947526574u64);
+
+            let circuit = MyCircuit::<Fr> { bytes, word };
+
+            // Test without public inputs
+            let prover = MockProver::<Fr>::run(9, &circuit, vec![]).unwrap();
+
+            assert_eq!(
+                prover.verify(),
+                Err(vec![
+                    VerifyFailure::Permutation {
+                        column: MetaColumn::from((Any::Advice, 8)),
+                        row: 0
+                    },
+                    VerifyFailure::Permutation {
+                        column: MetaColumn::from((Any::Advice, 17)),
+                        row: 0
+                    }
+                ])
+            )
+        }
+    }
+}
