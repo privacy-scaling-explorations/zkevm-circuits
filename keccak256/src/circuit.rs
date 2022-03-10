9a536cf31fbacfe626e033a029a2a14d039f0fc5
CPerezz <c.perezbaro@gmail.com>
2022-03-10 14:34:04 +0100

    keccak: Rename padding.rs into word_builder.rs

--- keccak256/src/circuit.rs
+++ keccak256/src/circuit.rs
@@ -1,4 +1,4 @@
-pub mod padding;
+pub mod word_builder;
 
 pub const MAX_INPUT_BYTES: usize = MAX_INPUT_WORDS * BYTES_PER_WORD;
 pub const MAX_INPUT_WORDS: usize = MAX_PERM_ROUNDS * NEXT_INPUTS_WORDS;
