45bfa30dd7c3c3a2f537e8c44dec294c1864affd
Jules de Smit <julesdesmit@gmail.com>
2022-03-10 19:59:04 +0000

    WIP adding verification logic

--- ipc-verifier/src/main.rs
+++ ipc-verifier/src/main.rs
@@ -1,9 +1,15 @@
+use halo2_proofs::plonk::{keygen_pk, keygen_vk, verify_proof, ProvingKey, SingleVerifier};
+use halo2_proofs::poly::commitment::{Params, ParamsVerifier};
+use pairing::bn256::{Bn256, Fr, G1Affine};
 use std::fs;
 use std::io::{Cursor, Error as IOError, Read, Write};
 use std::os::unix::net::UnixListener;
 use std::path::Path;
+use zkevm_circuits::{evm_circuit::EvmCircuit, state_circuit::StateCircuit};
 
 const SOCKET_PATH: &'static str = "/tmp/halo2.sock";
+// TODO: what should this be?
+const DEGREE: u32 = 1;
 
 fn main() {
     let socket = Path::new(SOCKET_PATH);
@@ -19,6 +25,22 @@ fn main() {
     let (mut socket, _addr) = listener
         .accept()
         .expect("should be able to accept a connection");
+
+    // Set up params
+    // TODO: this should probably come from a transcript file later on
+    let general_params: Params<G1Affine> = Params::<G1Affine>::unsafe_setup::<Bn256>(DEGREE);
+    let evm_circuit = EvmCircuit::<Fr>::configure();
+    // TODO: where do i get the config?
+    let state_circuit = StateCircuit::<Fr, true, 49152, 16384, 2000, 16384, 1300, 16384>::default();
+
+    let evm_vk = keygen_vk(&general_params, &evm_circuit).unwrap();
+    let evm_pk = keygen_pk(&general_params, evm_vk, &evm_circuit).unwrap();
+
+    let state_vk = keygen_vk(&general_params, &state_circuit).unwrap();
+    let state_pk = keygen_pk(&general_params, state_vk, &state_circuit).unwrap();
+
+    let verifier_params: ParamsVerifier<Bn256> =
+        general_params.verifier((DEGREE * 2) as usize).unwrap();
 
     loop {
         // Read msg length
@@ -38,7 +60,7 @@ fn main() {
         let (evm_proof, state_proof) =
             recover_transcripts(&mut reader).expect("should be able to recover proofs");
 
-        let verified = verify_proofs(evm_proof, state_proof);
+        let verified = verify_proofs(evm_proof, state_proof, &verifier_params, &evm_pk, &state_pk);
 
         socket
             .write(&vec![verified as u8])
@@ -71,6 +93,12 @@ fn recover_transcripts<R: Read>(reader: &mut R) -> Result<(Vec<u8>, Vec<u8>), IO
     Ok((evm_proof, state_proof))
 }
 
-fn verify_proofs(evm_proof: Vec<u8>, state_proof: Vec<u8>) -> bool {
+fn verify_proofs(
+    evm_proof: Vec<u8>,
+    state_proof: Vec<u8>,
+    verifier_params: &ParamsVerifier<Bn256>,
+    evm_pk: &ProvingKey<G1Affine>,
+    state_pk: &ProvingKey<G1Affine>,
+) -> bool {
     true
 }
