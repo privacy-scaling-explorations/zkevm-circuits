a0068b21a7699a3f3fd7e0a1c234ee7e89e0d8ec
CPerezz <c.perezbaro@gmail.com>
2022-03-10 10:49:47 +0100

    keccak: Refactor lookup for rangechek

--- keccak256/src/circuit/padding.rs
+++ keccak256/src/circuit/padding.rs
@@ -1,8 +1,8 @@
-use super::{BYTES_PER_WORD, MAX_INPUT_BYTES, MAX_INPUT_WORDS};
+use super::BYTES_PER_WORD;
 use crate::permutation::tables::RangeCheckConfig;
 use eth_types::Field;
 use halo2_proofs::{
-    circuit::{layouter::RegionLayouter, AssignedCell, Layouter},
+    circuit::{AssignedCell, Layouter},
     plonk::{Advice, Column, ConstraintSystem, Error, Expression, Selector},
     poly::Rotation,
 };
@@ -13,69 +13,72 @@ pub type Byte = u8;
 pub type AssignedByte<F> = AssignedCell<F, F>;
 pub type AssignedWord<F> = AssignedCell<F, F>;
 
+#[derive(Debug, Clone)]
 /// Gets 8 Advice columns with the 8bytes to form the word + the final word
 /// value of the composed bytes at the end.
 pub(crate) struct WordBuilderConfig<F> {
     q_enable: Selector,
-    bytes_and_word: [Column<Advice>; BYTES_PER_WORD + 1],
+    bytes: [Column<Advice>; BYTES_PER_WORD],
+    word: Column<Advice>,
     byte_table: RangeCheckConfig<F, 255>,
-    _marker: PhantomData<F>,
 }
 
 impl<F: Field> WordBuilderConfig<F> {
     pub(crate) fn configure(
         meta: &mut ConstraintSystem<F>,
-        bytes: [Column<Advice>; BYTES_PER_WORD + 1],
+        bytes: [Column<Advice>; BYTES_PER_WORD],
+        word: Column<Advice>,
+        byte_table: RangeCheckConfig<F, 255>,
     ) -> WordBuilderConfig<F> {
         let q_enable = meta.complex_selector();
-        let byte_table = RangeCheckConfig::<F, 255>::configure(meta);
 
         // Enable equality for all byte cells.
         bytes.iter().for_each(|&byte| meta.enable_equality(byte));
+        meta.enable_equality(word);
 
-        meta.lookup("Range check for word bytes", |meta| {
-            let q_enable = meta.query_selector(q_enable);
-            bytes
-                .iter()
-                .map(|&byte| {
-                    (
-                        q_enable.clone() * meta.query_advice(byte, Rotation::cur()),
-                        byte_table.range,
-                    )
-                })
-                .collect_vec()
+        bytes.iter().for_each(|&byte| {
+            meta.lookup("Range check for word byte", |meta| {
+                let q_enable = meta.query_selector(q_enable);
+
+                vec![(
+                    q_enable.clone() * meta.query_advice(byte, Rotation::cur()),
+                    byte_table.range,
+                )]
+            });
         });
 
         meta.create_gate("Build Word", |meta| {
             let q_enable = meta.query_selector(q_enable);
 
             // Running sum to construct a u64 word with 8 u8 bytes shifted by it's position.
-            let bytes_sum: Expression<F> = bytes[0..BYTES_PER_WORD]
+            let bytes_sum: Expression<F> = bytes
                 .iter()
                 .map(|&column| meta.query_advice(column, Rotation::cur()))
                 .enumerate()
-                .map(|(idx, byte_expr)| Expression::Constant(F::from(1u64 << idx)) * byte_expr)
+                .map(|(idx, byte_expr)| {
+                    Expression::Constant(F::from(1u64 << (idx * 8))) * byte_expr
+                })
                 .reduce(|acc, byte_shifted| acc + byte_shifted)
                 // Unwrapping is safe here as we recieve an array that contails all elements.
                 .unwrap();
 
-            let word = meta.query_advice(*bytes.last().unwrap(), Rotation::cur());
+            let word = meta.query_advice(word, Rotation::cur());
 
             vec![q_enable * (bytes_sum - word)]
         });
         Self {
             q_enable,
-            bytes_and_word: bytes,
+            bytes,
+            word,
             byte_table,
-            _marker: PhantomData,
         }
     }
 
-    pub fn load(&self, layouter: &mut impl Layouter<F>) -> Result<(), Error> {
+    pub(crate) fn load(&self, layouter: &mut impl Layouter<F>) -> Result<(), Error> {
         self.byte_table.load(layouter)
     }
 
-    pub fn assign_state(
+    pub(crate) fn assign_bytes(
         &self,
         layouter: &mut impl Layouter<F>,
         bytes: [AssignedByte<F>; BYTES_PER_WORD],
@@ -88,7 +91,7 @@ impl<F: Field> WordBuilderConfig<F> {
                 // Copy bytes to byte columns
                 let byte_cells: [AssignedByte<F>; BYTES_PER_WORD] = bytes
                     .iter()
-                    .zip(self.bytes_and_word[0..BYTES_PER_WORD].iter())
+                    .zip(self.bytes.iter())
                     .map(|(byte, &byte_col)| {
                         byte.copy_advice(|| "Copy byte", &mut region, byte_col, offset)
                     })
@@ -101,23 +104,20 @@ impl<F: Field> WordBuilderConfig<F> {
                 let word_scalar = byte_cells
                     .iter()
                     .enumerate()
-                    .map(|(idx, byte_cell)| F::from(1u64 << idx) * byte_cell.value().unwrap())
+                    .map(|(idx, byte_cell)| {
+                        F::from(1u64 << (idx * 8)) * byte_cell.value().unwrap_or(&F::zero())
+                    })
                     .reduce(|acc, byte_shifted| acc + byte_shifted)
                     // Unwrapping is safe here as we recieve an array that contails all elements.
                     .unwrap();
 
                 region.assign_advice(
                     || "Assign byte-constructed Word",
-                    *self.bytes_and_word.last().unwrap(),
+                    self.word,
                     offset,
                     || Ok(word_scalar),
                 )
             },
         )
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
 }
