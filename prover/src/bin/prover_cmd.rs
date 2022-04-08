07d38c99c792901bd42f9ddfab4ebba9798fc3db
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-04-08 06:41:40 +0000

    prover: add http/json-rpc daemon command
    * Moves the proof computation function from prover_cmd into a module.
    * Add prover_rpcd that provides a task-queue-like JSON-RPC interface
      for dispatching proof generation work.

--- prover/src/bin/prover_cmd.rs
+++ prover/src/bin/prover_cmd.rs
@@ -1,37 +1,17 @@
-use bus_mapping::circuit_input_builder::BuilderClient;
-use bus_mapping::rpc::GethClient;
 use env_logger::Env;
-use ethers_providers::Http;
-use halo2_proofs::{
-    plonk::*,
-    poly::commitment::Params,
-    transcript::{Blake2bWrite, Challenge255},
-};
-use pairing::bn256::{Fr, G1Affine};
-use rand::SeedableRng;
-use rand_xorshift::XorShiftRng;
+use halo2_proofs::poly::commitment::Params;
+use pairing::bn256::G1Affine;
 use std::env::var;
 use std::fs::File;
 use std::io::BufReader;
-use std::str::FromStr;
-use zkevm_circuits::evm_circuit::{
-    table::FixedTableTag, test::TestCircuit, witness::block_convert,
-};
-use zkevm_circuits::state_circuit::StateCircuit;
 
-#[derive(serde::Serialize)]
-pub struct Proofs {
-    state_proof: eth_types::Bytes,
-    evm_proof: eth_types::Bytes,
-}
+use prover::compute_proof::compute_proof;
 
 /// This command generates and prints the proofs to stdout.
 /// Required environment variables:
 /// - BLOCK_NUM - the block number to generate the proof for
 /// - RPC_URL - a geth http rpc that supports the debug namespace
 /// - PARAMS_PATH - a path to a file generated with the gen_params tool
-// TODO: move the proof generation into a module once we implement a rpc daemon for generating
-// proofs.
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -54,85 +34,9 @@ async fn main() {
     let params: Params<G1Affine> =
         Params::read::<_>(&mut BufReader::new(params_fs)).expect("Failed to read params");
 
-    // request & build the inputs for the circuits
-    let geth_client = GethClient::new(Http::from_str(&rpc_url).expect("GethClient from RPC_URL"));
-    let builder = BuilderClient::new(geth_client)
+    let result = compute_proof(&params, &block_num, &rpc_url)
         .await
-        .expect("BuilderClient from GethClient");
-    let builder = builder
-        .gen_inputs(block_num)
-        .await
-        .expect("gen_inputs for BLOCK_NUM");
+        .expect("compute_proof");
 
-    // TODO: only {evm,state}_proof are implemented right now
-    let evm_proof;
-    let state_proof;
-    let block = block_convert(&builder.block, &builder.code_db);
-    {
-        // generate evm_circuit proof
-        let circuit = TestCircuit::<Fr>::new(block.clone(), FixedTableTag::iterator().collect());
-
-        // TODO: can this be pre-generated to a file?
-        // related
-        // https://github.com/zcash/halo2/issues/443
-        // https://github.com/zcash/halo2/issues/449
-        let vk = keygen_vk(&params, &circuit).expect("keygen_vk for params, evm_circuit");
-        let pk = keygen_pk(&params, vk, &circuit).expect("keygen_pk for params, vk, evm_circuit");
-
-        // Create randomness
-        let rng = XorShiftRng::from_seed([
-            0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06,
-            0xbc, 0xe5,
-        ]);
-
-        // create a proof
-        let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
-        create_proof(&params, &pk, &[circuit], &[], rng, &mut transcript).expect("evm proof");
-        evm_proof = transcript.finalize();
-    }
-
-    {
-        // generate state_circuit proof
-        //
-        // TODO: this should be configurable
-        const MEMORY_ADDRESS_MAX: usize = 2000;
-        const STACK_ADDRESS_MAX: usize = 1300;
-        const MEMORY_ROWS_MAX: usize = 16384;
-        const STACK_ROWS_MAX: usize = 16384;
-        const STORAGE_ROWS_MAX: usize = 16384;
-        const GLOBAL_COUNTER_MAX: usize = MEMORY_ROWS_MAX + STACK_ROWS_MAX + STORAGE_ROWS_MAX;
-
-        let circuit = StateCircuit::<
-            Fr,
-            true,
-            GLOBAL_COUNTER_MAX,
-            MEMORY_ADDRESS_MAX,
-            STACK_ADDRESS_MAX,
-            GLOBAL_COUNTER_MAX,
-        >::new(block.randomness, &block.rws);
-
-        // TODO: same quest like in the first scope
-        let vk = keygen_vk(&params, &circuit).expect("keygen_vk for params, state_circuit");
-        let pk = keygen_pk(&params, vk, &circuit).expect("keygen_pk for params, vk, state_circuit");
-
-        // Create randomness
-        let rng = XorShiftRng::from_seed([
-            0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06,
-            0xbc, 0xe5,
-        ]);
-
-        // create a proof
-        let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
-        create_proof(&params, &pk, &[circuit], &[], rng, &mut transcript).expect("state proof");
-        state_proof = transcript.finalize();
-    }
-
-    serde_json::to_writer(
-        std::io::stdout(),
-        &Proofs {
-            evm_proof: evm_proof.into(),
-            state_proof: state_proof.into(),
-        },
-    )
-    .expect("serialize and write");
+    serde_json::to_writer(std::io::stdout(), &result).expect("serialize and write");
 }
