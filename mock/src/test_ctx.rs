e806164a84585edc74b06cf7f476725fdb49cb33
Carlos Pérez <37264926+CPerezz@users.noreply.github.com>
2022-04-08 12:46:26 +0200

    Move chain_id from Tx to Block on mock  (#439)
    * update: Move `chain_id` from Tx to Block on mock
    
    When generating a `TestContext` now, users will be able to set the
    `chain_id` parameter only at block level but not at tx one.
    
    Anyway, `MockTransaction` will still have this field. But there's no
    public API to interact with it. Instead, the values of the `chain_id`
    for the txs of a block will automatically be set to the value that was
    set as `chain_id` for the block.
    
    This closes the circle on the turn arround that was done in
    https://github.com/appliedzkp/zkevm-specs/pull/168 by adding `CHAINID`
    as a blockCtx Opcode rather than a Tx one for simplification &
    optimization purposes.
    
    Resolves: #437
    
    * update: Update Cargo.lock
    
    * fix: Reference MockTransaction instead of MockBlock

--- mock/src/test_ctx.rs
+++ mock/src/test_ctx.rs
@@ -161,12 +161,7 @@ impl<const NACC: usize, const NTX: usize> TestContext<NACC, NTX> {
         block.transactions.extend_from_slice(&transactions);
         func_block(&mut block, transactions).build();
 
-        let transactions: Vec<Transaction> = block
-            .transactions
-            .iter()
-            .cloned()
-            .map(Transaction::from)
-            .collect();
+        let chain_id = block.chain_id;
         let block = Block::<Transaction>::from(block);
         let accounts: [Account; NACC] = accounts
             .iter()
@@ -179,7 +174,7 @@ impl<const NACC: usize, const NTX: usize> TestContext<NACC, NTX> {
         let geth_traces = gen_geth_traces(block.clone(), accounts.clone(), history_hashes.clone())?;
 
         Ok(Self {
-            chain_id: transactions[0].chain_id.unwrap_or_default(),
+            chain_id,
             accounts,
             history_hashes: history_hashes.unwrap_or_default(),
             eth_block: block,
