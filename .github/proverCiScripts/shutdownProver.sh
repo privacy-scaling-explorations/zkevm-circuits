2e189402cbc8f1cb4ba0cac2ea0c8fcc1c9940bc
Nick Tampakas <nick@gmx.co.uk>
2022-04-11 14:40:52 +0300

    updated shutdown script to dynamically get InstanceID

--- .github/proverCiScripts/shutdownProver.sh
+++ .github/proverCiScripts/shutdownProver.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #set -e 
-#set -xx
+#set -x
 
-aws ec2 stop-instances --instance-ids i-0f92100cb5a0ff7d5 --profile cirunner
+profile="cirunner"
+provers_vpc_id="vpc-09fb44da782f32abb"
+
+running_prover=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[proverbench-multiAZ] Name=instance-state-name,Values=[running] Name=network-interface.vpc-id,Values=[$provers_vpc_id] --query "Reservations[*].Instances[*][InstanceId]" --output text)
+
+aws ec2 stop-instances --profile $profile --instance-ids $running_prover
+
 sleep 30
