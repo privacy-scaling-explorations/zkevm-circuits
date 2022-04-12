4e8e00a5c8f11171cf84a434fd2b15a67343442e
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-04-12 15:35:20 +0800

    fix: restructure call context reads and tests for internal call

--- bus-mapping/src/evm/opcodes/calldataload.rs
+++ bus-mapping/src/evm/opcodes/calldataload.rs
@@ -29,19 +29,36 @@ impl Opcode for Calldataload {
             offset,
         )?;
 
-        state.push_op(
-            &mut exec_step,
-            RW::READ,
-            CallContextOp {
-                call_id: state.call()?.call_id,
-                field: CallContextField::TxId,
-                value: state.tx_ctx.id().into(),
-            },
-        );
-
         let is_root = state.call()?.is_root;
-
-        if !is_root {
+        if is_root {
+            state.push_op(
+                &mut exec_step,
+                RW::READ,
+                CallContextOp {
+                    call_id: state.call()?.call_id,
+                    field: CallContextField::TxId,
+                    value: state.tx_ctx.id().into(),
+                },
+            );
+            state.push_op(
+                &mut exec_step,
+                RW::READ,
+                CallContextOp {
+                    call_id: state.call()?.call_id,
+                    field: CallContextField::CallDataLength,
+                    value: state.call()?.call_data_length.into(),
+                },
+            );
+        } else {
+            state.push_op(
+                &mut exec_step,
+                RW::READ,
+                CallContextOp {
+                    call_id: state.call()?.call_id,
+                    field: CallContextField::CallerId,
+                    value: state.call()?.caller_id.into(),
+                },
+            );
             state.push_op(
                 &mut exec_step,
                 RW::READ,
@@ -60,15 +77,6 @@ impl Opcode for Calldataload {
                     value: state.call()?.call_data_offset.into(),
                 },
             );
-            state.push_op(
-                &mut exec_step,
-                RW::READ,
-                CallContextOp {
-                    call_id: state.call()?.call_id,
-                    field: CallContextField::CallerId,
-                    value: state.call()?.caller_id.into(),
-                },
-            );
         }
 
         let (offset, src_addr, src_addr_end, caller_id, call_data) = {
@@ -84,19 +92,19 @@ impl Opcode for Calldataload {
         let calldata_word = (0..32)
             .map(|idx| {
                 let addr = offset + idx;
-                let byte = if addr < src_addr_end {
-                    call_data[addr]
+                if addr < src_addr_end {
+                    let byte = call_data[addr];
+                    if !is_root {
+                        state.push_op(
+                            &mut exec_step,
+                            RW::READ,
+                            MemoryOp::new(caller_id, (src_addr + idx).into(), byte),
+                        );
+                    }
+                    byte
                 } else {
                     0
-                };
-                if !is_root {
-                    state.push_op(
-                        &mut exec_step,
-                        RW::READ,
-                        MemoryOp::new(caller_id, src_addr.into(), byte),
-                    );
                 }
-                byte
             })
             .collect::<Vec<u8>>();
 
@@ -114,15 +122,12 @@ impl Opcode for Calldataload {
 #[cfg(test)]
 mod calldataload_tests {
     use eth_types::{
-        bytecode,
+        address, bytecode,
         evm_types::{OpcodeId, StackAddress},
         geth_types::GethData,
-        Word,
+        Address, ToWord, Word,
     };
-    use mock::{
-        test_ctx::helpers::{account_0_code_account_1_no_code, tx_from_1_to_0},
-        TestContext,
-    };
+    use mock::{test_ctx::helpers::account_0_code_account_1_no_code, TestContext};
     use rand::random;
 
     use crate::{circuit_input_builder::ExecState, mock::BlockData, operation::StackOp};
@@ -133,100 +138,59 @@ mod calldataload_tests {
         (0..size).map(|_| random()).collect::<Vec<u8>>()
     }
 
-    #[test]
-    fn calldataload_root_opcode_impl() {
-        // 1. should be right padded
-        test_root_ok(0u64, vec![1u8, 2u8], {
-            let mut v = vec![0u8; 32];
-            v[0] = 1u8;
-            v[1] = 2u8;
-            Word::from_big_endian(&v)
-        });
-        // 2. exactly 32 bytes
-        let calldata = rand_bytes(32);
-        test_root_ok(0u64, calldata.clone(), Word::from_big_endian(&calldata));
-        // 3. take only 32 bytes
-        let calldata = rand_bytes(64);
-        test_root_ok(
-            12u64,
-            calldata.clone(),
-            Word::from_big_endian(&calldata[12..44]),
-        );
-    }
+    fn test_internal_ok(
+        call_data_length: usize,
+        call_data_offset: usize,
+        offset: usize,
+        pushdata: Vec<u8>,
+        call_data_word: Word,
+    ) {
+        let addr_a = address!("0x000000000000000000000000000000000cafe00a");
+        let addr_b = address!("0x000000000000000000000000000000000cafe00b");
 
-    // This test must be enabled and should pass once `CREATE` is handled with a
-    // dummy gen associated ops.
-    #[test]
-    #[ignore]
-    fn calldataload_internal_opcode_impl() {
-        test_internal_ok();
-    }
-
-    fn test_internal_ok() {
-        // PUSH3 600035
-        // PUSH1 00
-        // MSTORE
-        // PUSH1 03
-        // PUSH1 1D
-        // RETURN
-        let contract_code: Vec<u8> = hex::decode("626000356000526003601DF3").unwrap();
-        let create_and_call = bytecode! {
-            // 1. Store the following bytes to memory
-            PUSH12(Word::from_big_endian(&contract_code))
-            PUSH1(0x00)
-            MSTORE
-            // 2. Create a contract with code: 60 00 35
-            PUSH1(0x0c) // size
-            PUSH1(0x14) // offset
-            PUSH1(0x00) // value
-            CREATE
-            // 3. Call created contract, i.e. CALLDATALOAD is in internal call
-            PUSH1(0x00)   // retSize
-            PUSH1(0x00)   // retOffset
-            PUSH1(0x20)   // argsSize
-            PUSH1(0x00)   // argsOffset
-            PUSH1(0x00)   // value
-            DUP6          // address
-            PUSH2(0xFFFF) // gas
-            CALL
-        };
-
-        let block: GethData = TestContext::<2, 1>::new(
-            None,
-            account_0_code_account_1_no_code(create_and_call),
-            tx_from_1_to_0,
-            |block, _tx| block.number(0xcafeu64),
-        )
-        .unwrap()
-        .into();
-
-        let mut builder = BlockData::new_from_geth_data(block.clone()).new_circuit_input_builder();
-        builder
-            .handle_block(&block.eth_block, &block.geth_traces)
-            .unwrap();
-
-        let _step = builder.block.txs()[0]
-            .steps()
-            .iter()
-            .find(|step| step.exec_state == ExecState::Op(OpcodeId::CALLDATALOAD))
-            .unwrap();
-    }
-
-    fn test_root_ok(offset: u64, calldata: Vec<u8>, calldata_word: Word) {
-        let code = bytecode! {
+        // code B gets called by code A, so the call is an internal call.
+        let code_b = bytecode! {
             PUSH32(offset)
             CALLDATALOAD
             STOP
         };
 
-        let block: GethData = TestContext::<2, 1>::new(
+        let mut memory_a = std::iter::repeat(0)
+            .take(32 - pushdata.len() - call_data_offset)
+            .chain(pushdata.clone())
+            .collect::<Vec<u8>>();
+        if memory_a.len() < call_data_length {
+            memory_a.resize(call_data_length, 0);
+        }
+        let code_a = bytecode! {
+            // populate memory in A's context.
+            PUSH32(Word::from_big_endian(&pushdata))
+            PUSH1(0x00) // offset
+            MSTORE
+            // call addr_b
+            PUSH1(0x00) // retLength
+            PUSH1(0x00) // retOffset
+            PUSH1(call_data_length) // argsLength
+            PUSH1(call_data_offset) // argsOffset
+            PUSH1(0x00) // value
+            PUSH32(addr_b.to_word()) // addr
+            PUSH32(0x1_0000) // gas
+            CALL
+            STOP
+        };
+
+        // Get the execution steps from the external tracer
+        let block: GethData = TestContext::<3, 1>::new(
             None,
-            account_0_code_account_1_no_code(code),
+            |accs| {
+                accs[0].address(addr_b).code(code_b);
+                accs[1].address(addr_a).code(code_a);
+                accs[2]
+                    .address(Address::random())
+                    .balance(Word::from(1u64 << 30));
+            },
             |mut txs, accs| {
-                txs[0]
-                    .to(accs[0].address)
-                    .from(accs[1].address)
-                    .input(calldata.into());
+                txs[0].to(accs[1].address).from(accs[2].address);
             },
             |block, _tx| block.number(0xcafeu64),
         )
@@ -244,36 +208,207 @@ mod calldataload_tests {
             .find(|step| step.exec_state == ExecState::Op(OpcodeId::CALLDATALOAD))
             .unwrap();
 
+        let call_id = builder.block.txs()[0].calls()[step.call_index].call_id;
+        let caller_id = builder.block.txs()[0].calls()[step.call_index].caller_id;
+
+        // 1 stack read, 3 call context reads, 32 memory reads and 1 stack write.
+        assert_eq!(step.bus_mapping_instance.len(), 37);
+
+        // stack read and write.
         assert_eq!(
-            [0, 2]
+            [0, 36]
                 .map(|idx| &builder.block.container.stack[step.bus_mapping_instance[idx].as_usize()])
                 .map(|op| (op.rw(), op.op())),
             [
                 (
                     RW::READ,
-                    &StackOp::new(1, StackAddress::from(1023), Word::from(offset)),
+                    &StackOp::new(call_id, StackAddress::from(1023), Word::from(offset)),
                 ),
                 (
                     RW::WRITE,
-                    &StackOp::new(1, StackAddress::from(1023), calldata_word),
+                    &StackOp::new(call_id, StackAddress::from(1023), call_data_word),
                 ),
             ]
         );
 
+        // call context reads.
         assert_eq!(
-            {
-                let op =
-                    &builder.block.container.call_context[step.bus_mapping_instance[1].as_usize()];
-                (op.rw(), op.op())
-            },
-            (
-                RW::READ,
-                &CallContextOp {
-                    call_id: builder.block.txs()[0].calls()[0].call_id,
-                    field: CallContextField::TxId,
-                    value: Word::from(1),
-                }
-            ),
+            [1, 2, 3]
+                .map(|idx| &builder.block.container.call_context
+                    [step.bus_mapping_instance[idx].as_usize()])
+                .map(|op| (op.rw(), op.op())),
+            [
+                (
+                    RW::READ,
+                    &CallContextOp {
+                        call_id,
+                        field: CallContextField::CallerId,
+                        value: Word::from(caller_id),
+                    },
+                ),
+                (
+                    RW::READ,
+                    &CallContextOp {
+                        call_id,
+                        field: CallContextField::CallDataLength,
+                        value: Word::from(call_data_length),
+                    },
+                ),
+                (
+                    RW::READ,
+                    &CallContextOp {
+                        call_id,
+                        field: CallContextField::CallDataOffset,
+                        value: Word::from(call_data_offset),
+                    }
+                ),
+            ],
         );
+
+        // 32 memory reads from caller memory
+        assert_eq!(
+            (0..32)
+                .map(|idx| &builder.block.container.memory
+                    [step.bus_mapping_instance[4 + idx].as_usize()])
+                .map(|op| (op.rw(), op.op().clone()))
+                .collect::<Vec<(RW, MemoryOp)>>(),
+            (0..32)
+                .map(|idx| {
+                    (
+                        RW::READ,
+                        MemoryOp::new(
+                            caller_id,
+                            (call_data_offset + offset + idx).into(),
+                            memory_a[offset + idx],
+                        ),
+                    )
+                })
+                .collect::<Vec<(RW, MemoryOp)>>(),
+        );
+    }
+
+    fn test_root_ok(offset: u64, calldata: Vec<u8>, calldata_word: Word) {
+        let code = bytecode! {
+            PUSH32(offset)
+            CALLDATALOAD
+            STOP
+        };
+
+        let block: GethData = TestContext::<2, 1>::new(
+            None,
+            account_0_code_account_1_no_code(code),
+            |mut txs, accs| {
+                txs[0]
+                    .to(accs[0].address)
+                    .from(accs[1].address)
+                    .input(calldata.clone().into());
+            },
+            |block, _tx| block.number(0xcafeu64),
+        )
+        .unwrap()
+        .into();
+
+        let mut builder = BlockData::new_from_geth_data(block.clone()).new_circuit_input_builder();
+        builder
+            .handle_block(&block.eth_block, &block.geth_traces)
+            .unwrap();
+
+        let step = builder.block.txs()[0]
+            .steps()
+            .iter()
+            .find(|step| step.exec_state == ExecState::Op(OpcodeId::CALLDATALOAD))
+            .unwrap();
+
+        let call_id = builder.block.txs()[0].calls()[0].call_id;
+
+        // 1 stack read, 2 call context reads and 1 stack write.
+        assert_eq!(step.bus_mapping_instance.len(), 4);
+
+        // stack read and write.
+        assert_eq!(
+            [0, 3]
+                .map(|idx| &builder.block.container.stack[step.bus_mapping_instance[idx].as_usize()])
+                .map(|op| (op.rw(), op.op())),
+            [
+                (
+                    RW::READ,
+                    &StackOp::new(call_id, StackAddress::from(1023), Word::from(offset)),
+                ),
+                (
+                    RW::WRITE,
+                    &StackOp::new(call_id, StackAddress::from(1023), calldata_word),
+                ),
+            ]
+        );
+
+        // call context reads.
+        assert_eq!(
+            [1, 2]
+                .map(|idx| &builder.block.container.call_context
+                    [step.bus_mapping_instance[idx].as_usize()])
+                .map(|op| (op.rw(), op.op())),
+            [
+                (
+                    RW::READ,
+                    &CallContextOp {
+                        call_id,
+                        field: CallContextField::TxId,
+                        value: Word::from(1),
+                    }
+                ),
+                (
+                    RW::READ,
+                    &CallContextOp {
+                        call_id,
+                        field: CallContextField::CallDataLength,
+                        value: Word::from(calldata.len()),
+                    },
+                )
+            ],
+        );
+    }
+
+    #[test]
+    fn calldataload_opcode_root() {
+        // 1. should be right padded
+        test_root_ok(0u64, vec![1u8, 2u8], {
+            let mut v = vec![0u8; 32];
+            v[0] = 1u8;
+            v[1] = 2u8;
+            Word::from_big_endian(&v)
+        });
+
+        // 2. exactly 32 bytes
+        let calldata = rand_bytes(32);
+        test_root_ok(0u64, calldata.clone(), Word::from_big_endian(&calldata));
+
+        // 3. out-of-bounds: take only 32 bytes
+        let calldata = rand_bytes(64);
+        test_root_ok(
+            12u64,
+            calldata.clone(),
+            Word::from_big_endian(&calldata[12..44]),
+        );
+    }
+
+    #[test]
+    fn calldataload_opcode_internal() {
+        let pushdata = rand_bytes(0x08);
+        let expected = std::iter::repeat(0)
+            .take(0x20 - pushdata.len())
+            .chain(pushdata.clone())
+            .collect::<Vec<u8>>();
+        test_internal_ok(
+            0x20, // call data length
+            0x00, // call data offset
+            0x00, // offset
+            pushdata,
+            Word::from_big_endian(&expected),
+        );
+
+        let pushdata = rand_bytes(0x10);
+        let mut expected = pushdata.clone();
+        expected.resize(0x20, 0);
+        test_internal_ok(0x20, 0x10, 0x00, pushdata, Word::from_big_endian(&expected));
     }
 }
