d33f58c30d5d0ac011152c1e7725260b32c2d881
DreamWuGit <wwuwwei@126.com>
2022-05-23 18:17:18 +0800

    Refactor rw operation for issue #498 (#522)
    * refactor read&write oeprations
    
    * fix clippy
    
    * fix build error

--- bus-mapping/src/evm/opcodes/swap.rs
+++ bus-mapping/src/evm/opcodes/swap.rs
@@ -1,6 +1,6 @@
 use super::Opcode;
 use crate::circuit_input_builder::{CircuitInputStateRef, ExecStep};
-use crate::{operation::RW, Error};
+use crate::Error;
 use eth_types::GethExecStep;
 
 /// Placeholder structure used to implement [`Opcode`] trait over it
@@ -19,34 +19,15 @@ impl<const N: usize> Opcode for Swap<N> {
         // Peek b and a
         let stack_b_value_read = geth_step.stack.nth_last(N)?;
         let stack_b_position = geth_step.stack.nth_last_filled(N);
-        state.push_stack_op(
-            &mut exec_step,
-            RW::READ,
-            stack_b_position,
-            stack_b_value_read,
-        )?;
+        state.stack_read(&mut exec_step, stack_b_position, stack_b_value_read)?;
+
         let stack_a_value_read = geth_step.stack.last()?;
         let stack_a_position = geth_step.stack.last_filled();
-        state.push_stack_op(
-            &mut exec_step,
-            RW::READ,
-            stack_a_position,
-            stack_a_value_read,
-        )?;
+        state.stack_read(&mut exec_step, stack_a_position, stack_a_value_read)?;
 
         // Write a into b_position, write b into a_position
-        state.push_stack_op(
-            &mut exec_step,
-            RW::WRITE,
-            stack_b_position,
-            stack_a_value_read,
-        )?;
-        state.push_stack_op(
-            &mut exec_step,
-            RW::WRITE,
-            stack_a_position,
-            stack_b_value_read,
-        )?;
+        state.stack_write(&mut exec_step, stack_b_position, stack_a_value_read)?;
+        state.stack_write(&mut exec_step, stack_a_position, stack_b_value_read)?;
 
         Ok(vec![exec_step])
     }
@@ -54,9 +35,8 @@ impl<const N: usize> Opcode for Swap<N> {
 
 #[cfg(test)]
 mod swap_tests {
-    use super::*;
     use crate::mock::BlockData;
-    use crate::operation::StackOp;
+    use crate::operation::{StackOp, RW};
     use eth_types::{bytecode, evm_types::StackAddress, geth_types::GethData, Word};
     use itertools::Itertools;
     use mock::test_ctx::{helpers::*, TestContext};
