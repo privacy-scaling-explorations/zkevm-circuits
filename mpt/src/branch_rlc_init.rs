929fb5825f39e10dedb6ed65c697decb2afae2ba
Miha Stopar <stopar.miha@gmail.com>
2022-06-02 12:14:17 +0200

    missing branch init constraints

--- mpt/src/branch_rlc_init.rs
+++ mpt/src/branch_rlc_init.rs
@@ -61,8 +61,31 @@ impl<F: FieldExt> BranchRLCInitChip<F> {
             let two_rlp_bytes_c = meta.query_advice(s_advices[0], Rotation::cur());
             let three_rlp_bytes_c = meta.query_advice(s_advices[1], Rotation::cur());
 
-            // TODO: two_rlp_bytes and three_rlp_bytes are bools for S and C
-            // TODO: two_rlp_bytes + three_rlp_bytes = 1 for S and C
+            let one = Expression::Constant(F::one());
+            constraints.push((
+                "branch init two_rlp_bytes_s boolean",
+                q_enable.clone() * two_rlp_bytes_s.clone() * (one.clone() - two_rlp_bytes_s.clone()),
+            ));
+            constraints.push((
+                "branch init two_rlp_bytes_c boolean",
+                q_enable.clone() * two_rlp_bytes_c.clone() * (one.clone() - two_rlp_bytes_c.clone()),
+            ));
+            constraints.push((
+                "branch init three_rlp_bytes_s boolean",
+                q_enable.clone() * three_rlp_bytes_s.clone() * (one.clone() - three_rlp_bytes_s.clone()),
+            ));
+            constraints.push((
+                "branch init three_rlp_bytes_c boolean",
+                q_enable.clone() * three_rlp_bytes_c.clone() * (one.clone() - three_rlp_bytes_c.clone()),
+            ));
+            constraints.push((
+                "branch init two_rlp_bytes_s + three_rlp_bytes_s = 1",
+                q_enable.clone() * (one.clone() - two_rlp_bytes_s.clone() - three_rlp_bytes_s.clone()),
+            ));
+            constraints.push((
+                "branch init two_rlp_bytes_c + three_rlp_bytes_c = 1",
+                q_enable.clone() * (one.clone() - two_rlp_bytes_c.clone() - three_rlp_bytes_c.clone()),
+            ));
 
             let s_rlp1 = meta.query_advice(s_advices[2], Rotation::cur());
             let s_rlp2 = meta.query_advice(s_advices[3], Rotation::cur());
