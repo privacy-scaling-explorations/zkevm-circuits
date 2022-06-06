f9c1d334194291b489bcd277ae3f58f70620ef47
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-06-06 12:39:22 +0530

    tests(bus-mapping): codesize steps

--- bus-mapping/src/evm/opcodes/codesize.rs
+++ bus-mapping/src/evm/opcodes/codesize.rs
@@ -33,3 +33,60 @@ impl Opcode for Codesize {
         Ok(vec![exec_step])
     }
 }
+
+#[cfg(test)]
+mod codesize_tests {
+    use eth_types::{
+        bytecode,
+        evm_types::{OpcodeId, StackAddress},
+        geth_types::GethData,
+        Word,
+    };
+    use mock::{
+        test_ctx::helpers::{account_0_code_account_1_no_code, tx_from_1_to_0},
+        TestContext,
+    };
+
+    use crate::{
+        circuit_input_builder::ExecState,
+        mock::BlockData,
+        operation::{StackOp, RW},
+    };
+
+    #[test]
+    fn codesize_opcode_impl() {
+        let code = bytecode! {
+            CODESIZE
+            STOP
+        };
+
+        let block: GethData = TestContext::<2, 1>::new(
+            None,
+            account_0_code_account_1_no_code(code),
+            tx_from_1_to_0,
+            |block, _tx| block.number(0xcafeu64),
+        )
+        .unwrap()
+        .into();
+
+        let mut builder = BlockData::new_from_geth_data(block.clone()).new_circuit_input_builder();
+        builder
+            .handle_block(&block.eth_block, &block.geth_traces)
+            .unwrap();
+
+        let step = builder.block.txs()[0]
+            .steps()
+            .iter()
+            .find(|step| step.exec_state == ExecState::Op(OpcodeId::CODESIZE))
+            .unwrap();
+
+        assert_eq!(step.bus_mapping_instance.len(), 1);
+
+        let op = &builder.block.container.stack[step.bus_mapping_instance[0].as_usize()];
+        assert_eq!(op.rw(), RW::WRITE);
+        assert_eq!(
+            op.op(),
+            &StackOp::new(1, StackAddress::from(1023), Word::from(2))
+        );
+    }
+}
