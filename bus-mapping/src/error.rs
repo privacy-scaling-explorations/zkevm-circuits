0f4c5658a8fe5e47a67b1532dfb2e22c3ec16cfc
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-06-09 15:12:24 +0200

    fix(bus-mapping): handle max code size err

--- bus-mapping/src/error.rs
+++ bus-mapping/src/error.rs
@@ -25,6 +25,8 @@ pub enum Error {
     StorageKeyNotFound(Address, Word),
     /// Code not found in the CodeDB
     CodeNotFound(H256),
+    /// Code is above maximum permitted size.
+    CodesizeNotPermitted,
     /// Unable to figure out error at a [`GethExecStep`]
     UnexpectedExecStepError(&'static str, GethExecStep),
     /// Invalid [`eth_types::GethExecTrace`] due to an invalid/unexpected value
