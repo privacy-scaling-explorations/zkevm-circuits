9666e11dd5920c749d1c8272b0474be426d33c7d
Zhang Zhuo <mycinbrin@gmail.com>
2022-06-14 16:54:27 +0800

    feat(evm-circuit): add dummy gadget (#563)
    * feat(evm-circuit): add dummy gadget; make sha3 uses dummy gadget; enable erc20 integration tests
    
    * some comments

--- zkevm-circuits/src/evm_circuit/witness.rs
+++ zkevm-circuits/src/evm_circuit/witness.rs
@@ -1214,6 +1214,7 @@ impl From<&circuit_input_builder::ExecStep> for ExecutionState {
                     OpcodeId::DIFFICULTY | OpcodeId::BASEFEE => ExecutionState::BLOCKCTXU256,
                     OpcodeId::GAS => ExecutionState::GAS,
                     OpcodeId::SELFBALANCE => ExecutionState::SELFBALANCE,
+                    OpcodeId::SHA3 => ExecutionState::SHA3,
                     OpcodeId::SHR => ExecutionState::SHR,
                     OpcodeId::SLOAD => ExecutionState::SLOAD,
                     OpcodeId::SSTORE => ExecutionState::SSTORE,
