b92e1ac74a2ee07fed7f9b24f7c4c98ebad6fe8c
ChihChengLiang <chihchengliang@gmail.com>
2022-06-30 22:46:49 +0100

    rm absorb and mixing

--- keccak256/src/permutation.rs
+++ keccak256/src/permutation.rs
@@ -1,10 +1,8 @@
 #![allow(clippy::type_complexity)]
 #![allow(clippy::too_many_arguments)]
-pub(crate) mod absorb;
 pub mod circuit;
 pub(crate) mod generic;
 pub(crate) mod iota;
-pub(crate) mod mixing;
 pub(crate) mod pi;
 pub(crate) mod rho;
 pub(crate) mod rho_helpers;
