223027ca2d7184527245e30903e6ecf2d6496919
lightsing <light.tsing@gmail.com>
2022-07-26 12:42:23 +0800

    fix tracer_tests

--- bus-mapping/src/circuit_input_builder/tracer_tests.rs
+++ bus-mapping/src/circuit_input_builder/tracer_tests.rs
@@ -410,6 +410,7 @@ fn tracer_err_address_collision() {
         .find(|(_, s)| s.op == OpcodeId::CREATE2)
         .unwrap();
     let next_step = block.geth_traces[0].struct_logs.get(index + 1);
+    let memory = next_step.unwrap().memory.clone();
 
     let create2_address: Address = {
         // get first RETURN
@@ -428,6 +429,7 @@ fn tracer_err_address_collision() {
     // Set up call context at CREATE2
     builder.tx_ctx.call_is_success.push(false);
     builder.state_ref().push_call(mock_internal_create());
+    builder.state_ref().call_ctx_mut().unwrap().memory = memory;
     // Set up account and contract that exist during the second CREATE2
     builder.builder.sdb.set_account(
         &ADDR_B,
@@ -645,6 +647,10 @@ fn tracer_err_invalid_code() {
     .unwrap()
     .into();
 
+    println!(
+        "{}",
+        serde_json::to_string(&block.geth_traces[0].struct_logs).unwrap()
+    );
     // get last RETURN
     let (index, step) = block.geth_traces[0]
         .struct_logs
@@ -660,6 +666,7 @@ fn tracer_err_invalid_code() {
     // Set up call context at RETURN
     builder.tx_ctx.call_is_success.push(false);
     builder.state_ref().push_call(mock_internal_create());
+    builder.state_ref().call_ctx_mut().unwrap().memory = step.memory.clone();
     assert_eq!(
         builder.state_ref().get_step_err(step, next_step).unwrap(),
         Some(ExecError::InvalidCreationCode)
@@ -1573,6 +1580,7 @@ fn create2_address() {
         .unwrap();
     let next_step_return = block.geth_traces[0].struct_logs.get(index_return + 1);
     let addr_expect = next_step_return.unwrap().stack.last().unwrap();
+    let memory = next_step_return.unwrap().memory.clone();
 
     // get CREATE2
     let step_create2 = block.geth_traces[0]
@@ -1584,6 +1592,7 @@ fn create2_address() {
     // Set up call context at CREATE2
     builder.tx_ctx.call_is_success.push(false);
     builder.state_ref().push_call(mock_internal_create());
+    builder.state_ref().call_ctx_mut().unwrap().memory = memory;
     let addr = builder.state_ref().create2_address(step_create2).unwrap();
 
     assert_eq!(addr.to_word(), addr_expect);
