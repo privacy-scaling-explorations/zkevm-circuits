48d4e0c68a2221adc7c7b8fc6735c6ab37de131c
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-08-17 12:00:12 +0800

    feat: muladd gadget fewer columns, more rows

--- gadgets/src/mul_add.rs
+++ gadgets/src/mul_add.rs
@@ -1,10 +1,23 @@
 //! Chip that implements instructions to check: a * b + c == d (mod 2^256) where
 //! a, b, c and d are all 256-bit words.
+//!
+//! The circuit layout is as follows:
+#[rustfmt::skip]
+// | q_enable | q_step | col0      | col1      | col2      | col3      | col4      |
+// |----------|--------|-----------|-----------|-----------|-----------|-----------|
+// | 1        | 1      | a_limb0   | a_limb1   | a_limb2   | a_limb3   | -         |
+// | 1        | 0      | b_limb0   | b_limb1   | b_limb2   | b_limb3   | -         |
+// | 1        | 0      | c_lo      | c_hi      | d_lo      | d_hi      | -         |
+// | 1        | 0      | carry_lo0 | carry_lo1 | carry_lo2 | carry_lo3 | carry_lo4 |
+// | 1        | 0      | carry_lo5 | carry_lo6 | carry_lo7 | carry_lo8 | -         |
+// | 1        | 0      | carry_hi0 | carry_hi1 | carry_hi2 | carry_hi3 | carry_hi4 |
+// | 1        | 0      | carry_hi5 | carry_hi6 | carry_hi7 | carry_hi8 | -         |
+// |----------|--------|-----------|-----------|-----------|-----------|-----------|
 
 use eth_types::{Field, ToLittleEndian, Word};
 use halo2_proofs::{
     circuit::Region,
-    plonk::{Advice, Column, ConstraintSystem, Error, Expression, VirtualCells},
+    plonk::{Advice, Column, ConstraintSystem, Error, Expression, Selector, VirtualCells},
     poly::Rotation,
 };
 
@@ -13,10 +26,23 @@ use crate::util::{expr_from_bytes, pow_of_two, split_u256, split_u256_limb64, Ex
 /// Config for the MulAddChip.
 #[derive(Clone)]
 pub struct MulAddConfig<F> {
-    /// Radix of contributions to the low 128-bit of the product.
-    pub carry_lo: [Column<Advice>; 9],
-    /// Radix of contributions to the low 256-bit of the product.
-    pub carry_hi: [Column<Advice>; 9],
+    /// Whether the row represents the first step.
+    pub q_step: Selector,
+    /// First of the columns which we use over multiple rows to represent the
+    /// schema described above.
+    pub col0: Column<Advice>,
+    /// Second of the columns which we use over multiple rows to represent the
+    /// schema described above.
+    pub col1: Column<Advice>,
+    /// Third of the columns which we use over multiple rows to represent the
+    /// schema described above.
+    pub col2: Column<Advice>,
+    /// Fourth of the columns which we use over multiple rows to represent the
+    /// schema described above.
+    pub col3: Column<Advice>,
+    /// Fifth of the columns which we use over multiple rows to represent the
+    /// schema described above.
+    pub col4: Column<Advice>,
     /// Sum of the parts higher than 256-bit in the product.
     pub overflow: Expression<F>,
 }
@@ -34,35 +60,39 @@ impl<F: Field> MulAddChip<F> {
     pub fn configure(
         meta: &mut ConstraintSystem<F>,
         q_enable: impl FnOnce(&mut VirtualCells<'_, F>) -> Expression<F>,
-        a_limbs: impl FnOnce(&mut VirtualCells<F>) -> [Expression<F>; 4],
-        b_limbs: impl FnOnce(&mut VirtualCells<F>) -> [Expression<F>; 4],
-        c_lo: impl FnOnce(&mut VirtualCells<F>) -> Expression<F>,
-        c_hi: impl FnOnce(&mut VirtualCells<F>) -> Expression<F>,
-        d_lo: impl FnOnce(&mut VirtualCells<F>) -> Expression<F>,
-        d_hi: impl FnOnce(&mut VirtualCells<F>) -> Expression<F>,
     ) -> MulAddConfig<F> {
-        let carry_lo = [(); 9].map(|_| meta.advice_column());
-        let carry_hi = [(); 9].map(|_| meta.advice_column());
+        let q_step = meta.complex_selector();
+        let col0 = meta.advice_column();
+        let col1 = meta.advice_column();
+        let col2 = meta.advice_column();
+        let col3 = meta.advice_column();
+        let col4 = meta.advice_column();
         let mut overflow = 0.expr();
 
         meta.create_gate("mul add gate", |meta| {
             let q_enable = q_enable(meta);
-            let carry_los = carry_lo
-                .iter()
-                .map(|c| meta.query_advice(*c, Rotation::cur()))
-                .collect::<Vec<Expression<F>>>();
-            let carry_his = carry_hi
-                .iter()
-                .map(|c| meta.query_advice(*c, Rotation::cur()))
-                .collect::<Vec<Expression<F>>>();
+            let q_step = meta.query_selector(q_step);
 
-            let c_lo = c_lo(meta);
-            let c_hi = c_hi(meta);
-            let d_lo = d_lo(meta);
-            let d_hi = d_hi(meta);
+            let a_limbs =
+                [col0, col1, col2, col3].map(|column| meta.query_advice(column, Rotation::cur()));
+            let b_limbs =
+                [col0, col1, col2, col3].map(|column| meta.query_advice(column, Rotation::next()));
 
-            let a_limbs = a_limbs(meta);
-            let b_limbs = b_limbs(meta);
+            let c_lo = meta.query_advice(col0, Rotation(2));
+            let c_hi = meta.query_advice(col1, Rotation(2));
+            let d_lo = meta.query_advice(col2, Rotation(2));
+            let d_hi = meta.query_advice(col3, Rotation(2));
+
+            let carry_los = [col0, col1, col2, col3, col4]
+                .map(|col| meta.query_advice(col, Rotation(3)))
+                .into_iter()
+                .chain([col0, col1, col2, col3].map(|col| meta.query_advice(col, Rotation(4))))
+                .collect::<Vec<Expression<F>>>();
+            let carry_his = [col0, col1, col2, col3, col4]
+                .map(|col| meta.query_advice(col, Rotation(5)))
+                .into_iter()
+                .chain([col0, col1, col2, col3].map(|col| meta.query_advice(col, Rotation(6))))
+                .collect::<Vec<Expression<F>>>();
 
             let carry_lo_expr = expr_from_bytes(&carry_los);
             let carry_hi_expr = expr_from_bytes(&carry_his);
@@ -92,12 +122,16 @@ impl<F: Field> MulAddChip<F> {
 
             [check_a, check_b]
                 .into_iter()
-                .map(move |poly| q_enable.clone() * poly)
+                .map(move |poly| q_enable.clone() * q_step.clone() * poly)
         });
 
         MulAddConfig {
-            carry_lo,
-            carry_hi,
+            q_step,
+            col0,
+            col1,
+            col2,
+            col3,
+            col4,
             overflow,
         }
     }
@@ -138,35 +172,120 @@ impl<F: Field> MulAddChip<F> {
         let (carry_hi, _) = temp.overflowing_add(carry_lo_minus_d_hi);
         let carry_hi = carry_hi >> 128;
 
-        self.config
-            .carry_lo
-            .iter()
-            .zip(carry_lo.to_le_bytes().iter())
-            .enumerate()
-            .map(|(i, (column, byte))| {
-                region.assign_advice(
-                    || format!("carry_lo({}): offset: {}", i, offset),
-                    *column,
-                    offset,
-                    || Ok(F::from(*byte as u64)),
-                )
-            })
-            .collect::<Result<Vec<_>, _>>()?;
+        // enable q_step at rotation 0.
+        self.config.q_step.enable(region, offset)?;
 
-        self.config
-            .carry_hi
-            .iter()
-            .zip(carry_hi.to_le_bytes().iter())
-            .enumerate()
-            .map(|(i, (column, byte))| {
-                region.assign_advice(
-                    || format!("carry_hi({}): offset: {}", i, offset),
-                    *column,
-                    offset,
-                    || Ok(F::from(*byte as u64)),
-                )
-            })
-            .collect::<Result<Vec<_>, _>>()?;
+        // a limbs.
+        for (i, (column, value)) in [
+            self.config.col0,
+            self.config.col1,
+            self.config.col2,
+            self.config.col3,
+        ]
+        .into_iter()
+        .zip(a_limbs)
+        .enumerate()
+        {
+            region.assign_advice(
+                || format!("a limb ({})", i),
+                column,
+                offset,
+                || Ok(F::from(value.as_u64())),
+            )?;
+        }
+
+        // b limbs.
+        for (i, (column, value)) in [
+            self.config.col0,
+            self.config.col1,
+            self.config.col2,
+            self.config.col3,
+        ]
+        .into_iter()
+        .zip(b_limbs)
+        .enumerate()
+        {
+            region.assign_advice(
+                || format!("b limb ({})", i),
+                column,
+                offset + 1,
+                || Ok(F::from(value.as_u64())),
+            )?;
+        }
+
+        // c_lo, c_hi, d_lo, d_hi.
+        region.assign_advice(
+            || "c_lo",
+            self.config.col0,
+            offset + 2,
+            || Ok(F::from_u128(c_lo.as_u128())),
+        )?;
+        region.assign_advice(
+            || "c_hi",
+            self.config.col1,
+            offset + 2,
+            || Ok(F::from_u128(c_hi.as_u128())),
+        )?;
+        region.assign_advice(
+            || "d_lo",
+            self.config.col2,
+            offset + 2,
+            || Ok(F::from_u128(d_lo.as_u128())),
+        )?;
+        region.assign_advice(
+            || "d_hi",
+            self.config.col3,
+            offset + 2,
+            || Ok(F::from_u128(d_hi.as_u128())),
+        )?;
+
+        // carry_lo.
+        for (i, ((col, rot), val)) in [
+            (self.config.col0, offset + 3),
+            (self.config.col1, offset + 3),
+            (self.config.col2, offset + 3),
+            (self.config.col3, offset + 3),
+            (self.config.col4, offset + 3),
+            (self.config.col0, offset + 4),
+            (self.config.col1, offset + 4),
+            (self.config.col2, offset + 4),
+            (self.config.col3, offset + 4),
+        ]
+        .into_iter()
+        .zip(carry_lo.to_le_bytes().iter())
+        .enumerate()
+        {
+            region.assign_advice(
+                || format!("carry lo ({})", i),
+                col,
+                rot,
+                || Ok(F::from(*val as u64)),
+            )?;
+        }
+
+        // carry_hi.
+        for (i, ((col, rot), val)) in [
+            (self.config.col0, offset + 5),
+            (self.config.col1, offset + 5),
+            (self.config.col2, offset + 5),
+            (self.config.col3, offset + 5),
+            (self.config.col4, offset + 5),
+            (self.config.col0, offset + 6),
+            (self.config.col1, offset + 6),
+            (self.config.col2, offset + 6),
+            (self.config.col3, offset + 6),
+        ]
+        .into_iter()
+        .zip(carry_hi.to_le_bytes().iter())
+        .enumerate()
+        {
+            region.assign_advice(
+                || format!("carry hi ({})", i),
+                col,
+                rot,
+                || Ok(F::from(*val as u64)),
+            )?;
+        }
 
         Ok(())
     }
@@ -181,21 +300,15 @@ mod test {
         circuit::SimpleFloorPlanner,
         dev::MockProver,
         pairing::bn256::Fr as Fp,
-        plonk::{Advice, Circuit, Column, Selector},
-        poly::Rotation,
+        plonk::{Circuit, Selector},
     };
     use rand::Rng;
 
-    use crate::{
-        mul_add::{MulAddChip, MulAddConfig},
-        util::{split_u256, split_u256_limb64, Expr},
-    };
+    use crate::mul_add::{MulAddChip, MulAddConfig};
 
     macro_rules! try_test_circuit {
         ($values:expr) => {{
-            // TODO: remove zk blinding factors in halo2 to restore the
-            // correct k (without the extra + 2).
-            let k = usize::BITS - $values.len().leading_zeros() + 2;
+            let k = 10;
             let circuit = TestCircuit::<Fp> {
                 values: $values,
                 _marker: PhantomData,
@@ -207,9 +320,7 @@ mod test {
 
     macro_rules! try_test_circuit_error {
         ($values:expr) => {{
-            // TODO: remove zk blinding factors in halo2 to restore the
-            // correct k (without the extra + 2).
-            let k = usize::BITS - $values.len().leading_zeros() + 2;
+            let k = 10;
             let circuit = TestCircuit::<Fp> {
                 values: $values,
                 _marker: PhantomData,
@@ -232,9 +343,6 @@ mod test {
         #[derive(Clone)]
         struct TestCircuitConfig<F> {
             q_enable: Selector,
-            a_limbs: [Column<Advice>; 4],
-            b_limbs: [Column<Advice>; 4],
-            d: [Column<Advice>; 2],
             mul_config: MulAddConfig<F>,
         }
 
@@ -251,24 +359,9 @@ mod test {
 
             fn configure(meta: &mut halo2_proofs::plonk::ConstraintSystem<F>) -> Self::Config {
                 let q_enable = meta.complex_selector();
-                let a_limbs = [(); 4].map(|_| meta.advice_column());
-                let b_limbs = [(); 4].map(|_| meta.advice_column());
-                let d = [(); 2].map(|_| meta.advice_column());
-                let mul_config = MulAddChip::configure(
-                    meta,
-                    |meta| meta.query_selector(q_enable),
-                    |meta| a_limbs.map(|a| meta.query_advice(a, Rotation::cur())),
-                    |meta| b_limbs.map(|b| meta.query_advice(b, Rotation::cur())),
-                    |_meta| 0.expr(), // c == 0 for multiplication
-                    |_meta| 0.expr(), // c == 0 for multiplication
-                    |meta| meta.query_advice(d[0], Rotation::cur()),
-                    |meta| meta.query_advice(d[1], Rotation::cur()),
-                );
+                let mul_config = MulAddChip::configure(meta, |meta| meta.query_selector(q_enable));
                 Self::Config {
                     q_enable,
-                    a_limbs,
-                    b_limbs,
-                    d,
                     mul_config,
                 }
             }
@@ -279,49 +372,14 @@ mod test {
                 mut layouter: impl halo2_proofs::circuit::Layouter<F>,
             ) -> Result<(), halo2_proofs::plonk::Error> {
                 let chip = MulAddChip::construct(config.mul_config);
-                let values = self
-                    .values
-                    .iter()
-                    .map(|(a, b, d)| (split_u256_limb64(a), split_u256_limb64(b), split_u256(d)))
-                    .collect::<Vec<([Word; 4], [Word; 4], (Word, Word))>>();
-
                 layouter.assign_region(
                     || "witness",
                     |mut region| {
-                        for (offset, (a_limbs, b_limbs, (d_lo, d_hi))) in values.iter().enumerate()
-                        {
+                        let mut offset = 0;
+                        for (a, b, d) in self.values.iter() {
                             config.q_enable.enable(&mut region, offset)?;
-                            for (column, a_limb) in config.a_limbs.into_iter().zip(a_limbs) {
-                                region.assign_advice(
-                                    || format!("a_limb: {}", offset),
-                                    column,
-                                    offset,
-                                    || Ok(F::from(a_limb.as_u64())),
-                                )?;
-                            }
-                            for (column, b_limb) in config.b_limbs.into_iter().zip(b_limbs) {
-                                region.assign_advice(
-                                    || format!("b_limb: {}", offset),
-                                    column,
-                                    offset,
-                                    || Ok(F::from(b_limb.as_u64())),
-                                )?;
-                            }
-                            region.assign_advice(
-                                || format!("d_lo: {}", offset),
-                                config.d[0],
-                                offset,
-                                || Ok(F::from_u128(d_lo.low_u128())),
-                            )?;
-                            region.assign_advice(
-                                || format!("d_hi: {}", offset),
-                                config.d[1],
-                                offset,
-                                || Ok(F::from_u128(d_hi.low_u128())),
-                            )?;
-
-                            let (a, b, d) = self.values[offset];
-                            chip.assign(&mut region, offset, [a, b, Word::zero(), d])?;
+                            chip.assign(&mut region, offset, [*a, *b, Word::zero(), *d])?;
+                            offset += 7
                         }
                         Ok(())
                     },
@@ -334,8 +392,9 @@ mod test {
         }
 
         let mut rng = rand::thread_rng();
-        let mut values = Vec::with_capacity(100);
-        for _ in 0..100 {
+        let n = 100;
+        let mut values = Vec::with_capacity(n);
+        for _ in 0..n {
             let a = rand_word();
             let b = rand_word();
             let (d, _) = a.overflowing_mul(b);
@@ -344,7 +403,6 @@ mod test {
 
         try_test_circuit!(values.clone());
         try_test_circuit_error!(values
-            .clone()
             .into_iter()
             .map(|(a, b, d)| {
                 if rng.gen::<bool>() {
