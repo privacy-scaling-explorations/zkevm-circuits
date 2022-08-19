5a95d73be38aeb9d4a434b31723b711039b55957
Miha Stopar <stopar.miha@gmail.com>
2022-08-19 16:31:19 +0200

    Documenting account_non_existing

--- mpt/src/account_non_existing.rs
+++ mpt/src/account_non_existing.rs
@@ -1,5 +1,4 @@
 use halo2_proofs::{
-    circuit::Chip,
     plonk::{Advice, Column, ConstraintSystem, Expression, Fixed, VirtualCells},
     poly::Rotation,
 };
@@ -34,17 +33,16 @@ For example, the row might be:
 
 We are proving that there is no account at the specified address. There are two versions of proof:
     1. A leaf is returned by getProof that is not at the required address (we call this a wrong leaf).
-    In this case, the `ACCOUNT_NON_EXISTING` row contains the nibbles of the address that was enquired
+    In this case, the `ACCOUNT_NON_EXISTING` row contains the nibbles of the address (the nibbles that remain
+    after the nibbles used for traversing through the branches are removed) that was enquired
     while `ACCOUNT_LEAF_KEY` row contains the nibbles of the wrong leaf. We need to prove that
     the difference is nonzero. This way we prove that there exists some account which has some
     number of the starting nibbles the same as the enquired address (the path through branches
-    above the leaf), but at the same time the whole address is not the same.
-    // TODO: we need to ensure that the enquired address next nibble is the same as position of the
-    wrong leaf in the last branch.
+    above the leaf), but at the same time the full address is not the same - the nibbles stored in a leaf differ.
     2. A branch is the last element of the getProof response and there is a nil object
     at the address position. Placeholder account leaf is added in this case.
-    In this case, the `ACCOUNT_NON_EXISTING` row contains just zeros as it is not needed.
-    We just need to prove that the branch contains nil object (128) at the enquired address.
+    In this case, the `ACCOUNT_NON_EXISTING` row contains the same nibbles as `ACCOUNT_LEAF_KEY` and it is
+    not needed. We just need to prove that the branch contains nil object (128) at the enquired address.
 
 The whole account leaf looks like:
 [248,106,161,32,252,237,52,8,133,130,180,167,143,97,28,115,102,25,94,62,148,249,8,6,55,244,16,75,187,208,208,127,251,120,61,73,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -55,6 +53,27 @@ The whole account leaf looks like:
 [0,160,86,232,31,23,27,204,85,166,255,131,69,230,146,192,248,110,91,72,224,27,153,108,173,192,1,98,47,181,227,99,180,33,0,160,197,210,70,1,134,247,35,60,146,126,125,178,220,199,3,192,229,0,182,83,202,130,39,59,123,250,216,4,93,133,164,122]
 [0,160,86,232,31,23,27,204,85,166,255,131,69,230,146,192,248,110,91,72,224,27,153,108,173,192,1,98,47,181,227,99,180,33,0,160,197,210,70,1,134,247,35,60,146,126,125,178,220,199,3,192,229,0,182,83,202,130,39,59,123,250,216,4,93,133,164,122]
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+We can observe that the example account leaf above is not for non-existing account proof as the first and third
+rows contain the same nibbles (the difference is solely in RLP specific bytes which are not needed
+in `ACCOUNT_NON_EXISTING` row).
+
+For the example of non-existing account proof account leaf see below:
+
+[248 102 157 55 236 125 29 155 142 209 241 75 145 144 143 254 65 81 209 56 13 192 157 236 195 213 73 132 11 251 149 241 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 6]
+[248 102 157 55 236 125 29 155 142 209 241 75 145 144 143 254 65 81 209 56 13 192 157 236 195 213 73 132 11 251 149 241 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 4]
+[1 0 0 56 133 130 180 167 143 97 28 115 102 25 94 62 148 249 8 6 55 244 16 75 187 208 208 127 251 120 61 73 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 18]
+[184 70 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 248 68 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 7]
+[184 70 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 248 68 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 8]
+[0 160 112 158 181 221 162 20 124 79 184 25 162 13 167 162 146 25 237 242 59 120 184 154 118 137 92 181 187 152 115 82 223 48 0 160 7 190 1 231 231 32 111 227 30 206 233 26 215 93 173 166 90 214 186 67 58 230 71 161 185 51 4 105 247 198 103 124 0 9]
+[0 160 112 158 181 221 162 20 124 79 184 25 162 13 167 162 146 25 237 242 59 120 184 154 118 137 92 181 187 152 115 82 223 48 0 160 7 190 1 231 231 32 111 227 30 206 233 26 215 93 173 166 90 214 186 67 58 230 71 161 185 51 4 105 247 198 103 124 0 11]
+[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 10]
+
+In this case, the nibbles in the third row are different from the nibbles in the first or second row. Here, we are
+proving that the account does not exist at the address which starts with the same nibbles as the leaf that is
+in the rows above (except for the `ACCOUNT_NON_EXISTING` row) and continues with nibbles `ACCOUNT_NON_EXISTING` row.
+
+Note that the selector (being 1 in this case) at `s_main.rlp1` specifies whether it is wrong leaf or nil case.
 */
 
 #[derive(Clone, Debug)]
@@ -207,7 +226,19 @@ impl<F: FieldExt> AccountNonExistingConfig<F> {
 
                 let address_rlc = meta.query_advice(address_rlc, Rotation::cur());
 
-                // Note: key_rlc_acc is computed as in account_leaf_key, but using non_existing_account row.
+                /*
+                Differently as for the other proofs, account-non-existing proof compares `address_rlc` with the address
+                stored in `ACCOUNT_NON_EXISTING` row, not in `ACCOUNT_LEAF_KEY` row.
+
+                The crucial thing is that we have a wrong leaf at the address (not exactly the same, just some starting
+                set of nibbles is the same) where we are proving there is no account.
+                If there would be an account at the specified address, it would be positioned in the branch where
+                the wrong account is positioned. Note that position is determined by the starting set of nibbles.
+                Once we add the remaining nibbles to the starting ones, we need to obtain the enquired address.
+                There is a constraint below which makes sure the remaining nibbles are different for wrong leaf
+                and the non-existing account (in the case of wrong leaf, while the case with nil being in branch
+                is different).
+                */
                 constraints.push((
                     "Account address RLC",
                     q_enable.clone()
