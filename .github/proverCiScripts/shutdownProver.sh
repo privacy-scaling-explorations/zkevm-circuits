12d78cace84d0a109a6f953c1f2fb102703f08d5
johnsonjie <32984960+johnsonjie@users.noreply.github.com>
2022-08-23 12:44:14 +0800

    Update shutdownProver.sh

--- .github/proverCiScripts/shutdownProver.sh
+++ .github/proverCiScripts/shutdownProver.sh
@@ -3,7 +3,7 @@
 #set -x
 
 profile="cirunner"
-provers_vpc_id="vpc-09fb44da782f32abb"
+provers_vpc_id="vpc-1176d875"
 
 running_prover=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[proverbench-multiAZ] Name=instance-state-name,Values=[running] Name=network-interface.vpc-id,Values=[$provers_vpc_id] --query "Reservations[*].Instances[*][InstanceId]" --output text)
 
