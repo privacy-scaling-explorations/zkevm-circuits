5a1b51220facfa527e57777d459457fd4fd883b3
Miha Stopar <stopar.miha@gmail.com>
2022-09-01 14:44:07 +0200

    comments for branch_hash_in_parent

--- mpt/src/branch/branch_hash_in_parent.rs
+++ mpt/src/branch/branch_hash_in_parent.rs
@@ -10,6 +10,59 @@ use crate::{
     param::{KECCAK_INPUT_WIDTH, KECCAK_OUTPUT_WIDTH, IS_BRANCH_S_PLACEHOLDER_POS, RLP_NUM, IS_BRANCH_C_PLACEHOLDER_POS, IS_S_BRANCH_NON_HASHED_POS, IS_C_BRANCH_NON_HASHED_POS}, columns::{MainCols, AccumulatorPair},
 };
 
+/*
+A branch occupies 19 rows:
+BRANCH.IS_INIT
+BRANCH.IS_CHILD 0
+...
+BRANCH.IS_CHILD 15
+BRANCH.IS_EXTENSION_NODE_S
+BRANCH.IS_EXTENSION_NODE_C
+
+Example:
+
+[1 0 1 0 248 241 0 248 241 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 160 164 92 78 34 81 137 173 236 78 208 145 118 128 60 46 5 176 8 229 165 42 222 110 4 252 228 93 243 26 160 241 85 0 160 95 174 59 239 229 74 221 53 227 115 207 137 94 29 119 126 56 209 55 198 212 179 38 213 219 36 111 62 46 43 176 168 1]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 160 60 157 212 182 167 69 206 32 151 2 14 23 149 67 58 187 84 249 195 159 106 68 203 199 199 65 194 33 215 102 71 138 0 160 60 157 212 182 167 69 206 32 151 2 14 23 149 67 58 187 84 249 195 159 106 68 203 199 199 65 194 33 215 102 71 138 1]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 160 21 230 18 20 253 84 192 151 178 53 157 0 9 105 229 121 222 71 120 109 159 109 9 218 254 1 50 139 117 216 194 252 0 160 21 230 18 20 253 84 192 151 178 53 157 0 9 105 229 121 222 71 120 109 159 109 9 218 254 1 50 139 117 216 194 252 1]
+[0 160 229 29 220 149 183 173 68 40 11 103 39 76 251 20 162 242 21 49 103 245 160 99 143 218 74 196 2 61 51 34 105 123 0 160 229 29 220 149 183 173 68 40 11 103 39 76 251 20 162 242 21 49 103 245 160 99 143 218 74 196 2 61 51 34 105 123 1]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 160 0 140 67 252 58 164 68 143 34 163 138 133 54 27 218 38 80 20 142 115 221 100 73 161 165 75 83 53 8 58 236 1 0 160 0 140 67 252 58 164 68 143 34 163 138 133 54 27 218 38 80 20 142 115 221 100 73 161 165 75 83 53 8 58 236 1 1]
+[0 160 149 169 206 0 129 86 168 48 42 127 100 73 109 90 171 56 216 28 132 44 167 14 46 189 224 213 37 0 234 165 140 236 0 160 149 169 206 0 129 86 168 48 42 127 100 73 109 90 171 56 216 28 132 44 167 14 46 189 224 213 37 0 234 165 140 236 1]
+[0 160 42 63 45 28 165 209 201 220 231 99 153 208 48 174 250 66 196 18 123 250 55 107 64 178 159 49 190 84 159 179 138 235 0 160 42 63 45 28 165 209 201 220 231 99 153 208 48 174 250 66 196 18 123 250 55 107 64 178 159 49 190 84 159 179 138 235 1]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 16]
+[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 17]
+
+The constraints in this file check whether the RLC of the whole branch is in its parent node
+at the proper position. The RLC is computed over the first 17 rows (the last 2 rows are reserved
+for the cases when the parent of the branch is an extension node).
+
+Let us say we have the following situation:
+
+Branch1:
+  node1_0       node3_0_RLC
+  ...           node3_0_RLC
+  node1_15      node3_0_RLC
+Branch2
+  node2_0
+  ...
+  node2_15
+
+Let us say we are checking `Branch2` to be at the proper position in `Branch1`.
+We compute `Branch2` RLC (the constraints for ensuring the proper RLC computation are in `branch_rlc.rs`).
+Let us say the `modified_node = 3` in `Branch1`. That means there is `node3_0_RLC` stored in all 16
+rows. We need to check that `(Branch2_RLC, node3_0_RLC)` is in the Keccak table which would mean
+that `hash(Branch2) = node3_0`.
+*/
+
 #[derive(Clone, Debug)]
 pub(crate) struct BranchHashInParentConfig<F> {
     _marker: PhantomData<F>,
@@ -32,9 +85,15 @@ impl<F: FieldExt> BranchHashInParentConfig<F> {
         let config = BranchHashInParentConfig { _marker: PhantomData };
         let one = Expression::Constant(F::from(1_u64));
 
-        // Note: branch in the first level cannot be shorter than 32 bytes (it is always hashed).
+        /*
+        When branch is in the first level of the account trie, we need to check whether
+        `hash(branch) = account_trie_root`. We do this by checking whether
+        `(branch_RLC, account_trie_root_RLC)` is in the Keccak table.
+        
+        Note: branch in the first level cannot be shorter than 32 bytes (it is always hashed).
+        */
         meta.lookup_any(
-            "branch in first level - hash compared to root",
+            "Branch in first level of account trie - hash compared to root",
             |meta| {
                 let mut constraints = vec![];
                 let q_not_first = meta.query_fixed(q_not_first, Rotation::cur());
@@ -88,10 +147,12 @@ impl<F: FieldExt> BranchHashInParentConfig<F> {
         );
 
         /*
-        Check whether hash of a branch is in parent branch:
-        (branch RLC, mod_node_hash_rlc retrieved from the previous branch) needs to be in keccak tabel
+        This is the scenario described at the top of the file.
+        When branch is in some other branch, we need to check whether
+        `hash(branch) = parent_branch_modified_node`. We do this by checking whether
+        `(branch_RLC, parent_branch_modified_node_RLC)` is in the Keccak table.
         */
-        meta.lookup_any("Branch hash in parent", |meta| {
+        meta.lookup_any("Branch hash in parent branch", |meta| {
             let not_first_level = meta.query_advice(not_first_level, Rotation::cur());
 
             // -17 because we are in the last branch child (-16 takes us to branch init)
@@ -162,10 +223,11 @@ impl<F: FieldExt> BranchHashInParentConfig<F> {
         });
 
         /*
-        Similar as the gate above, but here the branch is not hashed. That means its RLC needs to
-        be the same as RLC of the node in the parent branch at `modified_node` position.
+        Similar as the gate above, but here the branch is not hashed.
+        Instead of checking `hash(branch) = parent_branch_modified_node`, we check whether
+        `branch_RLC = parent_branch_modified_node_RLC`.
         */
-        meta.create_gate("NON-HASHED branch hash in parent", |meta| {
+        meta.create_gate("NON-HASHED branch hash in parent branch", |meta| {
             let q_not_first = meta.query_fixed(q_not_first, Rotation::cur());
             let not_first_level = meta.query_advice(not_first_level, Rotation::cur());
 
