b346ab1540c1ca076a1a77e7bcf8171cb74100c7
DoHoonKim <dohoon@pi-lab.io>
2022-09-23 00:41:36 +0900

    Fix for changes

--- zkevm-circuits/src/evm_circuit/execution/extcodecopy.rs
+++ zkevm-circuits/src/evm_circuit/execution/extcodecopy.rs
@@ -59,7 +59,7 @@ impl<F: Field> ExecutionGadget<F> for ExtcodecopyGadget<F> {
         let memory_address = MemoryAddressGadget::construct(cb, memory_offset, memory_length);
 
         let tx_id = cb.call_context(None, CallContextFieldTag::TxId);
-        let mut reversion_info = cb.reversion_info(None);
+        let mut reversion_info = cb.reversion_info_read(None);
         let is_warm = cb.query_bool();
         cb.account_access_list_write(
             tx_id.expr(),
