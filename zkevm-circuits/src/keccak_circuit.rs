4a6b55a355e15a096427ca2209d3dcf94fd034ac
CPerezz <c.perezbaro@gmail.com>
2022-11-30 20:26:53 +0100

    remove: Benchmarks of discarted keccak impls

--- zkevm-circuits/src/keccak_circuit.rs
+++ zkevm-circuits/src/keccak_circuit.rs
@@ -4,4 +4,4 @@ pub mod keccak_packed_multi;
 /// Util
 pub mod util;
 
-pub use keccak_packed_multi::KeccakPackedConfig as KeccakConfig;
+pub use keccak_packed_multi::KeccakCircuitConfig as KeccakConfig;
