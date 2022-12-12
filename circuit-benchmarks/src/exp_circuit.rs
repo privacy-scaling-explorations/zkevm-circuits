1ed3449d1376d315c069cae43c77ebd0359e9696
Paul <chengyuejia@foxmail.com>
2022-12-12 15:34:29 +0800

    fmt

--- circuit-benchmarks/src/exp_circuit.rs
+++ circuit-benchmarks/src/exp_circuit.rs
@@ -61,7 +61,7 @@ mod tests {
         let mut transcript = Blake2bWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
 
         // Bench proof generation time
-        let proof_message = format!("Tx Circuit Proof generation with degree = {}", DEGREE);
+        let proof_message = format!("Exp Circuit Proof generation with degree = {}", DEGREE);
         let start2 = start_timer!(|| proof_message);
         create_proof::<
             KZGCommitmentScheme<Bn256>,
@@ -83,7 +83,7 @@ mod tests {
         end_timer!(start2);
 
         // Bench verification time
-        let start3 = start_timer!(|| "Tx Circuit Proof verification");
+        let start3 = start_timer!(|| "Exp Circuit Proof verification");
         let mut verifier_transcript = Blake2bRead::<_, G1Affine, Challenge255<_>>::init(&proof[..]);
         let strategy = SingleStrategy::new(&general_params);
 
