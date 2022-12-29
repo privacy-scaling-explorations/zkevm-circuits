6cb9b8e01dbdfbdc29b2de43a1893fa4c77b6030
Steven Gu <asongala@163.com>
2022-12-29 19:30:16 +0800

    Update `SarGadget` according to spec.

--- zkevm-circuits/src/evm_circuit/execution/sar.rs
+++ zkevm-circuits/src/evm_circuit/execution/sar.rs
@@ -6,45 +6,48 @@ use crate::evm_circuit::util::common_gadget::SameContextGadget;
 use crate::evm_circuit::util::constraint_builder::Transition::Delta;
 use crate::evm_circuit::util::constraint_builder::{ConstraintBuilder, StepStateTransition};
 use crate::evm_circuit::util::math_gadget::{IsEqualGadget, IsZeroGadget, LtGadget};
-use crate::evm_circuit::util::{from_bytes, sum, CachedRegion, Cell, Word};
+use crate::evm_circuit::util::{from_bytes, select, CachedRegion, Cell, Word};
 use crate::evm_circuit::witness::{Block, Call, ExecStep, Transaction};
 use crate::util::Expr;
 use array_init::array_init;
 use bus_mapping::evm::OpcodeId;
 use eth_types::{Field, ToLittleEndian, U256};
+use gadgets::util::split_u256;
 use halo2_proofs::circuit::Value;
 use halo2_proofs::plonk::Error;
 
-/// Construction of word shift right for `signed(a) >> shift == signed(b)`.
+/// SarGadget verifies SAR opcode.
+/// Verify signed word shift right as `signed(a) >> shift == signed(b)`;
+/// when `shift` is an unsigned word, but both `a` and `b` are signed words.
 #[derive(Clone, Debug)]
 pub(crate) struct SarGadget<F> {
     same_context: SameContextGadget<F>,
     shift: Word<F>,
     a: Word<F>,
     b: Word<F>,
-    // Four 64-bit limbs of word `a`
+    // Four 64-bit limbs of word `a`.
     a64s: [Cell<F>; 4],
-    // Four 64-bit limbs of word `b`
+    // Four 64-bit limbs of word `b`.
     b64s: [Cell<F>; 4],
     // Each of the four `a64s` limbs is split into two parts (`a64s_lo` and `a64s_hi`) at position
     // `shf_mod64`, `a64s_lo` is the lower `shf_mod64` bits.
     a64s_lo: [Cell<F>; 4],
     // `a64s_hi` is the higher `64 - shf_mod64` bits.
     a64s_hi: [Cell<F>; 4],
-    // shift[0] / 64
+    // `shift` lower part divides 64 as `shf_lo / 64`.
     shf_div64: Cell<F>,
-    // shift[0] % 64
+    // `shift` lower part mods 64 as `shf_lo % 64`.
     shf_mod64: Cell<F>,
     // 1 << shf_mod64
     p_lo: Cell<F>,
     // 1 << (64 - shf_mod64)
     p_hi: Cell<F>,
-    // p_top = is_neg * (u64::MAX - p_hi + 1))
+    // is_neg * (u64::MAX + 1 - p_hi)
     p_top: Cell<F>,
-    // is_neg = is_neg(a)
+    // Identify if `a` is a negative word.
     is_neg: LtGadget<F, 1>,
-    // shift < 256
-    shf_lt256: IsZeroGadget<F>,
+    // Identify if higher 128-bit part of `shift` is zero or not.
+    shf_hi_is_zero: IsZeroGadget<F>,
     // shf_div64 == 0
     shf_div64_eq0: IsZeroGadget<F>,
     // shf_div64 == 1
@@ -53,9 +56,9 @@ pub(crate) struct SarGadget<F> {
     shf_div64_eq2: IsEqualGadget<F>,
     // shf_div64 == 3
     shf_div64_eq3: IsEqualGadget<F>,
-    // a64s_lo[idx] < p_lo
+    // Verify `a64s_lo[idx]` should be less than `p_lo` when idx in `(0, 1, 2, 3)`.
     a64s_lo_lt_p_lo: [LtGadget<F, 16>; 4],
-    // a64s_hi[idx] < p_hi
+    // Verify `a64s_hi[idx]` should be less than `p_hi` when idx in `(0, 1, 2, 3)`.
     a64s_hi_lt_p_hi: [LtGadget<F, 16>; 4],
 }
 
@@ -71,7 +74,6 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
         let a = cb.query_word();
         let b = cb.query_word();
 
-        // pop a and shift from stack, and push b to the stack.
         cb.stack_pop(shift.expr());
         cb.stack_pop(a.expr());
         cb.stack_push(b.expr());
@@ -86,23 +88,33 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
         let p_hi = cb.query_cell();
         let p_top = cb.query_cell();
         let is_neg = LtGadget::construct(cb, 127.expr(), a.cells[31].expr());
-        let shf_lt256 = IsZeroGadget::construct(cb, sum::expr(&shift.cells[1..32]));
+
+        // Split `shift` to lower and higher parts.
+        let shf_lo = from_bytes::expr(&shift.cells[0..16]);
+        let shf_hi = from_bytes::expr(&shift.cells[16..32]);
+        let shf_hi_is_zero = IsZeroGadget::construct(cb, shf_hi.expr());
+
         for idx in 0..4 {
             let offset = idx * N_BYTES_U64;
 
             // a64s constraint
             cb.require_equal(
-                "a64s[idx] == from_bytes(a[8 * idx..8 * (idx + 1)])",
+                "a64s[idx] should be equal to corresponding part of a",
                 a64s[idx].expr(),
                 from_bytes::expr(&a.cells[offset..offset + N_BYTES_U64]),
             );
 
             // b64s constraint
             cb.require_equal(
-                 "b64s[idx] * shf_lt256 + is_neg * (1 - shf_lt256) * u64::MAX == from_bytes(b[8 * idx..8 * (idx + 1)])",
-                 b64s[idx].expr() * shf_lt256.expr() + is_neg.expr() * (1.expr() - shf_lt256.expr()) * u64::MAX.expr(),
-                 from_bytes::expr(&b.cells[offset..offset + N_BYTES_U64]),
-             );
+                "b64s[idx] should be equal to corresponding part of a",
+                select::expr(
+                    shf_hi_is_zero.expr(),
+                    b64s[idx].expr(),
+                    is_neg.expr() * u64::MAX.expr(),
+                ),
+                from_bytes::expr(&b.cells[offset..offset + N_BYTES_U64]),
+            );
+
             cb.require_equal(
                 "a64s[idx] == a64s_lo[idx] + a64s_hi[idx] * p_lo",
                 a64s[idx].expr(),
@@ -110,28 +122,28 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
             );
         }
 
-        // a64s_lo[idx] < p_lo
+        // Constrain `a64s_lo[idx] < p_lo`.
         let a64s_lo_lt_p_lo = array_init(|idx| {
             let lt = LtGadget::construct(cb, a64s_lo[idx].expr(), p_lo.expr());
-            cb.require_equal("a64s_lo[idx] < p_lo", lt.expr(), 1.expr());
+            cb.require_zero("a64s_lo[idx] < p_lo", 1.expr() - lt.expr());
             lt
         });
 
-        // a64s_hi[idx] < p_hi
+        // Constrain `a64s_hi[idx] < p_hi`.
         let a64s_hi_lt_p_hi = array_init(|idx| {
             let lt = LtGadget::construct(cb, a64s_hi[idx].expr(), p_hi.expr());
-            cb.require_equal("a64s_hi[idx] < p_hi", lt.expr(), 1.expr());
+            cb.require_zero("a64s_hi[idx] < p_hi", 1.expr() - lt.expr());
             lt
         });
 
-        // merge contraints
+        // Merge contraints
         let shf_div64_eq0 = IsZeroGadget::construct(cb, shf_div64.expr());
         let shf_div64_eq1 = IsEqualGadget::construct(cb, shf_div64.expr(), 1.expr());
         let shf_div64_eq2 = IsEqualGadget::construct(cb, shf_div64.expr(), 2.expr());
         let shf_div64_eq3 = IsEqualGadget::construct(cb, shf_div64.expr(), 3.expr());
 
         cb.require_equal(
-            "Constrain b64s[0]",
+            "Constrain merged b64s[0] value",
             b64s[0].expr(),
             (a64s_hi[0].expr() + a64s_lo[1].expr() * p_hi.expr()) * shf_div64_eq0.expr()
                 + (a64s_hi[1].expr() + a64s_lo[2].expr() * p_hi.expr()) * shf_div64_eq1.expr()
@@ -146,7 +158,7 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
                         - shf_div64_eq3.expr()),
         );
         cb.require_equal(
-            "Constrain b64s[1]",
+            "Constrain merged b64s[1] value",
             b64s[1].expr(),
             (a64s_hi[1].expr() + a64s_lo[2].expr() * p_hi.expr()) * shf_div64_eq0.expr()
                 + (a64s_hi[2].expr() + a64s_lo[3].expr() * p_hi.expr()) * shf_div64_eq1.expr()
@@ -159,7 +171,7 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
                         - shf_div64_eq2.expr()),
         );
         cb.require_equal(
-            "Constrain b64s[2]",
+            "Constrain merged b64s[2] value",
             b64s[2].expr(),
             (a64s_hi[2].expr() + a64s_lo[3].expr() * p_hi.expr()) * shf_div64_eq0.expr()
                 + (a64s_hi[3].expr() + p_top.expr()) * shf_div64_eq1.expr()
@@ -168,31 +180,45 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
                     * (1.expr() - shf_div64_eq0.expr() - shf_div64_eq1.expr()),
         );
         cb.require_equal(
-            "Constrain b64s[3]",
+            "Constrain merged b64s[3] value",
             b64s[3].expr(),
             (a64s_hi[3].expr() + p_top.expr()) * shf_div64_eq0.expr()
                 + is_neg.expr() * u64::MAX.expr() * (1.expr() - shf_div64_eq0.expr()),
         );
 
-        // shift constraint
+        // Shift constraint
         cb.require_equal(
-            "shift[0] == shf_mod64 + shf_div64 * 64",
-            shift.cells[0].expr(),
+            "shf_lo == shf_mod64 + shf_div64 * 64",
+            shf_lo.expr(),
             shf_mod64.expr() + shf_div64.expr() * 64.expr(),
         );
 
-        // p_lo == pow(2, shf_mod64)
+        // `is_neg` constraints
+        cb.require_boolean("is_neg is boolean", is_neg.expr());
         cb.add_lookup(
-            "Pow2 lookup",
+            "SignByte lookup for a and is_neg",
+            Lookup::Fixed {
+                tag: FixedTableTag::SignByte.expr(),
+                values: [
+                    a.cells[31].expr(),
+                    select::expr(is_neg.expr(), 255.expr(), 0.expr()),
+                    0.expr(),
+                ],
+            },
+        );
+
+        // Constrain `p_lo == pow(2, shf_mod64)`.
+        cb.add_lookup(
+            "Pow2 lookup for p_lo == pow(2, shf_mod64)",
             Lookup::Fixed {
                 tag: FixedTableTag::Pow2.expr(),
                 values: [shf_mod64.expr(), p_lo.expr(), 0.expr()],
             },
         );
 
-        // p_hi == pow(2, 64 - shf_mod64)
+        // Constrain `p_hi == pow(2, 64 - shf_mod64)`.
         cb.add_lookup(
-            "Pow2 lookup",
+            "Pow2 lookup for p_hi == pow(2, 64 - shf_mod64)",
             Lookup::Fixed {
                 tag: FixedTableTag::Pow2.expr(),
                 values: [64.expr() - shf_mod64.expr(), p_hi.expr(), 0.expr()],
@@ -206,6 +232,7 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
             gas_left: Delta(-OpcodeId::SAR.constant_gas_cost().expr()),
             ..Default::default()
         };
+
         let same_context = SameContextGadget::construct(cb, opcode, step_state_transition);
 
         Self {
@@ -223,7 +250,7 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
             p_hi,
             p_top,
             is_neg,
-            shf_lt256,
+            shf_hi_is_zero,
             shf_div64_eq0,
             shf_div64_eq1,
             shf_div64_eq2,
@@ -246,67 +273,67 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
         let indices = [step.rw_indices[0], step.rw_indices[1], step.rw_indices[2]];
         let [shift, a, b] = indices.map(|idx| block.rws[idx].stack_value());
 
-        self.a.assign(region, offset, Some(a.to_le_bytes()))?;
         self.shift
             .assign(region, offset, Some(shift.to_le_bytes()))?;
+        self.a.assign(region, offset, Some(a.to_le_bytes()))?;
         self.b.assign(region, offset, Some(b.to_le_bytes()))?;
 
-        let is_neg = is_neg(&a);
-        let shf0 = shift.to_le_bytes()[0] as usize;
-        let shf_div64 = shf0 / 64;
-        let shf_mod64 = shf0 % 64;
-        let p_lo: u128 = 1 << shf_mod64;
-        let p_hi: u128 = 1 << (64 - shf_mod64);
-        let p_top: u128 = if is_neg {
+        let is_neg = is_neg(a);
+        let (shf_lo, shf_hi) = split_u256(&shift);
+        let shf_lo = shf_lo.as_u128();
+        let shf_hi = shf_hi.as_u128();
+        let shf_div64 = shf_lo / 64;
+        let shf_mod64 = shf_lo % 64;
+        let p_lo = 1 << shf_mod64;
+        let p_hi = 1 << (64 - shf_mod64);
+        let p_top = if is_neg {
             u128::from(u64::MAX) + 1 - p_hi
         } else {
             0
         };
-        let shf_lt256 = shift
-            .to_le_bytes()
-            .iter()
-            .fold(0, |acc, val| acc + *val as u128)
-            - shf0 as u128;
         let a64s = a.0;
-        let mut a64s_lo = [0_u128; 4];
-        let mut a64s_hi = [0_u128; 4];
+        let mut a64s_lo = [0; 4];
+        let mut a64s_hi = [0; 4];
         for idx in 0..4 {
-            a64s_lo[idx] = u128::from(a64s[idx]) % p_lo;
             a64s_hi[idx] = u128::from(a64s[idx]) / p_lo;
+            a64s_lo[idx] = u128::from(a64s[idx]) % p_lo;
         }
         let mut b64s = if is_neg {
-            [0xFFFFFFFFFFFFFFFF_u128; 4]
+            [u128::from(u64::MAX); 4]
         } else {
-            [0_u128; 4]
+            [0; 4]
         };
-        b64s[3 - shf_div64 as usize] = a64s_hi[3] + p_top;
-        for k in 0..3 - shf_div64 {
-            b64s[k] = a64s_hi[k + shf_div64] + a64s_lo[k + shf_div64 + 1] * p_hi;
+        if shf_div64 < 4 {
+            let idx = shf_div64 as usize;
+            b64s[3 - idx] = a64s_hi[3] + p_top;
+            for k in 0..3 - idx {
+                b64s[k] = a64s_hi[k + idx] + a64s_lo[k + idx + 1] * p_hi;
+            }
         }
         self.a64s
             .iter()
-            .zip(a64s.iter())
-            .map(|(cell, val)| cell.assign(region, offset, Value::known(F::from(*val))))
+            .zip(a64s.into_iter())
+            .map(|(c, v)| c.assign(region, offset, Value::known(F::from(v))))
             .collect::<Result<Vec<_>, _>>()?;
         self.b64s
             .iter()
-            .zip(b64s.iter())
-            .map(|(cell, val)| cell.assign(region, offset, Value::known(F::from_u128(*val))))
+            .zip(b64s.into_iter())
+            .map(|(c, v)| c.assign(region, offset, Value::known(F::from_u128(v))))
             .collect::<Result<Vec<_>, _>>()?;
         self.a64s_lo
             .iter()
-            .zip(a64s_lo.iter())
-            .map(|(cell, val)| cell.assign(region, offset, Value::known(F::from_u128(*val))))
+            .zip(a64s_lo.into_iter())
+            .map(|(c, v)| c.assign(region, offset, Value::known(F::from_u128(v))))
             .collect::<Result<Vec<_>, _>>()?;
         self.a64s_hi
             .iter()
-            .zip(a64s_hi.iter())
-            .map(|(cell, val)| cell.assign(region, offset, Value::known(F::from_u128(*val))))
+            .zip(a64s_hi.into_iter())
+            .map(|(c, v)| c.assign(region, offset, Value::known(F::from_u128(v))))
             .collect::<Result<Vec<_>, _>>()?;
         self.shf_div64
-            .assign(region, offset, Value::known(F::from(shf_div64 as u64)))?;
+            .assign(region, offset, Value::known(F::from_u128(shf_div64)))?;
         self.shf_mod64
-            .assign(region, offset, Value::known(F::from(shf_mod64 as u64)))?;
+            .assign(region, offset, Value::known(F::from_u128(shf_mod64)))?;
         self.p_lo
             .assign(region, offset, Value::known(F::from_u128(p_lo)))?;
         self.p_hi
@@ -319,32 +346,33 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
             127.into(),
             u64::from(a.to_le_bytes()[31]).into(),
         )?;
-        self.shf_lt256
-            .assign(region, offset, F::from_u128(shf_lt256))?;
+        self.shf_hi_is_zero
+            .assign(region, offset, F::from_u128(shf_hi))?;
         self.shf_div64_eq0
-            .assign(region, offset, F::from(shf_div64 as u64))?;
+            .assign(region, offset, F::from_u128(shf_div64))?;
         self.shf_div64_eq1
-            .assign(region, offset, F::from(shf_div64 as u64), F::from(1))?;
+            .assign(region, offset, F::from_u128(shf_div64), F::from(1))?;
         self.shf_div64_eq2
-            .assign(region, offset, F::from(shf_div64 as u64), F::from(2))?;
+            .assign(region, offset, F::from_u128(shf_div64), F::from(2))?;
         self.shf_div64_eq3
-            .assign(region, offset, F::from(shf_div64 as u64), F::from(3))?;
+            .assign(region, offset, F::from_u128(shf_div64), F::from(3))?;
         self.a64s_lo_lt_p_lo
             .iter()
-            .zip(a64s_lo.iter())
-            .map(|(lt, val)| lt.assign(region, offset, F::from_u128(*val), F::from_u128(p_lo)))
+            .zip(a64s_lo.into_iter())
+            .map(|(l, v)| l.assign(region, offset, F::from_u128(v), F::from_u128(p_lo)))
             .collect::<Result<Vec<_>, _>>()?;
         self.a64s_hi_lt_p_hi
             .iter()
-            .zip(a64s_hi.iter())
-            .map(|(lt, val)| lt.assign(region, offset, F::from_u128(*val), F::from_u128(p_hi)))
+            .zip(a64s_hi.into_iter())
+            .map(|(l, v)| l.assign(region, offset, F::from_u128(v), F::from_u128(p_hi)))
             .collect::<Result<Vec<_>, _>>()?;
 
         Ok(())
     }
 }
 
-fn is_neg(x: &U256) -> bool {
+#[inline]
+fn is_neg(x: U256) -> bool {
     127 < x.to_le_bytes()[31]
 }
 
