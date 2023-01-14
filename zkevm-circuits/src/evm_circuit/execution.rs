308263add3d5eb3c1f603c1e7433b31624bffd0f
Steven <asongala@163.com>
2023-01-14 09:10:21 +0800

    Circuit for opcode `SAR` (#1036)
    * add SAR opcode
    
    * SarWordsGadget
    
    * fmt
    
    * SarWordsGadget
    
    fix data type
    
    use snake case
    
    fix name
    
    * refactor test mod
    
    * fix missing assignment of shf_div64_eq3
    
    * add shf_lt256
    
    need to fix SAR spec too
    
    * fix b64s initial
    
    * fix typo
    
    * Fix failed case.
    
    * fmt & fix
    
    * add N_BYTES_U64 & FixedTableTag, Lookup
    
    * add comment
    
    * refactor
    
    * fmt
    
    * fix
    
    * fix & halo2 update
    
    * fmt
    
    * fix overflow
    
    * fix
    
    * add a64s_hi[idx] < p_hi
    
    * Revert fix in `copy_circuit`.
    
    * Merge `SarWordsGadget` into `SarGadget`.
    
    * Fix unsigned overflow issue.
    
    * Update `SarGadget` according to spec.
    
    * Update test cases.
    
    * Add test cases from EIP-145, and `shf_mod64 < 64` constraint.
    
    * Split big test function to small ones.
    
    * Fix to check `shf_hi == 0` in `assign_exec_step` to reduce useless calculation for `b64s`, update circuit code and add test cases.
    
    * Add `p_top` constraint.
    
    * Replace `a64s` (and `b64s`) with `from_bytes::expr(&a.cells[8 * idx..8 * (idx + 1)])` (and `b.cells`).
    
    * Delete `is_neg` function and use it to calculate `is_neg` variable directly.
    
    * Replace function call `query_word` with `query_word_rlc`.
    
    * Fix to `shf_lt256`.
    
    Co-authored-by: Mickey <hedgefund996@gmail.com>
    Co-authored-by: Zhang Zhuo <mycinbrin@gmail.com>
    Co-authored-by: Luozhu <70309026+LuozhuZhang@users.noreply.github.com>

--- zkevm-circuits/src/evm_circuit/execution.rs
+++ zkevm-circuits/src/evm_circuit/execution.rs
@@ -81,6 +81,7 @@ mod push;
 mod return_revert;
 mod returndatacopy;
 mod returndatasize;
+mod sar;
 mod sdiv_smod;
 mod selfbalance;
 mod sha3;
@@ -142,6 +143,7 @@ use push::PushGadget;
 use return_revert::ReturnRevertGadget;
 use returndatacopy::ReturnDataCopyGadget;
 use returndatasize::ReturnDataSizeGadget;
+use sar::SarGadget;
 use sdiv_smod::SignedDivModGadget;
 use selfbalance::SelfbalanceGadget;
 use shl_shr::ShlShrGadget;
@@ -233,11 +235,11 @@ pub(crate) struct ExecutionConfig<F> {
     pop_gadget: PopGadget<F>,
     push_gadget: PushGadget<F>,
     return_revert_gadget: ReturnRevertGadget<F>,
+    sar_gadget: SarGadget<F>,
     sdiv_smod_gadget: SignedDivModGadget<F>,
     selfbalance_gadget: SelfbalanceGadget<F>,
     sha3_gadget: Sha3Gadget<F>,
     shl_shr_gadget: ShlShrGadget<F>,
-    sar_gadget: DummyGadget<F, 2, 1, { ExecutionState::SAR }>,
     extcodecopy_gadget: DummyGadget<F, 4, 0, { ExecutionState::EXTCODECOPY }>,
     returndatasize_gadget: ReturnDataSizeGadget<F>,
     returndatacopy_gadget: ReturnDataCopyGadget<F>,
@@ -1111,6 +1113,7 @@ impl<F: Field> ExecutionConfig<F> {
             ExecutionState::RETURN_REVERT => assign_exec_step!(self.return_revert_gadget),
             ExecutionState::RETURNDATASIZE => assign_exec_step!(self.returndatasize_gadget),
             ExecutionState::RETURNDATACOPY => assign_exec_step!(self.returndatacopy_gadget),
+            ExecutionState::SAR => assign_exec_step!(self.sar_gadget),
             ExecutionState::SCMP => assign_exec_step!(self.signed_comparator_gadget),
             ExecutionState::SDIV_SMOD => assign_exec_step!(self.sdiv_smod_gadget),
             ExecutionState::BLOCKCTXU64 => assign_exec_step!(self.block_ctx_u64_gadget),
@@ -1119,7 +1122,6 @@ impl<F: Field> ExecutionConfig<F> {
             ExecutionState::BLOCKHASH => assign_exec_step!(self.blockhash_gadget),
             ExecutionState::SELFBALANCE => assign_exec_step!(self.selfbalance_gadget),
             // dummy gadgets
-            ExecutionState::SAR => assign_exec_step!(self.sar_gadget),
             ExecutionState::EXTCODECOPY => assign_exec_step!(self.extcodecopy_gadget),
             ExecutionState::CREATE => assign_exec_step!(self.create_gadget),
             ExecutionState::CREATE2 => assign_exec_step!(self.create2_gadget),
