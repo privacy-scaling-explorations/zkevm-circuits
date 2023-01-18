9206fe2b5c3d7aad9869992c514b6660ca19a44b
Dream Wu <wwuwwei@126.com>
2023-01-18 10:51:37 +0800

    fix typing

--- bus-mapping/src/circuit_input_builder/input_state_ref.rs
+++ bus-mapping/src/circuit_input_builder/input_state_ref.rs
@@ -1191,7 +1191,7 @@ impl<'a> CircuitInputStateRef<'a> {
                 0u64.into(),
             );
 
-            //even call.rw_counter_end_of_reversion is zero for now, it will set in
+            //Even call.rw_counter_end_of_reversion is zero for now, it will set in
             //set_value_ops_call_context_rwc_eor later
             // if call fails, no matter root or internal, read RwCounterEndOfReversion for
             // circuit constraint.
