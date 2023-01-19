aaeeee86a88a403165e4cad0f9c6e4a55a7931c1
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2023-01-19 06:44:06 +0000

    fix

--- bus-mapping/src/evm/opcodes/error_oog_constant.rs
+++ bus-mapping/src/evm/opcodes/error_oog_constant.rs
@@ -1,6 +1,5 @@
 use crate::circuit_input_builder::{CircuitInputStateRef, ExecStep};
 use crate::evm::Opcode;
-use crate::operation::CallContextField;
 use crate::Error;
 use eth_types::GethExecStep;
 
@@ -17,28 +16,10 @@ impl Opcode for ErrorOogConstant {
         let next_step = geth_steps.get(1);
         exec_step.error = state.get_step_err(geth_step, next_step).unwrap();
 
-        let call = state.call()?.clone();
-        // call failed
-        state.call_context_write(
-            &mut exec_step,
-            call.call_id,
-            CallContextField::IsSuccess,
-            0.into(),
-        );
-        // remove all available gas for this call or root context
-        state.call_context_write(
-            &mut exec_step,
-            call.call_id,
-            CallContextField::GasLeft,
-            0.into(),
-        );
-        // XXX: any more to do?
-
-        if !call.is_root {
-            state.gen_restore_context_ops(&mut exec_step, geth_steps)?;
-        }
-
+        // handles all required steps
+        state.gen_restore_context_ops(&mut exec_step, geth_steps)?;
         state.handle_return(geth_step)?;
+
         Ok(vec![exec_step])
     }
 }
