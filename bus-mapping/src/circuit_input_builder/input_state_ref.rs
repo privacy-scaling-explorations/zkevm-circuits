72e6a29b4998cb44b753e6cc9feeb1ddc1cd7bb1
xiaodino <ruby@taiko.xyz>
2023-01-28 19:14:27 -0800

    Update

--- bus-mapping/src/circuit_input_builder/input_state_ref.rs
+++ bus-mapping/src/circuit_input_builder/input_state_ref.rs
@@ -304,14 +304,12 @@ impl<'a> CircuitInputStateRef<'a> {
         // AccountNonExisting proofs lookups).
         if (!matches!(op.field, AccountField::CodeHash)
             && (matches!(rw, RW::READ) || (op.value_prev.is_zero() && op.value.is_zero())))
-            && account.is_empty()
+            && account.is_empty() && !self.tx.invalid_tx
         {
-            /*
             panic!(
                 "RWTable Account field {:?} lookup to non-existing account rwc: {}, op: {:?}",
                 rw, self.block_ctx.rwc.0, op
             );
-            */
         }
         // -- sanity check end --
         // Perform the write to the account in the StateDB
