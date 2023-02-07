a723a8f82507f52a0529cd1e27e367a7386bcf2b
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2023-02-07 08:17:19 +0000

    zkevm-circuits: add Clone,Copy,Debug to ProofType
    Fixes build errors with newer rust versions

--- zkevm-circuits/src/table.rs
+++ zkevm-circuits/src/table.rs
@@ -459,6 +459,7 @@ impl RwTable {
 }
 
 /// The types of proofs in the MPT table
+#[derive(Clone, Copy, Debug)]
 pub enum ProofType {
     /// Nonce updated
     NonceChanged = AccountFieldTag::Nonce as isize,
