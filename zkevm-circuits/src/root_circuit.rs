45186365e5667ef78d42ee29e0d011f06b106004
Leo Lara <leo@leolara.me>
2023-02-09 12:18:43 +0100

    Add max_evm_rows to root circuit

--- zkevm-circuits/src/root_circuit.rs
+++ zkevm-circuits/src/root_circuit.rs
@@ -171,6 +171,7 @@ mod test {
                 max_rws: 256,
                 max_copy_rows: 256,
                 max_bytecode: 512,
+                max_evm_rows: 0,
                 keccak_padding: None,
             };
             let (k, circuit, instance, _) =
