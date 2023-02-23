2fc84ba09488e918880d3c873f570de7a4ed9b5c
Zhang Zhuo <mycinbrin@gmail.com>
2023-02-23 14:59:55 +0800

    fix some typos and outdated namings (#1238)
    ### Description
    
    [_PR description_]
    
    ### Issue Link
    
    [_link issue here_]
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ### Contents
    
    - [_item_]
    
    ### Rationale
    
    [_design decisions and extended information_]
    
    ### How Has This Been Tested?
    
    [_explanation_]
    
    <hr>
    
    ## How to fill a PR description 
    
    Please give a concise description of your PR.
    
    The target readers could be future developers, reviewers, and auditors.
    By reading your description, they should easily understand the changes
    proposed in this pull request.
    
    MUST: Reference the issue to resolve
    
    ### Single responsability
    
    Is RECOMMENDED to create single responsibility commits, but not
    mandatory.
    
    Anyway, you MUST enumerate the changes in a unitary way, e.g.
    
    ```
    This PR contains:
    - Cleanup of xxxx, yyyy
    - Changed xxxx to yyyy in order to bla bla
    - Added xxxx function to ...
    - Refactored ....
    ```
    
    ### Design choices
    
    RECOMMENDED to:
    - What types of design choices did you face?
    - What decisions you have made?
    - Any valuable information that could help reviewers to think critically

--- zkevm-circuits/src/evm_circuit/execution/push.rs
+++ zkevm-circuits/src/evm_circuit/execution/push.rs
@@ -38,7 +38,7 @@ impl<F: Field> ExecutionGadget<F> for PushGadget<F> {
         // linear combination uses little-endian, so we lookup from the LSB
         // which has index (program_counter + num_pushed), and then move left
         // (program_counter + num_pushed - idx) to lookup all 32 bytes
-        // condiionally by selectors.
+        // conditionally by selectors.
         // For PUSH2 as an example, we lookup from byte0, byte1, ..., byte31,
         // where the byte2 is actually the PUSH2 itself, and lookup are only
         // enabled for byte0 and byte1.
