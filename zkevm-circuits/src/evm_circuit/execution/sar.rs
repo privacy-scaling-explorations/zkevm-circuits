61e3193de362dc1d0b47825fe1861105e1a7e2df
Steven <asongala@163.com>
2023-03-06 19:11:07 +0800

    Bug: add `shf_div64 < 4` constraint to `SarGadget` (#1272)
    ### Description
    
    A malicious prover could construct overflow `shf_div64` value to make
    wrong cases to satisfy `SAR` constraints. Reference issue
    https://github.com/privacy-scaling-explorations/zkevm-circuits/issues/1227
    for details.
    
    Will try to sync to zkevm-specs after merged (`shf_lt256` should also be
    updated).
    
    ### Issue Link
    
    Close
    https://github.com/privacy-scaling-explorations/zkevm-circuits/issues/1227
    
    ### Type of change
    
    - [X] Bug fix (non-breaking change which fixes an issue)

--- zkevm-circuits/src/evm_circuit/execution/sar.rs
+++ zkevm-circuits/src/evm_circuit/execution/sar.rs
@@ -41,6 +41,8 @@ pub(crate) struct SarGadget<F> {
     p_top: Cell<F>,
     // Identify if `a` is a negative word.
     is_neg: LtGadget<F, 1>,
+    // Verify `shf_div64 < 4`.
+    shf_div64_lt_4: LtGadget<F, 1>,
     // Verify `shf_mod64 < 64`.
     shf_mod64_lt_64: LtGadget<F, 1>,
     // Identify if `shift` is less than 256 or not.
@@ -162,6 +164,8 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
         );
 
         // Shift constraint
+        let shf_div64_lt_4 = LtGadget::construct(cb, shf_div64.expr(), 4.expr());
+        cb.require_equal("shf_div64 < 4", shf_div64_lt_4.expr(), 1.expr());
         let shf_mod64_lt_64 = LtGadget::construct(cb, shf_mod64.expr(), 64.expr());
         cb.require_equal("shf_mod64 < 64", shf_mod64_lt_64.expr(), 1.expr());
         cb.require_equal(
@@ -232,6 +236,7 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
             p_hi,
             p_top,
             is_neg,
+            shf_div64_lt_4,
             shf_mod64_lt_64,
             shf_lt256,
             shf_lo_div64_eq0,
@@ -322,6 +327,8 @@ impl<F: Field> ExecutionGadget<F> for SarGadget<F> {
             127.into(),
             u64::from(a.to_le_bytes()[31]).into(),
         )?;
+        self.shf_div64_lt_4
+            .assign(region, offset, F::from_u128(shf_div64), 4.into())?;
         self.shf_mod64_lt_64
             .assign(region, offset, F::from_u128(shf_mod64), 64.into())?;
         self.shf_lt256
