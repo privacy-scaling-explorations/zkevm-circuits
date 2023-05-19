privacy-scaling-explorations/zkevm-circuits

commit 69cb8a9329a3f5263e0ebb4ee003dd26a9470107

Remove RwTableTag (#1406)
### Description

`RwTableTag` can be fully replaced by `Target`.

### Issue Link

Part of #1391 

### Type of change

- [x] Bug fix (non-breaking change which fixes an issue)
- [ ] New feature (non-breaking change which adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to not work as expected)
- [ ] This change requires a documentation update

### Contents

We add EnumIter, Hash, Expr to Target to let it serve different needs.

Note to reviewers: the `impl From<Target> for usize` is required by
BinaryNumberConfig.

bus-mapping/src/operation.rs
@@ -6,6 +6,9 @@ pub(crate) mod container;
 
 pub use container::OperationContainer;
 pub use eth_types::evm_types::{MemoryAddress, StackAddress};
+use gadgets::impl_expr;
+use halo2_proofs::plonk::Expression;
+use strum_macros::EnumIter;
 
 use core::{cmp::Ordering, fmt, fmt::Debug};
 use eth_types::{Address, Word};
@@ -88,10 +91,10 @@ impl RWCounter {
 }
 
 /// Enum used to differenciate between EVM Stack, Memory and Storage operations.
-#[derive(Debug, Clone, PartialEq, Eq, Copy)]
+#[derive(Debug, Clone, PartialEq, Eq, Copy, EnumIter, Hash)]
 pub enum Target {
     /// Start is a padding operation.
-    Start,
+    Start = 1,
     /// Means the target of the operation is the Memory.
     Memory,
     /// Means the target of the operation is the Stack.
@@ -112,6 +115,28 @@ pub enum Target {
     TxReceipt,
     /// Means the target of the operation is the TxLog.
     TxLog,
+}
+
+impl_expr!(Target);
+
+impl From<Target> for usize {
+    fn from(value: Target) -> usize {
+        value.into()
+    }
+}
+
+impl Target {
+    /// Returns true if the RwTable operation is reversible
+    pub fn is_reversible(self) -> bool {
+        matches!(
+            self,
+            Target::TxAccessListAccount
+                | Target::TxAccessListAccountStorage
+                | Target::TxRefund
+                | Target::Account
+                | Target::Storage
+        )
+    }
 }
 
 /// Trait used for Operation Kinds.

zkevm-circuits/src/copy_circuit.rs
@@ -10,7 +10,10 @@ mod test;
 #[cfg(any(feature = "test", test, feature = "test-circuits"))]
 pub use dev::CopyCircuit as TestCopyCircuit;
 
-use bus_mapping::circuit_input_builder::{CopyDataType, CopyEvent};
+use bus_mapping::{
+    circuit_input_builder::{CopyDataType, CopyEvent},
+    operation::Target,
+};
 use eth_types::{Field, Word};
 
 use gadgets::{
@@ -29,8 +32,8 @@ use std::{collections::HashMap, marker::PhantomData};
 use crate::{
     evm_circuit::util::constraint_builder::{BaseConstraintBuilder, ConstrainBuilderCommon},
     table::{
-        BytecodeFieldTag, BytecodeTable, CopyTable, LookupTable, RwTable, RwTableTag,
-        TxContextFieldTag, TxTable,
+        BytecodeFieldTag, BytecodeTable, CopyTable, LookupTable, RwTable, TxContextFieldTag,
+        TxTable,
     },
     util::{Challenges, SubCircuit, SubCircuitConfig},
     witness,
@@ -327,7 +330,7 @@ impl<F: Field> SubCircuitConfig<F> for CopyCircuitConfig<F> {
             vec![
                 meta.query_advice(rw_counter, Rotation::cur()),
                 not::expr(meta.query_selector(q_step)),
-                RwTableTag::Memory.expr(),
+                Target::Memory.expr(),
                 meta.query_advice(id, Rotation::cur()), // call_id
                 meta.query_advice(addr, Rotation::cur()), // memory address
                 0.expr(),
@@ -349,7 +352,7 @@ impl<F: Field> SubCircuitConfig<F> for CopyCircuitConfig<F> {
             vec![
                 meta.query_advice(rw_counter, Rotation::cur()),
                 1.expr(),
-                RwTableTag::TxLog.expr(),
+                Target::TxLog.expr(),
                 meta.query_advice(id, Rotation::cur()), // tx_id
                 meta.query_advice(addr, Rotation::cur()), // byte_index || field_tag || log_id
                 0.expr(),

zkevm-circuits/src/evm_circuit/execution/end_tx.rs
@@ -17,11 +17,10 @@ use crate::{
         },
         witness::{Block, Call, ExecStep, Transaction},
     },
-    table::{
-        BlockContextFieldTag, CallContextFieldTag, RwTableTag, TxContextFieldTag, TxReceiptFieldTag,
-    },
+    table::{BlockContextFieldTag, CallContextFieldTag, TxContextFieldTag, TxReceiptFieldTag},
     util::Expr,
 };
+use bus_mapping::operation::Target;
 use eth_types::{evm_types::MAX_REFUND_QUOTIENT_OF_GAS_USED, Field, ToScalar};
 use halo2_proofs::{circuit::Value, plonk::Error};
 use strum::EnumCount;
@@ -284,7 +283,7 @@ impl<F: Field> ExecutionGadget<F> for EndTxGadget<F> {
             // first transaction needs TxReceiptFieldTag::COUNT(3) lookups to tx receipt,
             // while later transactions need 4 (with one extra cumulative gas read) lookups
             let rw = &block.rws[(
-                RwTableTag::TxReceipt,
+                Target::TxReceipt,
                 (tx.id - 2) * (TxReceiptFieldTag::COUNT + 1) + 2,
             )];
             rw.receipt_value()

zkevm-circuits/src/evm_circuit/util/constraint_builder.rs
@@ -6,12 +6,12 @@ use crate::{
         util::{Cell, RandomLinearCombination, Word},
     },
     table::{
-        AccountFieldTag, BytecodeFieldTag, CallContextFieldTag, RwTableTag, TxContextFieldTag,
-        TxLogFieldTag, TxReceiptFieldTag,
+        AccountFieldTag, BytecodeFieldTag, CallContextFieldTag, TxContextFieldTag, TxLogFieldTag,
+        TxReceiptFieldTag,
     },
     util::{build_tx_log_expression, Challenges, Expr},
 };
-use bus_mapping::state_db::EMPTY_CODE_HASH_LE;
+use bus_mapping::{operation::Target, state_db::EMPTY_CODE_HASH_LE};
 use eth_types::Field;
 use gadgets::util::not;
 use halo2_proofs::{
@@ -677,7 +677,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         name: &str,
         counter: Expression<F>,
         is_write: Expression<F>,
-        tag: RwTableTag,
+        tag: Target,
         values: RwValues<F>,
     ) {
         let name = format!("rw lookup {}", name);
@@ -698,7 +698,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         &mut self,
         name: &'static str,
         is_write: Expression<F>,
-        tag: RwTableTag,
+        tag: Target,
         values: RwValues<F>,
     ) {
         self.rw_lookup_with_counter(
@@ -726,7 +726,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
     fn reversible_write(
         &mut self,
         name: &'static str,
-        tag: RwTableTag,
+        tag: Target,
         values: RwValues<F>,
         reversion_info: Option<&mut ReversionInfo<F>>,
     ) {
@@ -769,7 +769,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
     ) {
         self.reversible_write(
             "TxAccessListAccount write",
-            RwTableTag::TxAccessListAccount,
+            Target::TxAccessListAccount,
             RwValues::new(
                 tx_id,
                 account_address,
@@ -793,7 +793,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "account access list read",
             false.expr(),
-            RwTableTag::TxAccessListAccount,
+            Target::TxAccessListAccount,
             RwValues::new(
                 tx_id,
                 account_address,
@@ -818,7 +818,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
     ) {
         self.reversible_write(
             "TxAccessListAccountStorage write",
-            RwTableTag::TxAccessListAccountStorage,
+            Target::TxAccessListAccountStorage,
             RwValues::new(
                 tx_id,
                 account_address,
@@ -843,7 +843,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "TxAccessListAccountStorage read",
             false.expr(),
-            RwTableTag::TxAccessListAccountStorage,
+            Target::TxAccessListAccountStorage,
             RwValues::new(
                 tx_id,
                 account_address,
@@ -863,7 +863,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "TxRefund read",
             false.expr(),
-            RwTableTag::TxRefund,
+            Target::TxRefund,
             RwValues::new(
                 tx_id,
                 0.expr(),
@@ -886,7 +886,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
     ) {
         self.reversible_write(
             "TxRefund write",
-            RwTableTag::TxRefund,
+            Target::TxRefund,
             RwValues::new(
                 tx_id,
                 0.expr(),
@@ -912,7 +912,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "Account read",
             false.expr(),
-            RwTableTag::Account,
+            Target::Account,
             RwValues::new(
                 0.expr(),
                 account_address,
@@ -936,7 +936,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
     ) {
         self.reversible_write(
             "Account write",
-            RwTableTag::Account,
+            Target::Account,
             RwValues::new(
                 0.expr(),
                 account_address,
@@ -964,7 +964,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "account_storage_read",
             false.expr(),
-            RwTableTag::AccountStorage,
+            Target::Storage,
             RwValues::new(
                 tx_id,
                 account_address,
@@ -991,7 +991,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
     ) {
         self.reversible_write(
             "AccountStorage write",
-            RwTableTag::AccountStorage,
+            Target::Storage,
             RwValues::new(
                 tx_id,
                 account_address,
@@ -1042,7 +1042,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "CallContext lookup",
             is_write,
-            RwTableTag::CallContext,
+            Target::CallContext,
             RwValues::new(
                 call_id.unwrap_or_else(|| self.curr.state.call_id.expr()),
                 0.expr(),
@@ -1117,7 +1117,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "Stack lookup",
             is_write,
-            RwTableTag::Stack,
+            Target::Stack,
             RwValues::new(
                 self.curr.state.call_id.expr(),
                 self.curr.state.stack_pointer.expr() + stack_pointer_offset,
@@ -1143,7 +1143,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "Memory lookup",
             is_write,
-            RwTableTag::Memory,
+            Target::Memory,
             RwValues::new(
                 call_id.unwrap_or_else(|| self.curr.state.call_id.expr()),
                 memory_address,
@@ -1168,7 +1168,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "log data lookup",
             1.expr(),
-            RwTableTag::TxLog,
+            Target::TxLog,
             RwValues::new(
                 tx_id,
                 build_tx_log_expression(index, field_tag.expr(), log_id),
@@ -1194,7 +1194,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
         self.rw_lookup(
             "tx receipt lookup",
             is_write,
-            RwTableTag::TxReceipt,
+            Target::TxReceipt,
             RwValues::new(
                 tx_id,
                 0.expr(),
@@ -1215,7 +1215,7 @@ impl<'a, F: Field> EVMConstraintBuilder<'a, F> {
             "Start lookup",
             counter,
             0.expr(),
-            RwTableTag::Start,
+            Target::Start,
             RwValues {
                 id: 0.expr(),
                 address: 0.expr(),

zkevm-circuits/src/state_circuit.rs
@@ -10,6 +10,7 @@ mod random_linear_combination;
 mod dev;
 #[cfg(any(feature = "test", test))]
 mod test;
+use bus_mapping::operation::Target;
 #[cfg(any(feature = "test", test, feature = "test-circuits"))]
 pub use dev::StateCircuit as TestStateCircuit;
 
@@ -19,7 +20,7 @@ use self::{
 };
 use crate::{
     evm_circuit::{param::N_BYTES_WORD, util::rlc},
-    table::{AccountFieldTag, LookupTable, MPTProofType, MptTable, RwTable, RwTableTag},
+    table::{AccountFieldTag, LookupTable, MPTProofType, MptTable, RwTable},
     util::{Challenges, Expr, SubCircuit, SubCircuitConfig},
     witness::{self, MptUpdates, Rw, RwMap},
 };
@@ -275,7 +276,7 @@ impl<F: Field> StateCircuitConfig<F> {
                                 assert_eq!(state_root, old_root);
                                 state_root = new_root;
                             }
-                            if matches!(row.tag(), RwTableTag::CallContext) && !row.is_write() {
+                            if matches!(row.tag(), Target::CallContext) && !row.is_write() {
                                 assert_eq!(row.value_assignment(randomness), F::ZERO, "{:?}", row);
                             }
                             state_root
@@ -393,7 +394,7 @@ impl<F: Field> StateCircuitConfig<F> {
 /// Keys for sorting the rows of the state circuit
 #[derive(Clone, Copy)]
 pub struct SortKeysConfig {
-    tag: BinaryNumberConfig<RwTableTag, 4>,
+    tag: BinaryNumberConfig<Target, 4>,
     id: MpiConfig<u32, N_LIMBS_ID>,
     address: MpiConfig<Address, N_LIMBS_ACCOUNT_ADDRESS>,
     field_tag: Column<Advice>,

zkevm-circuits/src/state_circuit/constraint_builder.rs
@@ -7,9 +7,10 @@ use crate::{
         param::N_BYTES_WORD,
         util::{math_gadget::generate_lagrange_base_polynomial, not},
     },
-    table::{AccountFieldTag, MPTProofType, RwTableTag},
+    table::{AccountFieldTag, MPTProofType},
     util::Expr,
 };
+use bus_mapping::operation::Target;
 use eth_types::Field;
 use gadgets::binary_number::BinaryNumberConfig;
 use halo2_proofs::plonk::Expression;
@@ -101,35 +102,34 @@ impl<F: Field> ConstraintBuilder<F> {
 
     pub fn build(&mut self, q: &Queries<F>) {
         self.build_general_constraints(q);
-        self.condition(q.tag_matches(RwTableTag::Start), |cb| {
+        self.condition(q.tag_matches(Target::Start), |cb| {
             cb.build_start_constraints(q)
         });
-        self.condition(q.tag_matches(RwTableTag::Memory), |cb| {
+        self.condition(q.tag_matches(Target::Memory), |cb| {
             cb.build_memory_constraints(q)
         });
-        self.condition(q.tag_matches(RwTableTag::Stack), |cb| {
+        self.condition(q.tag_matches(Target::Stack), |cb| {
             cb.build_stack_constraints(q)
         });
-        self.condition(q.tag_matches(RwTableTag::AccountStorage), |cb| {
+        self.condition(q.tag_matches(Target::Storage), |cb| {
             cb.build_account_storage_constraints(q)
         });
-        self.condition(q.tag_matches(RwTableTag::TxAccessListAccount), |cb| {
+        self.condition(q.tag_matches(Target::TxAccessListAccount), |cb| {
             cb.build_tx_access_list_account_constraints(q)
         });
-        self.condition(
-            q.tag_matches(RwTableTag::TxAccessListAccountStorage),
-            |cb| cb.build_tx_access_list_account_storage_constraints(q),
-        );
-        self.condition(q.tag_matches(RwTableTag::TxRefund), |cb| {
+        self.condition(q.tag_matches(Target::TxAccessListAccountStorage), |cb| {
+            cb.build_tx_access_list_account_storage_constraints(q)
+        });
+        self.condition(q.tag_matches(Target::TxRefund), |cb| {
             cb.build_tx_refund_constraints(q)
         });
-        self.condition(q.tag_matches(RwTableTag::Account), |cb| {
+        self.condition(q.tag_matches(Target::Account), |cb| {
             cb.build_account_constraints(q)
         });
-        self.condition(q.tag_matches(RwTableTag::CallContext), |cb| {
+        self.condition(q.tag_matches(Target::CallContext), |cb| {
             cb.build_call_context_constraints(q)
         });
-        self.condition(q.tag_matches(RwTableTag::TxLog), |cb| {
+        self.condition(q.tag_matches(Target::TxLog), |cb| {
             cb.build_tx_log_constraints(q)
         });
     }
@@ -617,8 +617,8 @@ impl<F: Field> Queries<F> {
         self.mpt_proof_type.clone()
     }
 
-    fn tag_matches(&self, tag: RwTableTag) -> Expression<F> {
-        BinaryNumberConfig::<RwTableTag, 4>::value_equals_expr(tag, self.tag_bits.clone())
+    fn tag_matches(&self, tag: Target) -> Expression<F> {
+        BinaryNumberConfig::<Target, 4>::value_equals_expr(tag, self.tag_bits.clone())
     }
 
     fn first_access(&self) -> Expression<F> {

zkevm-circuits/src/state_circuit/test.rs
@@ -1,7 +1,7 @@
 #![allow(unused_imports)]
 pub use super::{dev::*, *};
 use crate::{
-    table::{AccountFieldTag, CallContextFieldTag, RwTableTag, TxLogFieldTag, TxReceiptFieldTag},
+    table::{AccountFieldTag, CallContextFieldTag, TxLogFieldTag, TxReceiptFieldTag},
     util::{unusable_rows, SubCircuit},
     witness::{MptUpdates, Rw, RwMap},
 };
@@ -824,7 +824,7 @@ fn invalid_stack_address_change() {
 #[test]
 fn invalid_tags() {
     let first_row_offset = -isize::try_from(N_ROWS).unwrap();
-    let tags: BTreeSet<usize> = RwTableTag::iter().map(|x| x as usize).collect();
+    let tags: BTreeSet<usize> = Target::iter().map(|x| x as usize).collect();
     for i in 0..16 {
         if tags.contains(&i) {
             continue;

zkevm-circuits/src/table.rs
@@ -50,7 +50,7 @@ pub(crate) use exp_table::ExpTable;
 pub(crate) use keccak_table::KeccakTable;
 
 pub(crate) use mpt_table::{MPTProofType, MptTable};
-pub(crate) use rw_table::{RwTable, RwTableTag};
+pub(crate) use rw_table::RwTable;
 pub(crate) use tx_table::{
     TxContextFieldTag, TxFieldTag, TxLogFieldTag, TxReceiptFieldTag, TxTable,
 };

commit 70d7da497405918ab66f337c566bcb57e6d98a03

Fix infinite loop (#1421)
### Description

Fix an infinite loop from a `From` implementation calling itself.

### Issue Link

Resolve
https://github.com/privacy-scaling-explorations/zkevm-circuits/issues/1420
Related
https://github.com/privacy-scaling-explorations/zkevm-circuits/issues/1417

### Type of change

- [x] Bug fix (non-breaking change which fixes an issue)

### Contents

@ChihChengLiang found that this issue was introduced in
https://github.com/privacy-scaling-explorations/zkevm-circuits/pull/1406
and by debugging on the debug build I found the infinite loop (which
caused the stack to overflow which terminated the program with a
segfault). The segfault is not observed in the release build, I guess
the compiler optimizes the recursive call into an infinite loop, so the
stack is never exhausted.

bus-mapping/src/operation.rs
@@ -121,7 +121,7 @@ impl_expr!(Target);
 
 impl From<Target> for usize {
     fn from(value: Target) -> usize {
-        value.into()
+        value as usize
     }
 }
 