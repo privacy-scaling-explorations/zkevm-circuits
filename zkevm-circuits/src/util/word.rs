93d30c18499288528499fcaaf928e72e0b24e255
sm.wu <hero78119@gmail.com>
2023-05-30 23:05:09 +0800

    duplicate word field in witness RwMap

--- zkevm-circuits/src/util/word.rs
+++ zkevm-circuits/src/util/word.rs
@@ -3,7 +3,7 @@
 // - Limbs: An EVN word is 256 bits. Limbs N means split 256 into N limb. For example, N = 4, each
 //   limb is 256/4 = 64 bits
 
-use eth_types::{Field, ToLittleEndian};
+use eth_types::{Field, ToLittleEndian, H160};
 use gadgets::util::{not, or, Expr};
 use halo2_proofs::{
     circuit::{AssignedCell, Value},
@@ -13,10 +13,12 @@ use itertools::Itertools;
 
 use crate::evm_circuit::util::{from_bytes, CachedRegion, Cell, RandomLinearCombination};
 
-/// The EVM word for witness
+/// evm word 32 bytes, half word 16 bytes
+const N_BYTES_HALF_WORD: usize = 16;
+
 #[derive(Clone, Debug, Copy)]
+/// The EVM word for witness
 pub struct WordLimbs<T, const N: usize> {
-    /// The limbs of this word.
     pub limbs: [T; N],
 }
 
@@ -83,6 +85,13 @@ pub trait WordExpr<F> {
 
 impl<F: Field, const N: usize> WordLimbs<Cell<F>, N> {
     /// assign limbs
+    /// N1 is number of bytes to assign, while N is number of limbs.
+    /// N1 % N = 0 (also implies N1 >= N)
+    /// If N1 > N, then N1 will be chunk into N1 / N size then aggregate to single expression
+    /// then assign to N limbs respectively.
+    /// e.g. N1 = 4 bytes, [b1, b2, b3, b4], and N = 2 limbs [l1, l2]
+    /// It equivalent `l1.assign(b1.expr() + b2.expr * F(256))`, `l2.assign(b3.expr() + b4.expr *
+    /// F(256))`
     pub fn assign<const N1: usize>(
         &self,
         region: &mut CachedRegion<'_, '_, F>,
@@ -98,6 +107,86 @@ impl<F: Field, const N: usize> WordLimbs<Cell<F>, N> {
                 .map(|(value, cell)| cell.assign(region, offset, Value::known(value)))
                 .collect()
         })
+    }
+
+    // N_LO, N_HI are number of bytes to assign to first half and second half of size N limbs,
+    // respectively N_LO and N_HI can be different size, the only requirement is N_LO % (N/2)
+    // and N_HI % (N/2) [N/2] limbs will be assigned separately.
+    // E.g. N_LO = 4 => [nl1, nl2, nl3, nl4]
+    // N_HI = 2 => [nh1, nh2]
+    // N = 2 => [l1, l2]
+    // it equivalent l1.assign(nl1.expr() + nl2.expr() * 256 + nl3.expr() * 256^2 +  nl3.expr() *
+    // 256^3) and l2.assign(nh1.expr() + nh2.expr() * 256)
+    pub fn assign_lo_hi<const N_LO: usize, const N_HI: usize>(
+        &self,
+        region: &mut CachedRegion<'_, '_, F>,
+        offset: usize,
+        bytes_lo: Option<[u8; N_LO]>,
+        bytes_hi: Option<[u8; N_HI]>,
+    ) -> Result<Vec<AssignedCell<F, F>>, Error> {
+        assert_eq!(N % 2, 0); // TODO use static_assertion instead
+        assert_eq!(N_LO % (N / 2), 0);
+        assert_eq!(N_HI % (N / 2), 0);
+        // assign lo
+        let bytes_lo_assigned: Result<Vec<AssignedCell<F, F>>, Error> =
+            bytes_lo.map_or(Err(Error::Synthesis), |bytes| {
+                bytes
+                    .chunks(N_LO / (N / 2)) // chunk in little endian
+                    .map(|chunk| from_bytes::value(chunk))
+                    .zip(self.limbs[0..(N / 2)].iter())
+                    .map(|(value, cell)| cell.assign(region, offset, Value::known(value)))
+                    .collect()
+            });
+
+        // assign hi
+        let bytes_hi_assigned: Result<Vec<AssignedCell<F, F>>, Error> =
+            bytes_hi.map_or(Err(Error::Synthesis), |bytes| {
+                bytes
+                    .chunks(N_HI / (N / 2)) // chunk in little endian
+                    .map(|chunk| from_bytes::value(chunk))
+                    .zip(self.limbs[N / 2..].iter())
+                    .map(|(value, cell)| cell.assign(region, offset, Value::known(value)))
+                    .collect()
+            });
+
+        match (bytes_lo_assigned, bytes_hi_assigned) {
+            (Ok(bytes_lo_assignedcells), Ok(bytes_hi_assignedcells)) => Ok([
+                bytes_lo_assignedcells.to_vec(),
+                bytes_hi_assignedcells.to_vec(),
+            ]
+            .concat()),
+            _ => Err(Error::Synthesis),
+        }
+    }
+
+    pub fn assign_u256(
+        &self,
+        region: &mut CachedRegion<'_, '_, F>,
+        offset: usize,
+        word: eth_types::Word,
+    ) -> Result<Vec<AssignedCell<F, F>>, Error> {
+        self.assign_lo_hi(
+            region,
+            offset,
+            word.to_le_bytes()[0..N_BYTES_HALF_WORD].try_into().ok(),
+            word.to_le_bytes()[N_BYTES_HALF_WORD..].try_into().ok(),
+        )
+    }
+
+    pub fn assign_h160(
+        &self,
+        region: &mut CachedRegion<'_, '_, F>,
+        offset: usize,
+        h160: H160,
+    ) -> Result<Vec<AssignedCell<F, F>>, Error> {
+        let bytes = h160.clone().as_fixed_bytes().as_mut();
+        bytes.reverse();
+        self.assign_lo_hi(
+            region,
+            offset,
+            bytes[0..N_BYTES_HALF_WORD].try_into().ok(),
+            bytes[N_BYTES_HALF_WORD..].try_into().ok(),
+        )
     }
 
     #[deprecated(note = "in fav of to_word trait. Make this private")]
@@ -122,21 +211,18 @@ impl<T: Clone> Word<T> {
     pub fn new(limbs: [T; 2]) -> Self {
         Self(WordLimbs::<T, 2>::new(limbs))
     }
-
     /// The high 128 bits limb
     pub fn hi(&self) -> T {
-        self.0.limbs[1].clone()
+        self.0.limbs[1]
     }
-
     /// the low 128 bits limb
     pub fn lo(&self) -> T {
-        self.0.limbs[0].clone()
+        self.0.limbs[0]
     }
     /// number of limbs
     pub fn n() -> usize {
         2
     }
-
     /// word to low and high 128 bits
     pub fn to_lo_hi(&self) -> (T, T) {
         (self.0.limbs[0].clone(), self.0.limbs[1].clone())
@@ -193,7 +279,6 @@ impl<F: Field> Word<Expression<F>> {
     pub fn from_lo_unchecked(lo: Expression<F>) -> Self {
         Self(WordLimbs::<Expression<F>, 2>::new([lo, 0.expr()]))
     }
-
     /// zero word
     pub fn zero() -> Self {
         Self(WordLimbs::<Expression<F>, 2>::new([0.expr(), 0.expr()]))
@@ -230,6 +315,11 @@ impl<F: Field> Word<Expression<F>> {
     /// No underflow check on lo/hi limbs
     pub fn sub_unchecked(self, rhs: Self) -> Self {
         Word::new([self.lo() - rhs.lo(), self.hi() - rhs.hi()])
+    }
+
+    /// No overflow check
+    pub fn expr_unchecked(&self) -> Expression<F> {
+        self.lo().clone() + self.hi().clone() * (1 << (N_BYTES_HALF_WORD * 8)).expr()
     }
 }
 
