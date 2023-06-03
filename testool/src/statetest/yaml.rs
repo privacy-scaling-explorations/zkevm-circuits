71e3410f584f9e31a32cae257c4690a2720a00ec
ChihChengLiang <chihchengliang@gmail.com>
2023-06-03 23:18:43 +0900

    fmt

--- testool/src/statetest/yaml.rs
+++ testool/src/statetest/yaml.rs
@@ -5,7 +5,7 @@ use super::{
 use crate::{utils::MainnetFork, Compiler};
 use anyhow::{bail, Context, Result};
 use eth_types::{
-    eth_core::{SigningKey, secret_key_to_address},
+    eth_core::{secret_key_to_address, SigningKey},
     geth_types::Account,
     Address, Bytes, H256, U256,
 };
