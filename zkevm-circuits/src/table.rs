f61a9cdb0874995466ea23a2929ffbbec2fffa15
sm.wu <hero78119@gmail.com>
2023-06-12 15:32:58 +0800

    fix code_hash advice column phase

--- zkevm-circuits/src/table.rs
+++ zkevm-circuits/src/table.rs
@@ -710,7 +710,7 @@ impl BytecodeTable {
     pub fn construct<F: Field>(meta: &mut ConstraintSystem<F>) -> Self {
         let [tag, index, is_code, value] = array::from_fn(|_| meta.advice_column());
         let code_hash_word = word::Word::new([meta.advice_column(), meta.advice_column()]);
-        let code_hash = meta.advice_column();
+        let code_hash = meta.advice_column_in(SecondPhase);
         Self {
             code_hash_word,
             code_hash,
