c6ffce6eae93bed518ffd2bb62e37fdca30399cc
Nick <nick@gmx.co.uk>
2023-07-21 15:31:41 +0300

    Remove trap

--- .github/testoolScripts/cleanup.sh
+++ .github/testoolScripts/cleanup.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -x
 
-error() {
-  sudo poweroff
-}
-
-trap 'error' ERR
-
 echo "Reports: http://testool-public.s3-website.eu-central-1.amazonaws.com"
 echo "Shuting down instance..."
 sudo poweroff
