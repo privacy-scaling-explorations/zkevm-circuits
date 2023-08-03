88c05769c51ebdd623b6de18540fac0b58578a5b
AronisAt79 <mariosaronis@gmail.com>
2023-08-03 11:53:31 +0300

    bypass solc compilation warning 2018 : Function state mutability can be restricted to view

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -28,7 +28,8 @@ pub const CHAIN_ID: u64 = 1337;
 /// Path to the test contracts
 pub const CONTRACTS_PATH: &str = "contracts";
 /// Solidity compilation warnings to ignore (by error code)
-pub const WARN: &[u64] = &[];
+/// 2018: Warning - "Function state mutability can be restricted to pure"
+pub const WARN: &[u64] = &[2018];
 /// List of contracts as (ContractName, ContractSolidityFile)
 pub const CONTRACTS: &[(&str, &str)] = &[
     ("Greeter", "greeter/Greeter.sol"),
