38c3d3ef7051bccac0fa4732c279f5e0dcb80e47
AronisAt79 <45406504+AronisAt79@users.noreply.github.com>
2023-09-18 15:33:44 +0300

    Update OpenZeppelinERC20TestToken.sol

--- integration-tests/contracts/OpenZeppelinERC20TestToken.sol
+++ integration-tests/contracts/OpenZeppelinERC20TestToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 // import "./ERC20.sol";
-import "./vendor/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "vendor/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract OpenZeppelinERC20TestToken is ERC20 {
     constructor(address owner) ERC20("TestToken1", "TT1") {
