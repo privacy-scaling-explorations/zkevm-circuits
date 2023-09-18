059e80dcbeeb08a7e918df7cc3d01521f806b52f
AronisAt79 <mariosaronis@gmail.com>
2023-09-18 14:56:58 +0300

    move oz erc20 contract under contracts directory

---
+++ integration-tests/contracts/OpenZeppelinERC20TestToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// import "./ERC20.sol";
+import "./vendor/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract OpenZeppelinERC20TestToken is ERC20 {
+    constructor(address owner) ERC20("TestToken1", "TT1") {
+        _mint(owner, 999999 * 18 ** decimals());
+    }
+}
+
