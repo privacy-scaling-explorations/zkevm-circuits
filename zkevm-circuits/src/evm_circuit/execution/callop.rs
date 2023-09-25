9e9badf3f29ac60dbdb02f3200aec1e8efcf6c11
darth-cy <qg2153@columbia.edu>
2023-09-25 15:41:17 -0700

    adjust import

--- zkevm-circuits/src/evm_circuit/execution/callop.rs
+++ zkevm-circuits/src/evm_circuit/execution/callop.rs
@@ -33,8 +33,7 @@ use bus_mapping::{
 };
 use eth_types::{
     evm_types::{memory::MemoryWordRange, GAS_STIPEND_CALL_WITH_VALUE}, 
-    // evm_types::GAS_STIPEND_CALL_WITH_VALUE,
-    Field, ToAddress, ToBigEndian, ToLittleEndian, ToScalar, U256,
+    Field, ToAddress, ToScalar, U256,
 };
 use halo2_proofs::{circuit::Value, plonk::Error};
 use log::trace;
