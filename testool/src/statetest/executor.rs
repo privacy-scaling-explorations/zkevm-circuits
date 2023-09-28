55077ce26a1f09deeb42b130d63e32a6e91306b2
Chih Cheng Liang <chihchengliang@gmail.com>
2023-09-28 22:57:12 +0800

    Update testool/src/statetest/executor.rs
    Co-authored-by: Eduard S. <eduardsanou@posteo.net>

--- testool/src/statetest/executor.rs
+++ testool/src/statetest/executor.rs
@@ -340,7 +340,7 @@ pub fn run_test(
 
         let prover = MockProver::run(k, &circuit, instance).unwrap();
         prover
-            .verify()
+            .verify_par()
             .map_err(|err| StateTestError::CircuitUnsatisfied {
                 num_failure: err.len(),
                 first_failure: err[0].to_string(),
