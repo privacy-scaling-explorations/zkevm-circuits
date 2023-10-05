92c2457cd9866aa3d73ea1122f3cdde8dd757169
AronisAt79 <mariosaronis@gmail.com>
2023-10-06 02:27:02 +0300

    add #[allow(clippy::all)] to lib.rs for generated modules

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -150,10 +150,13 @@ pub struct CompiledContract {
 }
 
 #[allow(missing_docs)]
+#[allow(clippy::all)]
 pub mod bindings_benchmarks;
 #[allow(missing_docs)]
+#[allow(clippy::all)]
 pub mod bindings_greeter;
 #[allow(missing_docs)]
+#[allow(clippy::all)]
 pub mod bindings_openzeppelinerc20testtoken;
 /// Common code for integration tests of circuits.
 pub mod integration_test_circuits;
