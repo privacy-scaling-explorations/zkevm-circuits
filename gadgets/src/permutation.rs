79ba525dd20596910fe5eb0c38a371ad6b873e8d
sm.wu <hero78119@gmail.com>
2023-10-11 21:50:07 +0800

    permutation gadget trade more columns with degree

--- gadgets/src/permutation.rs
+++ gadgets/src/permutation.rs
@@ -1,5 +1,6 @@
 //! Chip that implements permutation fingerprints
 //! fingerprints &= \prod_{row_j} \left ( \alpha - \sum_k (\gamma^k \cdot cell_j(k))  \right )
+use std::iter;
 #[rustfmt::skip]
 // | q_row_non_first | alpha     | gamma     | fingerprints    |
 // |-----------------|-----------|-----------|-----------------|
@@ -24,7 +25,7 @@ pub struct PermutationChipConfig<F> {
     // column
     fingerprints: Column<Advice>,
     alpha: Column<Advice>,
-    gamma: Column<Advice>,
+    power_of_gamma: Vec<Column<Advice>>,
     // selector
     q_row_non_first: Selector, // 1 between (first, end], exclude first
 
@@ -46,7 +47,6 @@ impl<F: Field> PermutationChipConfig<F> {
         alpha: Value<F>,
         gamma: Value<F>,
         prev_continuous_fingerprint: Value<F>,
-        next_continuous_fingerprint: Value<F>,
         col_values: &[Vec<Value<F>>],
     ) -> Result<PermutationAssignedCells<F>, Error> {
         self.annotate_columns_in_region(region, "state_circuit");
@@ -55,10 +55,12 @@ impl<F: Field> PermutationChipConfig<F> {
         let fingerprints =
             get_permutation_fingerprints(col_values, alpha, gamma, prev_continuous_fingerprint, 1);
 
-        // debug: internal computed fingerprint should match with external fingerprint
-        next_continuous_fingerprint
-            .zip(*fingerprints.last().unwrap())
-            .map(|(a, b)| debug_assert!(a == b, "{:?} != {:?}", a, b));
+        let power_of_gamma = {
+            let num_of_col = col_values.get(0).map(|row| row.len()).unwrap_or_default();
+            std::iter::successors(Some(Value::known(F::ONE)), |prev| (*prev * gamma).into())
+                .take(num_of_col)
+                .collect::<Vec<Value<F>>>()
+        };
 
         let mut last_fingerprint_cell = None;
         let mut alpha_first_cell = None;
@@ -82,16 +84,23 @@ impl<F: Field> PermutationChipConfig<F> {
                 offset,
                 || alpha,
             )?;
-            let gamma_cell = region.assign_advice(
-                || format!("gamma at index {}", offset),
-                self.gamma,
-                offset,
-                || gamma,
-            )?;
+            let gamma_cells = self
+                .power_of_gamma
+                .iter()
+                .zip(power_of_gamma.iter())
+                .map(|(col, value)| {
+                    region.assign_advice(
+                        || format!("gamma at index {}", offset),
+                        *col,
+                        offset,
+                        || *value,
+                    )
+                })
+                .collect::<Result<Vec<AssignedCell<F, F>>, Error>>()?;
 
             if offset == 0 {
                 alpha_first_cell = Some(alpha_cell);
-                gamma_first_cell = Some(gamma_cell);
+                gamma_first_cell = Some(gamma_cells[0].clone());
                 prev_continuous_fingerprint_cell = Some(fingerprint_cell.clone());
             }
             // last offset
@@ -113,13 +122,22 @@ impl<F: Field> PermutationChipConfig<F> {
         [
             (
                 self.fingerprints,
-                "GADGETS_PermutationChipConfig_fingerprints",
+                "GADGETS_PermutationChipConfig_fingerprints".to_string(),
             ),
-            (self.gamma, "GADGETS_PermutationChipConfig_gamma"),
-            (self.alpha, "GADGETS_PermutationChipConfig_alpha"),
+            (
+                self.alpha,
+                "GADGETS_PermutationChipConfig_alpha".to_string(),
+            ),
         ]
         .iter()
-        .for_each(|(col, ann)| region.name_column(|| format!("{}_{}", prefix, ann), *col));
+        .cloned()
+        .chain(self.power_of_gamma.iter().enumerate().map(|(i, col)| {
+            (
+                col.clone(),
+                format!("GADGETS_PermutationChipConfig_gamma_{}", i),
+            )
+        }))
+        .for_each(|(col, ann)| region.name_column(|| format!("{}_{}", prefix, ann), col));
     }
 }
 
@@ -138,16 +156,28 @@ impl<F: Field> PermutationChip<F> {
     ) -> PermutationChipConfig<F> {
         let fingerprints = meta.advice_column();
         let alpha = meta.advice_column();
-        let gamma = meta.advice_column();
+
+        // trade more column to reduce degree of power of gamma
+        let power_of_gamma = (0..cols.len() - 1)
+            .map(|_| meta.advice_column())
+            .collect::<Vec<Column<Advice>>>(); // first element is gamma**1
+
         let q_row_non_first = meta.selector();
 
         meta.enable_equality(fingerprints);
         meta.enable_equality(alpha);
-        meta.enable_equality(gamma);
+        meta.enable_equality(power_of_gamma[0]);
 
         meta.create_gate("permutation fingerprint update logic", |meta| {
             let alpha = meta.query_advice(alpha, Rotation::cur());
-            let gamma = meta.query_advice(gamma, Rotation::cur());
+            let power_of_gamma = iter::once(1.expr())
+                .chain(
+                    power_of_gamma
+                        .iter()
+                        .map(|column| meta.query_advice(*column, Rotation::cur())),
+                )
+                .collect::<Vec<Expression<F>>>();
+
             let q_row_non_first = meta.query_selector(q_row_non_first);
             let cols_cur_exprs = cols
                 .iter()
@@ -156,18 +186,11 @@ impl<F: Field> PermutationChip<F> {
             let fingerprint_prev = meta.query_advice(fingerprints, Rotation::prev());
             let fingerprint_cur = meta.query_advice(fingerprints, Rotation::cur());
 
-            let power_of_gamma = std::iter::successors(1.expr().into(), |prev: &Expression<F>| {
-                (prev.clone() * gamma.clone()).into()
-            })
-            .take(cols.len())
-            .collect::<Vec<Expression<F>>>();
-
             let perf_term = cols_cur_exprs
                 .iter()
-                .zip(power_of_gamma.iter())
+                .zip_eq(power_of_gamma.iter())
                 .map(|(a, b)| a.clone() * b.clone())
                 .fold(0.expr(), |a, b| a + b);
-
             [q_row_non_first * (fingerprint_cur - fingerprint_prev * (alpha - perf_term))]
         });
 
@@ -175,18 +198,40 @@ impl<F: Field> PermutationChip<F> {
             let q_row_non_first = meta.query_selector(q_row_non_first);
             let alpha_prev = meta.query_advice(alpha, Rotation::prev());
             let alpha_cur = meta.query_advice(alpha, Rotation::cur());
-            let gamma_prev = meta.query_advice(gamma, Rotation::prev());
-            let gamma_cur = meta.query_advice(gamma, Rotation::cur());
+
             [
-                q_row_non_first.clone() * (alpha_prev - alpha_cur),
-                q_row_non_first * (gamma_prev - gamma_cur),
+                vec![q_row_non_first.clone() * (alpha_prev - alpha_cur)],
+                power_of_gamma
+                    .iter()
+                    .map(|col| {
+                        let gamma_prev = meta.query_advice(*col, Rotation::prev());
+                        let gamma_cur = meta.query_advice(*col, Rotation::cur());
+                        q_row_non_first.clone() * (gamma_prev - gamma_cur)
+                    })
+                    .collect(),
             ]
+            .concat()
         });
+
+        meta.create_gate("power of gamma", |meta| {
+            let q_row_non_first = meta.query_selector(q_row_non_first);
+            let gamma = meta.query_advice(power_of_gamma[0], Rotation::cur());
+            power_of_gamma
+                .iter()
+                .tuple_windows()
+                .map(|(col1, col2)| {
+                    let col1_cur = meta.query_advice(*col1, Rotation::cur());
+                    let col2_cur = meta.query_advice(*col2, Rotation::cur());
+                    q_row_non_first.clone() * (col2_cur - col1_cur * gamma.clone())
+                })
+                .collect::<Vec<Expression<F>>>()
+        });
+
         PermutationChipConfig {
             fingerprints,
             q_row_non_first,
             alpha,
-            gamma,
+            power_of_gamma,
             _phantom: PhantomData::<F> {},
         }
     }
