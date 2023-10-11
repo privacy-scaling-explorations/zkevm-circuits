7984ae1f0a21a9dfc2a9e72c840706e03376ef72
ChihChengLiang <chihchengliang@gmail.com>
2023-10-11 23:31:39 +0800

    WIP fill in stubs

--- zkevm-circuits/src/bin/mpt_witness_gen/main.rs
+++ zkevm-circuits/src/bin/mpt_witness_gen/main.rs
@@ -48,7 +48,8 @@ fn generate_delete() {
     // The go code fetched all accounts and proofs, why?
     let state_db = StateDB::new();
 
-    let nodes =   prepare_witness( vec![trie_mod], state_db);
+    let nodes = prepare_witness(vec![trie_mod], state_db);
+    println!("nodes {:?}", nodes);
     // dump nodes to file
 }
 
@@ -58,95 +59,38 @@ fn prefetch_account(block_number: u64, address: Address) -> AccountProof {
     AccountProof(vec![])
 }
 
-fn prepare_witness(
-    trie_modifications: Vec<TrieModification>,
-    statedb: StateDB,
-) -> Vec<Node2> {
+fn prepare_witness(trie_modifications: Vec<TrieModification>, statedb: StateDB) -> Vec<Node2> {
     // mpt-witness-generator/witness/prepare_witness.go
     let mut nodes = vec![];
     for trie_mod in trie_modifications.iter() {
         match trie_mod.proof_type {
             MPTProofType::StorageChanged | MPTProofType::StorageDoesNotExist => {
-                //     kh := crypto.Keccak256(tMod.Key.Bytes())
-                //     if oracle.PreventHashingInSecureTrie {
-                //         kh = tMod.Key.Bytes()
-                //     }
-                //     keyHashed := trie.KeybytesToHex(kh)
+                let s_root = H256::zero();
+                let c_root = H256::zero();
 
-                //     addr := tMod.Address
-                //     addrh := crypto.Keccak256(addr.Bytes())
-                //     accountAddr := trie.KeybytesToHex(addrh)
-
-                //     oracle.PrefetchAccount(statedb.Db.BlockNumber, tMod.Address, nil)
-                //     // oracle.PrefetchStorage(statedb.Db.BlockNumber, addr, tMod.Key, nil)
-
-                //     if specialTest == 1 {
-                //         statedb.CreateAccount(addr)
-                //     }
-
-                //     accountProof, aNeighbourNode1, aExtNibbles1, aIsLastLeaf1, err :=
-                // statedb.GetProof(addr)     check(err)
-                //     storageProof, neighbourNode1, extNibbles1, isLastLeaf1, err :=
-                // statedb.GetStorageProof(addr, tMod.Key)     check(err)
-
-                //     sRoot := statedb.GetTrie().Hash()
-
-                //     if tMod.Type == StorageChanged {
-                //         statedb.SetState(addr, tMod.Key, tMod.Value)
-                //         statedb.IntermediateRoot(false)
-                //     }
-
-                //     cRoot := statedb.GetTrie().Hash()
-
-                //     proofType := "StorageChanged"
-                //     if tMod.Type == StorageDoesNotExist {
-                //         proofType = "StorageDoesNotExist"
-                //     }
-
-                //     accountProof1, aNeighbourNode2, aExtNibbles2, aIsLastLeaf2, err :=
-                // statedb.GetProof(addr)     check(err)
-
-                //     storageProof1, neighbourNode2, extNibbles2, isLastLeaf2, err :=
-                // statedb.GetStorageProof(addr, tMod.Key)     check(err)
-
-                //     aNode := aNeighbourNode2
-                //     aIsLastLeaf := aIsLastLeaf1
-                //     if len(accountProof) > len(accountProof1) {
-                //         // delete operation
-                //         aNode = aNeighbourNode1
-                //         aIsLastLeaf = aIsLastLeaf2
-                //     }
-
-                //     node := neighbourNode2
-                //     isLastLeaf := isLastLeaf1
-                //     if len(storageProof) > len(storageProof1) {
-                //         // delete operation
-                //         node = neighbourNode1
-                //         isLastLeaf = isLastLeaf2
-                //     }
-
-                //     if specialTest == 1 {
-                //         if len(accountProof1) != 2 {
-                //             panic("account should be in the second level (one branch above it)")
-                //         }
-                //         accountProof, accountProof1, sRoot, cRoot =
-                // modifyAccountSpecialEmptyTrie(addrh, accountProof1[len(accountProof1)-1])
-                //     }
-
-                // },
+                let nodes_account = convert_proof_to_witness();
+                let nodes_storage = convert_proof_to_witness();
 
                 nodes.push(start_node(trie_mod.proof_type, s_root, c_root));
-                // nodes.push(nodes_account)
-                // nodes.push(nodes_storage)
+                nodes.extend(nodes_account);
+                nodes.extend(nodes_storage);
                 nodes.push(end_node());
             }
             _ => {
-                //   nodes = obtainAccountProofAndConvertToWitness(i, tMod, len(trieModifications),
-                // statedb, specialTest)
+                nodes = obtain_account_proof_and_convert_to_witness();
+                return nodes;
             }
         }
     }
     nodes
+}
+
+fn obtain_account_proof_and_convert_to_witness() -> Vec<Node2> {
+    vec![]
+}
+
+fn convert_proof_to_witness() -> Vec<Node2> {
+    vec![]
 }
 
 fn start_node(proof_type: MPTProofType, s_root: H256, c_root: H256) -> Node2 {
@@ -192,5 +136,6 @@ fn end_node() -> Node2 {
     }
 }
 fn main() {
-    println!("mpt witness gen")
+    println!("mpt testing case gen");
+    generate_delete()
 }
