1e95486a55d4544813c44fe7281dfa2c68a3b3c6
Zhang Zhuo <mycinbrin@gmail.com>
2024-01-31 17:34:38 +0800

    Revert "Feat/ci fix (#1101)" (#1102)
    This reverts commit 93bc24e1dac1793e0df9da3817a92785cbb19570.

--- .github/ciChecksScripts/wakeUpRunner.sh
+++ .github/ciChecksScripts/wakeUpRunner.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
 profile="cirunner"
-runner_vpc_id="vpc-0323d32f82edcdec7"
+runner_vpc_id1="vpc-8bdf97ec"
+runner_vpc_id2="vpc-1176d875"
 region_opt="--region us-west-2"
 
 # Get runner status
-runner1=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[super1] Name=network-interface.vpc-id,Values=[$runner_vpc_id] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
-runner2=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[super2] Name=network-interface.vpc-id,Values=[$runner_vpc_id] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
+runner1=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[jenkins] Name=network-interface.vpc-id,Values=[$runner_vpc_id1] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
+runner2=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[jenkins] Name=network-interface.vpc-id,Values=[$runner_vpc_id2] --query "Reservations[*].Instances[*][InstanceId]" --output text | xargs)
 
-# start runner1
-while true; do
-    runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
-    echo 'runner1 - '$runner_status
-    if [[ $runner_status = "stopped" ]]; then
-        aws ec2 start-instances --profile $profile --instance-ids $runner1 ${region_opt}
-        break
-    elif [[ $runner_status = "running" ]]; then
-        echo 'waiting for runner1 restore'
-        sleep 30
+{
+    while true; do
         runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
-        if [[ $runner_status = "running" ]]; then
+        echo 'runner1 - '$runner_status
+        if [[ $runner_status = "stopped" ]]; then
+            aws ec2 start-instances --profile $profile --instance-ids $runner1 ${region_opt}
             break
+        elif [[ $runner_status = "running" ]]; then
+            echo 'waiting for runner1 restore'
+            sleep 30
+            runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
+            if [[ $runner_status = "running" ]]; then
+                break
+            fi
+        else
+            sleep 30
         fi
-    else
-        sleep 30
-    fi
-done
-
-# start runner2
+    done
+}&
 while true; do
-    runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
+    runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" --output text)
     echo 'runner2 - '$runner_status
     if [[ $runner_status = "stopped" ]]; then
-        aws ec2 start-instances --profile $profile --instance-ids $runner2 ${region_opt}
+        aws ec2 start-instances --profile $profile --instance-ids $runner2
         break
     elif [[ $runner_status = "running" ]]; then
         echo 'waiting for runner2 restore'
         sleep 30
-        runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
+        runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" --output text)
         if [[ $runner_status = "running" ]]; then
             break
         fi
@@ -45,3 +45,5 @@ while true; do
         sleep 30
     fi
 done
+
+exit 0
