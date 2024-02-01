734d84beed4d3280bffb6fc8f988a0b0c7472b4e
Zhang Zhuo <mycinbrin@gmail.com>
2024-02-01 21:35:12 +0800

    Feat/ci fix (#1103)
    * CI-fix
    
    * Update wakeUpRunner.sh
    
    * Update wakeUpRunner.sh
    
    * Update select-runner.yml
    
    ---------
    
    Co-authored-by: 张三 <32984960+johnsonjie@users.noreply.github.com>

--- .github/ciChecksScripts/wakeUpRunner.sh
+++ .github/ciChecksScripts/wakeUpRunner.sh
@@ -1,43 +1,24 @@
 #!/bin/bash
 
 profile="cirunner"
-runner_vpc_id1="vpc-8bdf97ec"
-runner_vpc_id2="vpc-1176d875"
+runner_vpc_id="vpc-0323d32f82edcdec7"
 region_opt="--region us-west-2"
 
 # Get runner status
-runner1=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[jenkins] Name=network-interface.vpc-id,Values=[$runner_vpc_id1] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
-runner2=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[jenkins] Name=network-interface.vpc-id,Values=[$runner_vpc_id2] --query "Reservations[*].Instances[*][InstanceId]" --output text | xargs)
+runner1=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[super1] Name=network-interface.vpc-id,Values=[$runner_vpc_id] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
+runner2=$(aws ec2 describe-instances --profile $profile --filters Name=tag:Name,Values=[super2] Name=network-interface.vpc-id,Values=[$runner_vpc_id] --query "Reservations[*].Instances[*][InstanceId]" ${region_opt} --output text | xargs)
 
-{
-    while true; do
-        runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
-        echo 'runner1 - '$runner_status
-        if [[ $runner_status = "stopped" ]]; then
-            aws ec2 start-instances --profile $profile --instance-ids $runner1 ${region_opt}
-            break
-        elif [[ $runner_status = "running" ]]; then
-            echo 'waiting for runner1 restore'
-            sleep 30
-            runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
-            if [[ $runner_status = "running" ]]; then
-                break
-            fi
-        else
-            sleep 30
-        fi
-    done
-}&
+# start runner1
 while true; do
-    runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" --output text)
-    echo 'runner2 - '$runner_status
+    runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
+    echo 'runner1 - '$runner_status
     if [[ $runner_status = "stopped" ]]; then
-        aws ec2 start-instances --profile $profile --instance-ids $runner2
+        aws ec2 start-instances --profile $profile --instance-ids $runner1 ${region_opt}
         break
     elif [[ $runner_status = "running" ]]; then
-        echo 'waiting for runner2 restore'
+        echo 'waiting for runner1 restore'
         sleep 30
-        runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" --output text)
+        runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner1 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
         if [[ $runner_status = "running" ]]; then
             break
         fi
@@ -46,4 +27,21 @@ while true; do
     fi
 done
 
-exit 0
+# start runner2
+while true; do
+    runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
+    echo 'runner2 - '$runner_status
+    if [[ $runner_status = "stopped" ]]; then
+        aws ec2 start-instances --profile $profile --instance-ids $runner2 ${region_opt}
+        break
+    elif [[ $runner_status = "running" ]]; then
+        echo 'waiting for runner2 restore'
+        sleep 30
+        runner_status=$(aws ec2 describe-instances --profile $profile --instance-ids $runner2 --query "Reservations[*].Instances[*].State.[Name]" ${region_opt} --output text)
+        if [[ $runner_status = "running" ]]; then
+            break
+        fi
+    else
+        sleep 30
+    fi
+done
