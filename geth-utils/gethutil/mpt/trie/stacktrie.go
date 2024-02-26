3b78ffd4f9561abd0830eabab2c4f7c5f5bd435a
miha-stopar <stopar.miha@gmail.com>
2024-02-26 08:52:45 +0100

    Switched to Go 1.8 to be able to use slices.Reverse

--- geth-utils/gethutil/mpt/trie/stacktrie.go
+++ geth-utils/gethutil/mpt/trie/stacktrie.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"io"
 	"main/gethutil/mpt/types"
+	"slices"
 	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -739,12 +740,7 @@ func (st *StackTrie) GetProof(db ethdb.KeyValueReader, key []byte) ([][]byte, er
 		}
 		// The proof is now reversed (only for non-hashed),
 		// let's reverse it back to have the leaf at the bottom:
-		l := len(proof)
-		var proof_sorted [][]byte
-		for i := 0; i < l; i++ {
-			proof_sorted = append(proof_sorted, proof[l-i-1])
-		}
-		proof = proof_sorted
+		slices.Reverse(proof)
 	}
 
 	return proof, nil
