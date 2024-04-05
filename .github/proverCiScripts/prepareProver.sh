f8a62f9a860727d06b7ed12a4ad39e608237cac2
Eduard S <eduardsanou@posteo.net>
2024-04-05 14:17:24 +0000

    feat: install recent go

--- .github/proverCiScripts/prepareProver.sh
+++ .github/proverCiScripts/prepareProver.sh
@@ -9,3 +9,6 @@ source_dir=$2
 
 mkdir -p $target_dir
 
+# Install a recent go toolchain
+wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
